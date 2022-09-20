f15f1f8c6c4e0bd51eb3c91bbd550224a330c677
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-20 23:12:10 +0600

    feat: start codewars problem digits explosion

---
+++ 7kyu/Digits explosion/main.js
@@ -0,0 +1,16 @@
+/*
+
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+explode("312")
+should return :
+
+"333122"
+
+
+*/
+
+//solution
+
+function explode(s) {}
