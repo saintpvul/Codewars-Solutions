e82c76ce48901c7937a8dc7c6dbaf0f11416b951
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-20 23:43:05 +0600

    feat: complete codewars problem head,tail

--- 7kyu/Head, Tail, Init and Last/main.js
+++ 7kyu/Head, Tail, Init and Last/main.js
@@ -31,3 +31,16 @@ tail([1,2,3,4,5]); => [2,3,4,5]
 */
 
 //solition
+
+function head(arr) {
+  return +arr.slice(0, 1);
+}
+function tail(arr) {
+  return arr.slice(1);
+}
+function init(arr) {
+  return arr.slice(0, arr.length - 1);
+}
+function last(arr) {
+  return +arr.slice(arr.length - 1);
+}
