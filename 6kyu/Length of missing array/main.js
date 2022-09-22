36f39fb0e6be35bc63ba6b337cd5309374748229
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-22 23:47:20 +0600

    feat: complete codewars problem length of missing array

--- 6kyu/Length of missing array/main.js
+++ 6kyu/Length of missing array/main.js
@@ -22,3 +22,21 @@ I have created other katas. Have a look if you like coding and challenges.
 */
 
 //solution
+
+function getLengthOfMissingArray(arrayOfArrays) {
+  const lengths = (arrayOfArrays || [])
+    .map((a) => (a ? a.length : 0))
+    .sort((a, b) => a - b);
+
+  if (lengths.includes(0)) {
+    return 0;
+  }
+
+  for (let i = 0; i < lengths.length - 1; i++) {
+    if (lengths[i] + 1 !== lengths[i + 1]) {
+      return lengths[i] + 1;
+    }
+  }
+
+  return 0;
+}
