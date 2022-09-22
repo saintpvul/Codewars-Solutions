92c9d3b8226bdeb2afa7cf93c9f374ca4b7d034f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-23 00:08:23 +0600

    feat: complete codewars problem sorting by bit

--- 6kyu/Sorting by bits/main.js
+++ 6kyu/Sorting by bits/main.js
@@ -25,3 +25,10 @@ Example:
 */
 
 // solution
+
+function sortByBit(arr) {
+  const countBit = (bit) => bit.toString(2).replace(/[0]/g, "").length;
+  return arr.sort((a, b) =>
+    countBit(a) === countBit(b) ? a - b : countBit(a) - countBit(b)
+  );
+}
