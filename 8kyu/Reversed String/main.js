8d1ae197e8de21facfbba7e617b8419c88ac2c2c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-23 18:59:32 +0600

    feat: complete codewars reversed str

--- 8kyu/Reversed String/main.js
+++ 8kyu/Reversed String/main.js
@@ -1,7 +1,14 @@
 /*
 
+Complete the solution so that it reverses the string passed into it.
 
+'world'  =>  'dlrow'
+'word'   =>  'drow'
 
 */
 
 //solution
+
+function solution(str) {
+  return str.split("").reverse().join("");
+}
