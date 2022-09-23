a1397a7343674cedead0cc615480cddbf6fb9aa8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-24 01:44:10 +0600

    feat: start codewars problem convers number to reversed arr

---
+++ 8kyu/onvert number to reversed array of digits/main.js
@@ -0,0 +1,8 @@
+/*
+
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+*/
+
+// solution
