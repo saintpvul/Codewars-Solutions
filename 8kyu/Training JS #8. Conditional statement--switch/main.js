d1abcf539a85dc03abe4359d5aed236d9f09534c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-24 01:23:32 +0600

    feat: start codewars problem conditional statement -- switch

---
+++ 8kyu/Training JS #8. Conditional statement--switch/main.js
@@ -0,0 +1,19 @@
+/*
+
+Task
+Complete the function howManydays. It accepts 1 parameter month, which means the month of the year. Different months have a different number of days as shown in the table below. Return the number of days that are in month. There is no need for input validation: month will always be greater than 0 and less than or equal to 12.
+
++---------------+-------------+
+|    month      |    days     |
++---------------+-------------+
+|1,3,5,7,8,10,12|     31      |
++---------------+-------------+
+|4,6,9,11       |     30      |
++---------------+-------------+
+|2              |     28      |  (Do not consider the leap year)
++---------------+-------------+
+Tip: Using default for most of the cases can reduce your work.
+
+*/
+
+// solution
