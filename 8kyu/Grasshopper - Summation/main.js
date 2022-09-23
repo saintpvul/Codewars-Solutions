7767315c1f354de8d6c2b675fc16626fcc755ad2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-24 01:39:26 +0600

    feat: complete codewars problem grasshopper summation

--- 8kyu/Grasshopper - Summation/main.js
+++ 8kyu/Grasshopper - Summation/main.js
@@ -6,3 +6,11 @@ Write a program that finds the summation of every number from 1 to num. The numb
 */
 
 // solution
+
+var summation = function (num) {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+};
