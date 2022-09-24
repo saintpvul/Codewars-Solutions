748c860b688106bdf808e48df750a0c9d1bb1b83
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-24 21:29:11 +0600

    feat: complete codewars problem run-length encoding

--- 6kyu/Run-length encoding/main.js
+++ 6kyu/Run-length encoding/main.js
@@ -6,3 +6,17 @@ Your task is to write such a run-length encoding. For a given string, return a l
 */
 
 // solution
+
+var runLengthEncoding = function (str) {
+  const arr = [];
+  let value = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1]) {
+      arr.push([value, str[i]]);
+      value = 1;
+    } else {
+      value++;
+    }
+  }
+  return arr;
+};
