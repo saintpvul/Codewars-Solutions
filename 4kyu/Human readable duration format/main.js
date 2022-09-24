a62637f36e38eb6b9f37ab67b21168ed16158ce1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-24 21:31:14 +0600

    feat: start codewars problem human readable duration

---
+++ 4kyu/Human readable duration format/main.js
@@ -0,0 +1,19 @@
+/*
+
+Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+
+The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+It is much easier to understand with an example:
+
+* For seconds = 62, your function should return 
+    "1 minute and 2 seconds"
+* For seconds = 3662, your function should return
+    "1 hour, 1 minute and 2 seconds"
+For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+
+Note that spaces are important.
+
+*/
+
+//
