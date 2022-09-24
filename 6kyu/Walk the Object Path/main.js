51d0a5c8322cdd4bdf326daa435ee462c18fb35d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-24 21:28:38 +0600

    feat: start codewars problem run-length encoding

--- 6kyu/Walk the Object Path/main.js
+++ 6kyu/Walk the Object Path/main.js
@@ -1,5 +1,14 @@
 /*
 
+Implement the method find which takes in an two parameters object and path. The path will be a period-delimited string of properties that we will use to traverse through our object to find our target value.
+
+Check out this Codecast to get started! https://codecast.qualified.io/interaction/594197b92f524d001c1ab790
+
+Edge Cases And Further Consideration
+Be sure to handle passing array indices. For example, if we have an object: { people: ['John', 'Dave', 'Lisa'] } and the path is 'people.1', the target value is 'Dave' which is the string at position 1 inside of the people array.
+
+Also this method should handle invalid paths. If we have an object { user: { name: 'Dan' } } and the path is 'user.wallet.money', we should return undefined. Valid paths are exclusive to properties on the object which are not inherited, in other words it is specific to this object and does not need to look up the prototype chain.
+
 
 
 */
---
+++ 6kyu/u Run-length encoding/main.js
@@ -0,0 +1,8 @@
+/*
+
+Task
+Your task is to write such a run-length encoding. For a given string, return a list (or array) of pairs (or arrays) [ (i1, s1), (i2, s2), …, (in, sn) ], such that one can reconstruct the original string by replicating the character sx ix times and concatening all those strings. Your run-length encoding should be minimal, ie. for all i the values si and si+1 should differ.
+
+*/
+
+// solution
