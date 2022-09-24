cf3a41e0b401bc1cf833d6796197ede33ba4a6d1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-24 20:52:13 +0600

    feat: start codewars problemcan you keep the seecret

---
+++ 6kyu/Can you keep a secret/main.js
@@ -0,0 +1,12 @@
+/*
+
+there's no such thing as private properties on a coffeescript object! But, maybe there are?
+
+Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods
+
+getSecret() which returns the secret
+setSecret() which sets the secret
+
+*/
+
+//solution
