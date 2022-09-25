dea9433e9d6c982378b26dc342d186e28ce3e7cf
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-25 19:11:18 +0600

    feat: complete codewars problem grasshopper - grade book

--- 8kyu/Grasshopper - Grade book/main.js
+++ 8kyu/Grasshopper - Grade book/main.js
@@ -14,3 +14,19 @@ Tested values are all between 0 and 100. Theres is no need to check for negative
 */
 
 // solution
+function getGrade(s1, s2, s3) {
+  let score = (s1 + s2 + s3) / 3;
+  if (score >= 0 && score < 60) {
+    return "F";
+  } else if (score >= 60 && score < 70) {
+    return "D";
+  } else if (score >= 70 && score < 80) {
+    return "C";
+  } else if (score >= 80 && score < 90) {
+    return "B";
+  } else if (score >= 90 && score <= 100) {
+    return "A";
+  } else {
+    return "undefined";
+  }
+}
