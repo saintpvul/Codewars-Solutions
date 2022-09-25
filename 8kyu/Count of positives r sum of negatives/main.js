c25178dc7746f2c5425036fd617bf2360ccd0c82
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-25 17:25:30 +0600

    feat: complete codewars problem count of pos/sum of neg

--- 8kyu/Count of positives r sum of negatives/main.js
+++ 8kyu/Count of positives r sum of negatives/main.js
@@ -12,3 +12,28 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 */
 
 // solution
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+
+function countPositivesSumNegatives(input) {
+  let count = 0,
+    sum = 0;
+  if (!input || !input.length) {
+    return [];
+  }
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > 0) {
+      count++;
+    } else if (input[i] <= 0) {
+      sum += input[i];
+    }
+  }
+  return [count, sum];
+}
+
+console.log(countPositivesSumNegatives(arr));
+
+let empty = [];
+let n = [null, null];
+console.log(countPositivesSumNegatives(empty));
+console.log(countPositivesSumNegatives(n));
