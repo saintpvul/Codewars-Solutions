3e307206eeb080b0e250c39d2984d2183b7e358c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-25 17:41:11 +0600

    feat: complete codewars problem is divisible by x and y?

--- 8kyu/Is n divisible or not/main.js
+++ 8kyu/Is n divisible or not/main.js
@@ -4,4 +4,9 @@ Create a function that checks if a number n is divisible by two numbers x AND y.
 
 */
 
-//
+// solution
+
+function isDivisible(n, x, y) {
+  if (n % x === 0 && n % y === 0) return true;
+  else return false;
+}
