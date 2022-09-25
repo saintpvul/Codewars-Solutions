cbaad4ac30fcde1d739d0071173fed5900486bf4
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-25 16:19:31 +0600

    feat: complete codewars problem quarter of the year

--- 8kyu/Quarter of the year/main.js
+++ 8kyu/Quarter of the year/main.js
@@ -7,3 +7,30 @@ For example: month 2 (February), is part of the first quarter; month 6 (June), i
 */
 
 // solution
+
+const quarterOf = (month) => {
+  let quarter;
+  switch (month) {
+    case 1:
+    case 2:
+    case 3:
+      quarter = 1;
+      break;
+    case 4:
+    case 5:
+    case 6:
+      quarter = 2;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      quarter = 3;
+      break;
+    case 10:
+    case 11:
+    case 12:
+      quarter = 4;
+      break;
+  }
+  return quarter;
+};
