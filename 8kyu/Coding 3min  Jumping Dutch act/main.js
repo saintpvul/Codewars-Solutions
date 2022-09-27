950214ca7c66e83bcf95083965dbbc9c8a27a40a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-28 00:45:12 +0600

    feat: start codewars problem coding dutch act

---
+++ 8kyu/Coding 3min  Jumping Dutch act/main.js
@@ -0,0 +1,18 @@
+/*
+
+Coding 3min : Jumping Dutch act
+
+This is the simple version of Shortest Code series. If you need some challenges, please try the challenge version
+
+Task:
+Mr. despair wants to jump off Dutch act, So he came to the top of a building.
+
+Scientific research shows that a man jumped from the top of the roof, when the floor more than 6, the person will often die in an instant; When the floor is less than or equal to 6, the person will not immediately die, he would scream. (without proof)
+
+Input: floor, The height of the building (floor)
+
+Output: a string, The voice of despair(When jumping Dutch act)
+
+*/
+
+// solution
