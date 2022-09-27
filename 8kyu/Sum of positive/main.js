9640c0671f4597f84a7194bbf54273db16176080
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-28 00:06:50 +0600

    feat: complete codewars problem sum of pos

--- 8kyu/Sum of positive/main.js
+++ 8kyu/Sum of positive/main.js
@@ -9,3 +9,19 @@ Note: if there is nothing to sum, the sum is default to 0.
 */
 
 // solution
+
+let a = [1, 2, -3, 13, 0];
+
+function positiveSum(arr) {
+  let sum = 0;
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+        sum += arr[i];
+      }
+    }
+    return sum;
+  } else return 0;
+}
+
+console.log(positiveSum(a));
