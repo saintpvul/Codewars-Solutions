4a28132e5cfae0341a2848855f3107e2d50f47c7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-28 23:35:37 +0600

    feat: complete codewars problem bug fixing #6

--- 8kyu/Switch Case - Bug Fixing #6/main.js
+++ 8kyu/Switch Case - Bug Fixing #6/main.js
@@ -6,3 +6,28 @@ Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluat
 */
 
 // solution
+
+function evalObject(value) {
+  var result = 0;
+  switch (value.operation) {
+    case "+":
+      result = value.a + value.b;
+      break;
+    case "-":
+      result = value.a - value.b;
+      break;
+    case "/":
+      result = value.a / value.b;
+      break;
+    case "*":
+      result = value.a * value.b;
+      break;
+    case "%":
+      result = value.a % value.b;
+      break;
+    case "^":
+      result = Math.pow(value.a, value.b);
+      break;
+  }
+  return result;
+}
