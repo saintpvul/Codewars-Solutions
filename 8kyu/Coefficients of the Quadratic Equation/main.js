842aadaaea67b61fc73579c311e894b2f8283db8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-28 23:11:58 +0600

    feat: start codewars coefficient quadratic

---
+++ 8kyu/Coefficients of the Quadratic Equation/main.js
@@ -0,0 +1,15 @@
+/*
+
+In this Kata you are expected to find the coefficients of quadratic equation of the given two roots (x1 and x2).
+
+Equation will be the form of ax^2 + bx + c = 0
+
+Return type is a Vector (tuple in Rust, Array in Ruby) containing coefficients of the equations in the order (a, b, c).
+
+Since there are infinitely many solutions to this problem, we fix a = 1.
+
+Remember, the roots can be written like (x-x1) * (x-x2) = 0
+
+*/
+
+// solution
