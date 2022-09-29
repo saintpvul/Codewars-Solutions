4800a56d3cd3bf47ed8e36433d3c6058e4ccf66b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-30 00:01:22 +0600

    feat: start codewars problem powers of 2

---
+++ 8kyu/Powers of 2/main.js
@@ -0,0 +1,7 @@
+/*
+
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+*/
+
+// solution
