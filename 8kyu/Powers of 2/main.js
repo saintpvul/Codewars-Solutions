006c6bee0305bf9b0ff9a32c3c5d07d787f49483
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-30 00:21:57 +0600

    feat: complete codewars problem powers of 2

--- 8kyu/Powers of 2/main.js
+++ 8kyu/Powers of 2/main.js
@@ -5,3 +5,15 @@ Complete the function that takes a non-negative integer n as input, and returns 
 */
 
 // solution
+
+function powersOfTwo(n) {
+  let arr = [];
+  let j = 1;
+  for (let i = 0; i <= n; i++) {
+    arr.push(j);
+    j = j * 2;
+  }
+  return arr;
+}
+
+console.log(powersOfTwo(2));
