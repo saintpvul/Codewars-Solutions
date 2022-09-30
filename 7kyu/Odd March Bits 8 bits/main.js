16b27e195f8d8a5ec9f6569a4271da726ebe6b9d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-30 19:38:41 +0600

    feat: start codewars problem odd march bits 8 bits

---
+++ 7kyu/Odd March Bits 8 bits/main.js
@@ -0,0 +1,31 @@
+/*
+
+Odd bits are getting ready for Bits Battles.
+
+Therefore the n bits march from right to left along an 8 bits path. Once the most-significant bit reaches the left their march is done. Each step will be saved as an array of 8 integers.
+
+Return an array of all the steps.
+
+1 <= n <= 8
+
+NOTE: n != 0, because n represents the number of 1s.
+
+Examples
+This resembles a simple 8 LED chaser:
+
+n = 3
+
+00000111
+00001110
+00011100
+00111000
+01110000
+11100000
+n = 7
+
+01111111
+11111110
+
+*/
+
+// solution
