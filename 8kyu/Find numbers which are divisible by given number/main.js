29d173b2e0df516c3504a47d5663f4b20d747cb8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-01 20:27:00 +0600

    feat: complete codewars problem findnumbers whichare divisible

--- 8kyu/Find numbers which are divisible by given number/main.js
+++ 8kyu/Find numbers which are divisible by given number/main.js
@@ -8,3 +8,13 @@ Example(Input1, Input2 --> Output)
 */
 
 // solution
+
+function divisibleBy(numbers, divisor) {
+  let newArr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % divisor === 0) {
+      newArr.push(numbers[i]);
+    }
+  }
+  return newArr;
+}
