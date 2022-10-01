080adce7ad076dee995c61f19dc8551486ad999b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-01 18:54:47 +0600

    feat: start codewars problem begginer series #2 clock

---
+++ 8kyu/Beginner Series #2 Clock/main.js
@@ -0,0 +1,16 @@
+/*
+
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+
+*/
+
+// solution
