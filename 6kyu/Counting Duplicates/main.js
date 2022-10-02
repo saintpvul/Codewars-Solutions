746c914769e71f271672f67b39931be118da9f00
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-02 22:33:55 +0600

    feat: complete codewars problem counting chars

--- 6kyu/Counting Duplicates/main.js
+++ 6kyu/Counting Duplicates/main.js
@@ -15,3 +15,14 @@ Example
 */
 
 // solution
+
+function duplicateCount(text) {
+  return text
+    .toLowerCase()
+    .split("")
+    .filter((val, i, arr) => {
+      return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+    }).length;
+}
+
+console.log(duplicateCount("Indivisibility"));
