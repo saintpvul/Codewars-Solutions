3325ba81168a07729816b45dbdac79bfa095be66
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-02 23:02:41 +0600

    feat: complete codewars problem even or odd

--- 7kyu/Odd or Even/main.js
+++ 7kyu/Odd or Even/main.js
@@ -22,3 +22,11 @@ Have fun!
 */
 
 //solution
+
+function oddOrEven(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum % 2 === 0 ? "even" : "odd";
+}
