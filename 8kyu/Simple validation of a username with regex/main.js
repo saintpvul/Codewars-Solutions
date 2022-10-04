ff2f2d52b322f6717bbf2b694db268feda67b040
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-05 02:40:50 +0600

    feat: complete codewars problem simple validation of username

--- 8kyu/Simple validation of a username with regex/main.js
+++ 8kyu/Simple validation of a username with regex/main.js
@@ -10,3 +10,11 @@ Length should be between 4 and 16 characters (both included).
 */
 
 // solution
+
+function validateUsr(username) {
+  return (
+    username.length >= 4 &&
+    username.length <= 16 &&
+    username.match(/[a-z]|_|\d/g).length === username.length
+  );
+}
