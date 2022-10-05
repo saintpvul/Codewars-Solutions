1aaad5c7f9b512878e707a2b15b9ed9f7ff3536d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-06 03:52:21 +0600

    feat: complete codewars problem find the smallest

--- 8kyu/Find the smallest integer in the array/main.js
+++ 8kyu/Find the smallest integer in the array/main.js
@@ -11,3 +11,10 @@ You can assume, for the purpose of this kata, that the supplied array will not b
 */
 
 // solution
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let sort = args.sort((a, b) => a - b);
+    return sort[0];
+  }
+}
