6dbfcfa2a389c28fe0be2e81233fb702bc2ee41c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-05 21:51:49 +0600

    feat: start codewars problem total amount of point

---
+++ 8kyu/Total amount of points/main.js
@@ -0,0 +1,18 @@
+/*
+
+Our football team has finished the championship.
+
+Our teams match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x us our teams score and y is our opponents score.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Points are awarded for each match as follows:
+
+if x > y: 3 points (win)
+if x < y: 0 points (loss)
+if x = y: 1 point (tie)
+We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+*/
+
+// solution
