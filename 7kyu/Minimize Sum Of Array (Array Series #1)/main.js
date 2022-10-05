ee608f8f137ec0ddb4b8b5ac9a5e496a9e117ec3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-06 04:22:35 +0600

    feat: complete codewars problem minimize sum of array

--- 7kyu/Minimize Sum Of Array (Array Series #1)/main.js
+++ 7kyu/Minimize Sum Of Array (Array Series #1)/main.js
@@ -6,3 +6,12 @@ Given an array of integers , Find the minimum sum which is obtained from summing
 */
 
 // solution
+
+function minSum(arr) {
+  const sorted = arr.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < arr.length / 2; i++) {
+    sum += sorted[i] * sorted[sorted.length - 1 - i];
+  }
+  return sum;
+}
