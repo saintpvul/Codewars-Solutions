433cf6a07113953abe3e44d345f2306d35da6833
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-06 22:14:15 +0600

    feat: complete codewars problem mumbling

--- 7kyu/Mumbling/main.js
+++ 7kyu/Mumbling/main.js
@@ -11,3 +11,20 @@ The parameter of accum is a string which includes only letters from a..z and A..
 */
 
 // solution
+function accum(s) {
+  let str = "";
+  let num = 1;
+  for (let i = 0; i < s.length; i++) {
+    str += `${s[i].repeat(num).toLowerCase()}-`;
+    num++;
+  }
+  return str
+    .split("-")
+    .map((elem) => {
+      return elem.charAt(0).toUpperCase() + elem.slice(1).toLowerCase();
+    })
+    .join("-")
+    .slice(str, str.length - 1);
+}
+
+console.log(accum("ZpglnRxqenU"));
