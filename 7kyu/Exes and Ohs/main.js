c09aed652e375c5f770310eda6cfd2b264ccdd4f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-07 23:24:43 +0600

    feat: complete codewars problem exes and ohs

--- 7kyu/Exes and Ohs/main.js
+++ 7kyu/Exes and Ohs/main.js
@@ -13,3 +13,19 @@ XO("zzoo") => false
 */
 
 // solution
+
+function XO(str) {
+  let x = 0;
+  let o = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "x" || str[i] === "X") {
+      x++;
+    } else if (str[i] === "o" || str[i] === "O") {
+      o++;
+    }
+  }
+  return x === o;
+}
+
+console.log(XO("xoxox"));
+console.log(XO("xoxo"));
