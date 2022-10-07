8986eecd66fec42b5b2ddb27dfb59744e0ef8778
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-08 00:05:03 +0600

    feat: complete codewars problem check same case

--- 7kyu/Check same case/main.js
+++ 7kyu/Check same case/main.js
@@ -21,3 +21,19 @@ Examples
 */
 
 // solution
+
+function sameCase(a, b) {
+  if (
+    a.toLowerCase() == a.toUpperCase() ||
+    b.toLowerCase() == b.toUpperCase()
+  ) {
+    return -1;
+  } else if (
+    (a == a.toLowerCase() && b == b.toLowerCase()) ||
+    (a == a.toUpperCase() && b == b.toUpperCase())
+  ) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
