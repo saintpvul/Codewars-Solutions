3da58c63f3b1ca2b84b5ef83010745f4826b9738
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-08 23:01:24 +0600

    feat: complete codewars problem i am all alone

--- 5kyu/I am all alone (poor me)/main.js
+++ 5kyu/I am all alone (poor me)/main.js
@@ -31,3 +31,50 @@ ex1
 */
 
 // solution
+
+function allAlone(m) {
+  let h = m.length,
+    w = m[0].length;
+  let dirs = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+  let potus = () => {
+    for (let y = 0; y < h; y++)
+      for (let x = 0; x < w; x++) if (m[y][x] == "X") return [y, x];
+    return [-1, -1];
+  };
+  let hash = (y, x) => y * w + x;
+  let inside = (y, x) => y >= 0 && x >= 0 && y < h && x < w && m[y][x] != "#";
+  let q = [],
+    seen = new Set();
+
+  q.unshift(potus());
+  while (q.length) {
+    let [y, x] = q.pop();
+    let i = hash(y, x);
+    if (inside(y, x) && !seen.has(i)) {
+      if (m[y][x] == "o") return false;
+      seen.add(i);
+      for (let [dy, dx] of dirs) {
+        q.unshift([y + dy, x + dx]);
+      }
+    }
+  }
+  return true;
+}
+
+console.log(
+  allAlone([
+    "#################             ".split(""),
+    "#     o         #   o         ".split(""),
+    "#          ######        o    ".split(""),
+    "####       #                  ".split(""),
+    "   #       ###################".split(""),
+    "   #                         #".split(""),
+    "   #                  X      #".split(""),
+    "   ###########################".split(""),
+  ])
+);
