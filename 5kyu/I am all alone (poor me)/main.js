75461fdd5be32d4b7ce659657e9e0d787bfb1179
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-08 22:33:34 +0600

    feat: start codewars problem i am all alone

---
+++ 5kyu/I am all alone (poor me)/main.js
@@ -0,0 +1,33 @@
+/*
+
+Story
+POTUS thinks he is all alone in the White House on Christmas Eve.
+
+But is he?
+
+The White House has a wall-penetrating radar security system that sees everything.
+
+Kata Task
+Process the radar image.
+
+Return true if POTUS really is home alone.
+
+Legend
+# walls
+X POTUS
+o elves
+Notes
+The house corners are square only
+The radar can also see into nearby buildings
+Examples
+ex1
+   o                o        #######
+ ###############             #     #
+ #             #        o    #     #
+ #  X          ###############     #
+ #                                 #
+ ###################################
+
+*/
+
+// solution
