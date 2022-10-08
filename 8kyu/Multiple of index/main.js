a71d0b9373ce6d5b2762ade327e79d96bc04f38b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-08 22:22:26 +0600

    feat: start codewars problem multiple the index

---
+++ 8kyu/Multiple of index/main.js
@@ -0,0 +1,13 @@
+/*
+
+Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+Some cases:
+[22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+
+[68, -1, 1, -7, 10, 10] => [-1, 10]
+
+
+*/
+
+// solution
