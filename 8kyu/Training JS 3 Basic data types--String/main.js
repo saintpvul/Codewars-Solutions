1b2cb2cd29555a069e2789cb1b51f430ed498597
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-09 17:47:06 +0600

    feat: complete codewars problem Training JS #3: Basic data types--String

--- 8kyu/Training JS 3 Basic data types--String/main.js
+++ 8kyu/Training JS 3 Basic data types--String/main.js
@@ -1,7 +1,46 @@
 /*
 
+Task
+misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
 
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3
+
+If everything is right, click "Submit" again to submit your code pass this kata.
 
 */
 
 // solution
+
+var a1 = "A",
+  a2 = "a",
+  b1 = "B",
+  b2 = "b",
+  c1 = "C",
+  c2 = "c",
+  d1 = "D",
+  d2 = "d",
+  e1 = "E",
+  e2 = "e",
+  n1 = "N",
+  n2 = "n";
+function Dad() {
+  return d1 + a2 + d2;
+}
+function Bee() {
+  return b1 + e2 + e2;
+}
+function banana() {
+  return b2 + a2 + n2 + a2 + n2 + a2;
+}
+
+function answer1() {
+  return "no";
+}
+function answer2() {
+  return "no";
+}
+function answer3() {
+  return "yes";
+}
