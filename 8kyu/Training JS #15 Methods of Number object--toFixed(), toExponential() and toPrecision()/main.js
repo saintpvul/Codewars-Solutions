5d392ae1e924416076c62b44d11579535b52d484
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-10 21:33:18 +0600

    feat: start codewars problem Training JS #15: Methods of Number object--toFixed(), toExponential() and toPrecision()

---
+++ 8kyu/Training JS #15 Methods of Number object--toFixed(), toExponential() and toPrecision()/main.js
@@ -0,0 +1,16 @@
+/*
+
+Task
+Coding in function howManySmaller, function accept 2 parameter:arr and n. arr is a decimal array. n is a decimal.
+
+The first mission: let all elements in the array keep two decimal places(No need to convert number n).
+
+The second mission: Traversal arr, count the number of the element which smaller than n and return it.
+
+for example:
+
+howManySmaller([1.234,1.235,1.228],1.24) should return 2
+
+*/
+
+// solution
