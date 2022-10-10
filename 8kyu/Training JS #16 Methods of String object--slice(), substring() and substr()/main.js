c7d8afcbba472afb4766d69ae4a3807a518ec487
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-10 22:33:59 +0600

    feat: start codewars problem Training JS #16: Methods of String object--slice(), substring() and substr()

---
+++ 8kyu/Training JS #16 Methods of String object--slice(), substring() and substr()/main.js
@@ -0,0 +1,12 @@
+/*
+
+Task
+Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+The first mission: Traversing arr, find the shortest string length.
+
+The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0). you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+*/
+
+// solution
