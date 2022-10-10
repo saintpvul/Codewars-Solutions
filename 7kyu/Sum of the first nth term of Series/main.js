a479302decf3b55962b2332778b6e32ba08d3088
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-10 23:59:03 +0600

    feat: complete codewars problem sum of the first nth term

--- 7kyu/Sum of the first nth term of Series/main.js
+++ 7kyu/Sum of the first nth term of Series/main.js
@@ -6,3 +6,16 @@ Your task is to write a function which returns the sum of following series upto 
 */
 
 // solution
+
+function SeriesSum(n) {
+  if (n === 0) {
+    return "0.00";
+  }
+  let sum = 0;
+  const base = 1;
+  for (i = 0; i < n; i++) {
+    let mod = i * 3;
+    sum += 1 / (base + mod);
+  }
+  return String(sum.toFixed(2));
+}
