2469d11e03c2f89ee1ad5818bbd26b696d36ed05
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-11 22:56:42 +0600

    feat: complete codewars problem Find Nearest square number

--- 8kyu/Find Nearest square number/main.js
+++ 8kyu/Find Nearest square number/main.js
@@ -5,3 +5,12 @@ Your task is to find the nearest square number, nearest_sq(n), of a positive int
 */
 
 // solution
+
+function nearestSq(n) {
+  let sqrt = Math.round(Math.sqrt(n));
+  return sqrt * sqrt;
+}
+
+const nearestSq = (n) => Math.round(Math.sqrt(n)) ** 2;
+
+console.log(nearestSq(111));
