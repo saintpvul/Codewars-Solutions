e1ff11a793ac4827c05e0792a46f431259848187
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-12 00:06:36 +0600

    feat: complete codewars problem Configure package json for a node application

--- 8kyu/Configure package json for a node application/main.js
+++ 8kyu/Configure package json for a node application/main.js
@@ -8,3 +8,11 @@ To pass the kata you need only the least that a real package file would require.
 */
 
 //solution
+
+const configuration = {
+  name: "your-pack-name",
+  description: "",
+  version: "0.0.1",
+};
+// Hint: try creating with `npm init` command
+// Help! -> https://docs.npmjs.com/files/package.json
