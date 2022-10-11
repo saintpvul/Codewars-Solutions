0b3c87984ee00a88fd04b53329322243effebb43
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-11 23:52:06 +0600

    feat: start codewars problem Configure package json for a node application

---
+++ 8kyu/Configure package json for a node application/main.js
@@ -0,0 +1,10 @@
+/*
+
+Your task
+Create a package.json configuration and assign it to the configuration constant so it can be tested.
+
+To pass the kata you need only the least that a real package file would require.
+
+*/
+
+//solution
