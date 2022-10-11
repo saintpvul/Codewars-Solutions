7115f95d299ee0fcc6e7c65ecf0e9748a8106a7a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-12 01:10:04 +0600

    feat: complete codewars problem are they opposite

--- 8kyu/Sadly story 1 Are they opposite/main.js
+++ 8kyu/Sadly story 1 Are they opposite/main.js
@@ -8,3 +8,12 @@ The opposite means: All letters of the two strings are the same, but the case is
 */
 
 // solution
+
+function isOpposite(s1, s2) {
+  if (s1 === s2 || s1.toLowerCase() != s2.toLowerCase()) return false;
+
+  for (let i = 0; i < s1.length; i++) {
+    if (s1.charAt(i) === s2.charAt(i)) return false;
+  }
+  return true;
+}
