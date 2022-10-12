4bc54e6c6bdeeb43c417ddb3f22320982cb5ddf4
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-12 19:50:27 +0600

    feat: complete codewars problem find the position

--- 8kyu/Find the position!/main.js
+++ 8kyu/Find the position!/main.js
@@ -9,3 +9,12 @@ Ouput :: "Position of alphabet: 1"
 */
 
 // solution
+function position(letter) {
+  let alph = "abcdefghijklmnopqrstuvwxyz";
+
+  for (let i = 0; i < alph.length; i++) {
+    if (alph[i] === letter) {
+      return `Position of alphabet: ${i + 1}`;
+    }
+  }
+}
