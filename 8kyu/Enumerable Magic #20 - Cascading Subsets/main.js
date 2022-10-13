63a8e3f274753ccb46a18fcde9a1c94dc3be16d7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-13 21:56:51 +0600

    feat: complete codewars problem enumerable magic #20 - cascading subsets

--- 8kyu/Enumerable Magic #20 - Cascading Subsets/main.js
+++ 8kyu/Enumerable Magic #20 - Cascading Subsets/main.js
@@ -13,3 +13,16 @@ As you can see, the lists are cascading; ie, they overlap, but never out of orde
 */
 
 // solution
+
+function eachCons(array, n) {
+  let resArr = [];
+  for (let i = 0; i <= array.length - n; i++) {
+    let currArr = [];
+    for (let j = i; j < i + n; j++) {
+      currArr.push(array[j]);
+    }
+    resArr.push(currArr);
+  }
+  return resArr;
+}
+console.log(eachCons([1, 2, 3, 4, 5, 6, 7, 8], 4));
