882c10df8784095506b5cb423c451792f50a3c96
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-13 20:36:46 +0600

    feat: complete codewars problem how many stairs will suzuki climb in 20 years

--- 8kyu/How many stairs will Suzuki climb in 20 years/main.js
+++ 8kyu/How many stairs will Suzuki climb in 20 years/main.js
@@ -19,3 +19,13 @@ Your function should return the 20 year estimate of the stairs climbed using the
 */
 
 // solution
+
+function stairsIn20(s) {
+  let sum = 0;
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < s[i].length; j++) {
+      sum += s[i][j];
+    }
+  }
+  return sum * 20;
+}
