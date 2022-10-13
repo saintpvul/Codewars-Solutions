7ef8d057cb293275c0a24a45e7e000daba495fa7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-13 23:43:31 +0600

    feat: complete codewars problem define a card suit

--- 8kyu/Define a card suit/main.js
+++ 8kyu/Define a card suit/main.js
@@ -12,3 +12,27 @@ Our deck (is preloaded):
 */
 
 // solution
+
+function defineSuit(card) {
+  let suits = {
+    clubs: "♣",
+    diamonds: "♦",
+    hearts: "♥",
+    spades: "♠",
+  };
+  let index;
+  let foundedSign = card.replace(/\w/gi, "");
+  let values = Object.values(suits);
+  let keys = Object.keys(suits);
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === foundedSign[0]) {
+      index = i;
+      break;
+    }
+  }
+  return "" + keys[index];
+}
+
+console.log(defineSuit("K♥"));
+console.log(defineSuit("10♥"));
+console.log(defineSuit("♥"));
