72fb1f40dd585ac6d37b5fc5a190d761001842e2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-13 20:57:59 +0600

    feat: complete codewars problem object oriented piracy

--- 8kyu/Object Oriented Piracy/main.js
+++ 8kyu/Object Oriented Piracy/main.js
@@ -32,3 +32,14 @@ Good luck!
 */
 
 // solution
+
+class Ship {
+  constructor(draft, crew) {
+    this.draft = draft;
+    this.crew = crew;
+  }
+
+  isWorthIt() {
+    return this.draft - 1.5 * this.crew > 20;
+  }
+}
