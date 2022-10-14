4d6590a6438f338d773a1534e377b7ef60fdd4ae
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-14 23:11:02 +0600

    feat: complete codewars problem sum of diferences in array

--- 8kyu/Sum of differences in array/main.js
+++ 8kyu/Sum of differences in array/main.js
@@ -13,3 +13,20 @@ If the array is empty or the array has only one element the result should be 0 (
 */
 
 // solution
+
+function sumOfDifferences(arr) {
+  let sum = 0;
+  if (arr.length > 1) {
+    let sorted = arr.sort((a, b) => b - a);
+    let arrToSum = [];
+    for (let i = 0; i < sorted.length - 1; i++) {
+      arrToSum.push(sorted[i] - sorted[i + 1]);
+    }
+    sum = arrToSum.reduce((acc, item) => (acc += item));
+    return sum;
+  } else return sum;
+}
+
+console.log(sumOfDifferences([5]));
+console.log(sumOfDifferences([1, 2, 10]));
+console.log(sumOfDifferences([5, 123, 10]));
