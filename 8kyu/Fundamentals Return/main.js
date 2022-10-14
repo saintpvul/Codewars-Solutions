32e9d040f04a833e0e602f6ed3fe4fe64189e959
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-14 23:44:02 +0600

    feat: complete codewars problem fundamentals: return

--- 8kyu/Fundamentals Return/main.js
+++ 8kyu/Fundamentals Return/main.js
@@ -21,3 +21,15 @@ Note: All math operations will be: a (operation) b
 */
 
 // solution
+
+const add = (a, b) => a + b;
+
+const divide = (a, b) => a / b;
+
+const multiply = (a, b) => a * b;
+
+const mod = (a, b) => a % b;
+
+const exponent = (a, b) => a ** b;
+
+const subt = (a, b) => a - b;
