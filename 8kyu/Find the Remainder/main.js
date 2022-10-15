dec4d993383b61c7421c9e2b67d13289faaf4e15
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-16 03:30:49 +0600

    feat: start codewars problem find the remainder

---
+++ 8kyu/Find the Remainder/main.js
@@ -0,0 +1,15 @@
+/*
+
+Task:
+Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+Division by zero should return NaN.
+
+Examples:
+n = 17
+m = 5
+result = 2 (remainder of `17 / 5`)
+
+*/
+
+// solution
