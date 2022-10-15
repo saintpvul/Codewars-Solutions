23c8cdb6b6cc4a60457227eb924b78424281b092
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-15 18:05:33 +0600

    feat: start codewars problem is the day today

---
+++ 8kyu/Is the date today/main.js
@@ -0,0 +1,10 @@
+/*
+
+DESCRIPTION:
+Write a simple function that takes a Date as a parameter and returns a Boolean representing whether the date is today or not.
+
+Make sure that your function does not return a false positive by only checking the day.
+
+*/
+
+// solution
