961f419ef711bad0d70407aebb17aa2399b2ecc5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-17 21:11:08 +0600

    feat: start codewars problem ascii total

---
+++ 8kyu/ASCII Total/main.js
@@ -0,0 +1,11 @@
+/*
+
+You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all ASCII characters.
+
+examples:
+
+uniTotal("a") == 97 uniTotal("aaa") == 291
+
+*/
+
+// solution
