adee1470c5eca4494f209ae9b583b60fb301e405
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-19 02:12:17 +0600

    feat: complete codewars problem return two highest values in list

--- 8kyu/Return Two Highest Values in List/main.js
+++ 8kyu/Return Two Highest Values in List/main.js
@@ -14,3 +14,10 @@ Examples:
 */
 
 // solution
+
+const twoHighest = (arr) =>
+  Array.from(new Set(arr))
+    .sort((a, b) => b - a)
+    .slice(0, 2);
+
+console.log(twoHighest([15, 20, 20, 17]));
