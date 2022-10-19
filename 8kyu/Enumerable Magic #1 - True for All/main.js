4546118d4b71b57b17c259db18ce12bb5e460a2e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-19 23:39:38 +0600

    feat: complete codewars problem enumerable magic #1

--- 8kyu/Enumerable Magic #1 - True for All/main.js
+++ 8kyu/Enumerable Magic #1 - True for All/main.js
@@ -14,3 +14,13 @@ all((1, 2, 3, 4, 5), less_than_9)    -> True
 */
 
 // solution
+
+function all(arr, fun) {
+  return arr.every(fun);
+}
+
+console.log(
+  all([1, 2, 3, 4, 5], function (v) {
+    return v < 9;
+  })
+);
