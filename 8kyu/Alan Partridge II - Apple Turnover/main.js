66a29a098edeac40f6c7b94a27324bb57affd2fc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-20 02:02:57 +0600

    feat: complete codewars problem alan partidge ii - apple turnover

--- 8kyu/Alan Partridge II - Apple Turnover/main.js
+++ 8kyu/Alan Partridge II - Apple Turnover/main.js
@@ -21,3 +21,8 @@ Note: Input will either be a positive integer (or a string for untyped languages
 */
 
 // solution
+
+const apple = (x) =>
+  x * x > 1000
+    ? `It's hotter than the sun!!`
+    : `Help yourself to a honeycomb Yorkie for the glovebox.`;
