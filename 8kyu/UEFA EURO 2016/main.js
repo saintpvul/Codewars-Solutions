6af6a56d9c567215f7b92686c9b42641876d4099
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-21 02:59:54 +0600

    feat: complete codewars problem uefa euro 2016

--- 8kyu/UEFA EURO 2016/main.js
+++ 8kyu/UEFA EURO 2016/main.js
@@ -9,3 +9,11 @@ uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match P
 */
 
 // solution
+
+function uefaEuro2016(teams, scores) {
+  return scores[0] < scores[1]
+    ? `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
+    : scores[0] === scores[1]
+    ? `At match ${teams[0]} - ${teams[1]}, teams played draw.`
+    : `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`;
+}
