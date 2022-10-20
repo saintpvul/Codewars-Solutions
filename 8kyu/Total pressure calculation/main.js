52a43b688e8e67a7a665476872e0f7feca78b096
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-21 02:54:53 +0600

    feat: complete codewars problem total pressute calculation

--- 8kyu/Total pressure calculation/main.js
+++ 8kyu/Total pressure calculation/main.js
@@ -87,3 +87,19 @@ The gas constant  R=0.082dm3⋅atm⋅K−1⋅mol−1\ R = 0.082dm^3 \cdot atm \c
 */
 
 // solution
+
+function solution(
+  molarMass1,
+  molarMass2,
+  givenMass1,
+  givenMass2,
+  volume,
+  temp
+) {
+  return (
+    ((givenMass1 / molarMass1 + givenMass2 / molarMass2) *
+      0.082 *
+      (temp + 273.15)) /
+    volume
+  );
+}
