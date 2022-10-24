092d3c7147d9ebec8286d24f4dd338b0e3a208b8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-24 23:46:37 +0600

    feat: complete codewars problem be cocise i - the ternary operator

--- 8kyu/Be Concise I - The Ternary Operator/main.js
+++ 8kyu/Be Concise I - The Ternary Operator/main.js
@@ -21,3 +21,16 @@ Whatever you do, do not change what the function does. Good luck :)
 */
 
 // solution
+
+// TODO: Refactor and shorten the function
+
+const describeAge = (age) =>
+  `You're a(n) ${
+    age <= 12
+      ? "kid"
+      : age >= 13 && age <= 17
+      ? "teenager"
+      : age >= 18 && age <= 64
+      ? "adult"
+      : "elderly"
+  }`;
