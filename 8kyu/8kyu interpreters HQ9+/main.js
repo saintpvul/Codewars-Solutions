d489c2532e63c1eed1a3e28c89eb2b16e5ff08cd
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-25 00:32:18 +0600

    feat: complete codewars problem 8kyu interpreters hq9+

--- 8kyu/8kyu interpreters HQ9+/main.js
+++ 8kyu/8kyu interpreters HQ9+/main.js
@@ -28,3 +28,21 @@ For everything else, don't return anything (return null in C#, None in Rust).
 */
 
 // solution
+
+function HQ9(code) {
+  let bottles = "";
+  switch (code) {
+    case "H":
+      return "Hello World!";
+    case "Q":
+      return "Q";
+    case "9":
+      for (let i = 99; i > 2; i--) {
+        bottles += `${i} bottles of beer on the wall, ${i} bottles of beer.\nTake one down and pass it around, ${
+          i - 1
+        } bottles of beer on the wall.\n`;
+      }
+      bottles += `2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.`;
+      return bottles;
+  }
+}
