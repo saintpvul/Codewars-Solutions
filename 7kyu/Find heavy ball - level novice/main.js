ebf8859b9feb08d7b312d535b9e338ed56634aac
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-25 21:23:13 +0600

    feat: complete codewars problem find heavy ball - level novice

--- 7kyu/Find heavy ball - level novice/main.js
+++ 7kyu/Find heavy ball - level novice/main.js
@@ -29,3 +29,12 @@ master
 */
 
 // solution
+
+function findBall(scales, balls = [0, 1, 2, 3, 4, 5, 6, 7]) {
+  if (balls.length === 1) {
+    return balls[0];
+  }
+  let a = balls.slice(0, balls.length / 2),
+    b = balls.slice(a.length);
+  return findBall(scales, scales.getWeight(a, b) < 0 ? a : b);
+}
