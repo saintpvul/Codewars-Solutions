0054b21b84a84d0df0e8ac304ff5bc1925af0a0f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-25 21:47:56 +0600

    feat: start codewars problem explosive sum

---
+++ 4kyu/Explosive Sum/main.js
@@ -0,0 +1,30 @@
+/*
+
+How many ways can you make the sum of a number?
+From wikipedia: https://en.wikipedia.org/wiki/Partition_(number_theory)
+
+In number theory and combinatorics, a partition of a positive integer n, also called an integer partition, is a way of writing n as a sum of positive integers. Two sums that differ only in the order of their summands are considered the same partition. If order matters, the sum becomes a composition. For example, 4 can be partitioned in five distinct ways:
+
+4
+3 + 1
+2 + 2
+2 + 1 + 1
+1 + 1 + 1 + 1
+Examples
+Basic
+sum(1) // 1
+sum(2) // 2  -> 1+1 , 2
+sum(3) // 3 -> 1+1+1, 1+2, 3
+sum(4) // 5 -> 1+1+1+1, 1+1+2, 1+3, 2+2, 4
+sum(5) // 7 -> 1+1+1+1+1, 1+1+1+2, 1+1+3, 1+2+2, 1+4, 5, 2+3
+
+sum(10) // 42
+Explosive
+sum(50) // 204226
+sum(80) // 15796476
+sum(100) // 190569292
+See here for more examples.
+
+*/
+
+// solution
