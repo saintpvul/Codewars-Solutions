d9fd723a08219d9c6f7fa116dc47eb41ad878da5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-27 02:23:38 +0600

    feat: complete evil or odius codewars problem

--- 8kyu/Evil or Odious/main.js
+++ 8kyu/Evil or Odious/main.js
@@ -22,3 +22,6 @@ function evil(n) {
   }
   return count % 2 === 0 ? "It's Evil!" : "It's Odious!";
 }
+
+const evil = (n) =>
+  n.toString(2).match(/1/g).length % 2 === 0 ? "It's Evil!" : "It's Odious!";
