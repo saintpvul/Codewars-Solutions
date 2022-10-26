c213b6713fcf14832a60bacc63624eacb946a2fe
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-26 22:38:47 +0600

    complete: start simple calculator

--- 8kyu/simple calculator/main.js
+++ 8kyu/simple calculator/main.js
@@ -17,3 +17,18 @@ Good luck!
 */
 
 // solution
+function calculator(a, b, sign) {
+  if (typeof a === "number" && typeof b === "number") {
+    switch (sign) {
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+      case "/":
+        return a / b;
+      case "*":
+        return a * b;
+    }
+  }
+  return "unknown value";
+}
