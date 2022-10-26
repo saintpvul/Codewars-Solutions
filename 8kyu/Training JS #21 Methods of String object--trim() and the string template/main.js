11d4e20d29e26aad0710bb24450de27867e9f682
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-26 22:21:28 +0600

    feat: complete codewars problem training js #21 methods of string object -- trim() and the string template

--- 8kyu/Training JS #21 Methods of String object--trim() and the string template/main.js
+++ 8kyu/Training JS #21 Methods of String object--trim() and the string template/main.js
@@ -28,3 +28,8 @@ xyxyxyxyxy  <---The effect when you console.log it
 */
 
 // solution
+
+const fiveLine = (s) =>
+  `${s.trim()}\n${s.trim().repeat(2)}\n${s.trim().repeat(3)}\n${s
+    .trim()
+    .repeat(4)}\n${s.trim().repeat(5)}`;
