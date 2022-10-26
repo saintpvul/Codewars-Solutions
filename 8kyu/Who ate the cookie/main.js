f22e678441519fb08bed16e04558800fc09c1458
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-26 23:35:11 +0600

    feat: complete who ate the cookie codewars problem

--- 8kyu/Who ate the cookie/main.js
+++ 8kyu/Who ate the cookie/main.js
@@ -11,3 +11,12 @@ Please leave feedback for this kata. Cheers!
 */
 
 // solution
+
+const cookie = (x) =>
+  `Who ate the last cookie? It was ${
+    typeof x === "string"
+      ? "Zach"
+      : typeof x === "number"
+      ? "Monica"
+      : "the dog"
+  }!`;
