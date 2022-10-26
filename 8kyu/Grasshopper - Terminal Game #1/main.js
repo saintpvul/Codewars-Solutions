9e803c72e96ac1076903d9d539e50788e7ad39dc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-27 00:40:08 +0600

    feat: complete grasshopper - terminal game #1 codewars problem

--- 8kyu/Grasshopper - Terminal Game #1/main.js
+++ 8kyu/Grasshopper - Terminal Game #1/main.js
@@ -13,3 +13,19 @@ experience	0
 */
 
 // solution
+
+class Hero {
+  constructor(
+    name = "Hero",
+    position = "00",
+    health = 100,
+    damage = 5,
+    experience = 0
+  ) {
+    this.name = name;
+    this.position = position;
+    this.health = health;
+    this.damage = damage;
+    this.experience = experience;
+  }
+}
