3504c9d324dcc416e5d534a36617a9a72f4a98ad
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-27 02:31:38 +0600

    feat: complete unfinished loop codewars problem

--- 8kyu/Unfinished Loop - Bug Fixing #1/main.js
+++ 8kyu/Unfinished Loop - Bug Fixing #1/main.js
@@ -6,3 +6,13 @@ Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his 
 */
 
 // solution
+
+function createArray(number) {
+  let newArray = [];
+
+  for (let counter = 1; counter <= number; counter++) {
+    newArray.push(counter);
+  }
+
+  return newArray;
+}
