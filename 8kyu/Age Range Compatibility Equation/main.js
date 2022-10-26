d5085e21afdbcc675fe5a92bbffee7f8653595e5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-27 01:17:05 +0600

    feat: complete age range compatibillity equation codewars problem

--- 8kyu/Age Range Compatibility Equation/main.js
+++ 8kyu/Age Range Compatibility Equation/main.js
@@ -22,3 +22,8 @@ age = 17   =>   15-20
 */
 
 // solution
+
+const datingRange = (age) =>
+  age <= 14
+    ? `${Math.floor(age - 0.1 * age)}-${Math.floor(age + 0.1 * age)}`
+    : `${Math.floor(age / 2 + 7)}-${Math.floor((age - 7) * 2)}`;
