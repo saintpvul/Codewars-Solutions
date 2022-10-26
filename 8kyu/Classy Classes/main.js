ff8ff3e0ba4c6f1008b41803b20424cb9a37e4ac
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-27 00:59:12 +0600

    feat: complete classy classes codewars problem

--- 8kyu/Classy Classes/main.js
+++ 8kyu/Classy Classes/main.js
@@ -13,3 +13,13 @@ Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Cla
 */
 
 // solution
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  get info() {
+    return `${this.name}s age is ${this.age}`;
+  }
+}
