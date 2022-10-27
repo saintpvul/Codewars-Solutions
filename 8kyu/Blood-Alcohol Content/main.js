bb3a367d8fb3469643635cf4b334dae536d15790
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-27 20:54:42 +0600

    feat: complete blood-alcohol content codewars problem

--- 8kyu/Blood-Alcohol Content/main.js
+++ 8kyu/Blood-Alcohol Content/main.js
@@ -27,3 +27,12 @@ Using these parameters, create the function that will calculate Bob's and other 
 */
 
 // solution
+
+const bloodAlcoholContent = (drinks, weight, sex, time) =>
+  Number(
+    (
+      ((drinks.ounces * drinks.abv * 5.14) / weight) *
+        (sex === "male" ? 0.73 : 0.66) -
+      0.015 * time
+    ).toFixed(4)
+  );
