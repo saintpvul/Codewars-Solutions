4a50fa08d36aca7479b6da7ba656b9794053d1e8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-29 00:09:43 +0600

    feat: start do something n.times (simplify for loop) codewars problem

---
+++ 8kyu/Do something n.times (Simplifying for loops)/main.js
@@ -0,0 +1,8 @@
+/*
+
+Task Summary
+In case you got lost, here's precisely what you have to do: define a method Number.prototype.times that accepts a function f as an argument and executes it as many times as the integer it is called on (e.g. (100).times would execute something 100 times). The iteration variable i should be supplied to the anonymous function being executed in order to support looping through array elements.
+
+*/
+
+// solution
