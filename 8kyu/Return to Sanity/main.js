9f3b2414f1a3efc01f165b59e2069ba248487bf6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-29 03:30:54 +0600

    feat:complete return to sanity codewars problem

--- 8kyu/Return to Sanity/main.js
+++ 8kyu/Return to Sanity/main.js
@@ -5,3 +5,8 @@ This function should return an object, but it's not doing what's intended. What'
 */
 
 // solution
+
+function mystery() {
+  var results = { sanity: "Hello" };
+  return results;
+}
