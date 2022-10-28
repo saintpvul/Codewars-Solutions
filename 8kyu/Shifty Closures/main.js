0452e6613dabe2256e7b32f1b10943075cf02585
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-29 03:27:52 +0600

    feat: complete shifty closures codewars problem

--- 8kyu/Shifty Closures/main.js
+++ 8kyu/Shifty Closures/main.js
@@ -7,3 +7,12 @@ Why doesn't greet_abe() actually greet Abe?
 */
 
 // solution
+
+var nameA = "Abe";
+var greet_abe = function () {
+  return "Hello, " + nameA + "!";
+};
+let nameB = "Ben";
+var greet_ben = function () {
+  return "Hello, " + nameB + "!";
+};
