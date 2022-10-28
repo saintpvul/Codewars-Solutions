7061c9878773b6aa0e230d11310b0bc9846fdac9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-28 22:34:08 +0600

    feat: complete fix your code before the garden dies! codewars problem

--- 8kyu/Fix your code before the garden dies!/main.js
+++ 8kyu/Fix your code before the garden dies!/main.js
@@ -7,3 +7,11 @@ Your task is to debug the code before your plants die!
 */
 
 // solution
+
+function rainAmount(mm) {
+  if (mm < 40) {
+    return "You need to give your plant " + Math.abs(mm - 40) + "mm of water";
+  } else {
+    return "Your plant has had more than enough water for today!";
+  }
+}
