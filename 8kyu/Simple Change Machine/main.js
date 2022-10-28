512a9fcf59297bdfc2c1c38b9c110fce16def362
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-28 21:30:38 +0600

    feat: complete simple change machine codewars problem

--- 8kyu/Simple Change Machine/main.js
+++ 8kyu/Simple Change Machine/main.js
@@ -13,3 +13,20 @@ This change machine is programmed to accept and distribute strings rather than n
 */
 
 // solution
+
+function changeMe(moneyIn) {
+  switch (moneyIn) {
+    case "£5":
+      return "20p ".repeat(25).trim();
+    case "£2":
+      return "20p ".repeat(10).trim();
+    case "£1":
+      return "20p ".repeat(5).trim();
+    case "50p":
+      return "20p 20p 10p";
+    case "20p":
+      return "10p 10p";
+    default:
+      return moneyIn;
+  }
+}
