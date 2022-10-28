a9bab16073957e6c6ec0d26c6a52276bad6f2acd
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-29 01:55:47 +0600

    feat: complete implement array.prototype.filter() codewars problem

--- 8kyu/Implement Array.prototype.filter()/main.js
+++ 8kyu/Implement Array.prototype.filter()/main.js
@@ -10,3 +10,14 @@ Of course, the existing Array.prototype.filter() function has been undefined for
 */
 
 // solution
+
+Array.prototype.filter = function (func) {
+  const filtered = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (func(this[i])) {
+      filtered.push(this[i]);
+    }
+  }
+  return filtered;
+};
