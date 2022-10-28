d8ac2e9555f6188d70e758c2151c25ea5a115134
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-29 03:32:07 +0600

    feat: start hello happy codewarrior codewars problem

---
+++ 8kyu/Hello Happy Codevarrior!/main.js
@@ -0,0 +1,16 @@
+/*
+
+VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+var albert = new Warrior("Al")
+var boris  = new Warrior("Boris")
+
+albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+
+*/
+
+// solution
