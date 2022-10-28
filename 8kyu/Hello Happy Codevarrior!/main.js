36e55903b7f242549ff050eeae82233482abb63a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-29 03:40:55 +0600

    feat: complete hello happy codewarrior codewars problem

--- 8kyu/Hello Happy Codevarrior!/main.js
+++ 8kyu/Hello Happy Codevarrior!/main.js
@@ -14,3 +14,20 @@ albert.toString() --> "Hi! my name's Boris" <-- ohoh..
 */
 
 // solution
+
+class Warrior {
+  constructor(newName) {
+    this.wName = newName;
+  }
+
+  name(newName) {
+    if (newName) {
+      this.wName = newName;
+    }
+    return this.wName;
+  }
+}
+
+Warrior.prototype.toString = function () {
+  return `Hi! my name's ${this.wName}`;
+};
