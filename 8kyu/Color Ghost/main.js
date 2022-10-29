000b87710100311f2fd4ec94361af1e7e6fcea5b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-29 16:40:52 +0600

    feat: complete ghost color codewars problem

--- 8kyu/Color Ghost/main.js
+++ 8kyu/Color Ghost/main.js
@@ -13,3 +13,14 @@ ghost.color //=> "white" or "yellow" or "purple" or "red"
 */
 
 // solution
+
+class Ghost {
+  constructor() {
+    this.colors = ["white", "yellow", "purple", "red"];
+  }
+
+  get color() {
+    let randomColor = Math.trunc(Math.random() * this.colors.length);
+    return this.colors[randomColor];
+  }
+}
