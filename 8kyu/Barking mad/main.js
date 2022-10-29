27a2015b7c7d5762d70d77f71f0edf4632115082
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-29 16:31:24 +0600

    feat: complete barking mad codewars problem

--- 8kyu/Barking mad/main.js
+++ 8kyu/Barking mad/main.js
@@ -8,3 +8,19 @@ Use method prototypes to enable all Dogs to bark.
 
 */
 // solution
+
+function Dog(breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function () {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+
+scoobydoo.bark = function () {
+  return "Woof";
+};
