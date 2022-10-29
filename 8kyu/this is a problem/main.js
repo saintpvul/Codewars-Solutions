c818e930ae1bca12944060df4eea064193b2e138
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-29 16:36:18 +0600

    feat: complete this is a problem codewars problem

--- 8kyu/this is a problem/main.js
+++ 8kyu/this is a problem/main.js
@@ -18,3 +18,9 @@ n.name //Expected: John Doe
 */
 
 // solution
+
+function NameMe(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.name = this.firstName + " " + this.lastName;
+}
