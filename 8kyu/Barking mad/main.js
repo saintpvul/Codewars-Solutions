42b2586509ce301b8dc76c69f3c9b09c21fa6f10
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-29 16:31:12 +0600

    feat: start barking mad codewars problem

---
+++ 8kyu/Barking mad/main.js
@@ -0,0 +1,10 @@
+/*
+
+Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+snoopy.bark(); // return "Woof"
+scoobydoo.bark(); // undefined
+Use method prototypes to enable all Dogs to bark.
+
+*/
+// solution
