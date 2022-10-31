a69da99781060cb9269359227770ca1ba01f488b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-10-31 22:49:20 +0600

    feat: complete sum of two lowest positive integers codewars problem

--- 7kyu/Sum of two lowest positive integers/main.js
+++ 7kyu/Sum of two lowest positive integers/main.js
@@ -9,3 +9,22 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 */
 
 // solution
+
+function sumTwoSmallestNumbers(numbers) {
+  let first = Math.max(...numbers),
+    second = Math.max(...numbers);
+
+  if (numbers.length < 2) {
+    return "Array is too short";
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < first && numbers[i] > 0) {
+      second = first;
+      first = numbers[i];
+    } else if (numbers[i] < second && numbers[i] != first && numbers[i] > 0) {
+      second = numbers[i];
+    }
+  }
+  return first + second;
+}
