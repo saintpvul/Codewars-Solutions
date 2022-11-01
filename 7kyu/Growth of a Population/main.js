8f6407f6abd4a1433f7df9fcc935a15c8a7b2d46
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-01 23:55:36 +0600

    feat: complete growth of a population codewars problem

--- 7kyu/Growth of a Population/main.js
+++ 7kyu/Growth of a Population/main.js
@@ -31,3 +31,8 @@ Don't forget to convert the percent parameter as a percentage in the body of you
 */
 
 // solution
+
+const nbYear = (p0, percent, aug, p) =>
+  p0 < p
+    ? true + nbYear((p0 + (p0 * percent) / 100 + aug) | 0, percent, aug, p)
+    : false;
