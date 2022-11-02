8d22f26334e84c40c259ff9b59ac91a6ea6e87c0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-03 00:20:08 +0600

    feat: start debug sum of digits of a num codewars problem

---
+++ 7kyu/Debug Sum of Digits of a Number/main.js
@@ -0,0 +1,12 @@
+/*
+
+Debug   function getSumOfDigits that takes positive integer to calculate sum of it's digits. Assume that argument is an integer.
+
+Example
+123  => 6
+223  => 7
+1337 => 14
+
+*/
+
+// solution
