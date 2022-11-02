a81ef2908dca14fceb335bd8469e4bc9d7a15752
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-03 00:09:12 +0600

    feat: complete shorter concat [reverse longer] codewars problem

--- 7kyu/shorter concat [reverse longer]/main.js
+++ 7kyu/shorter concat [reverse longer]/main.js
@@ -10,3 +10,8 @@ If a and b have the same length treat a as the longer producing b+reverse(a)+b
 */
 
 // solution
+
+const shorter_reverse_longer = (a, b) =>
+  a.length > b.length || a.length === b.length
+    ? b + a.split("").reverse("").join("") + b
+    : a + b.split("").reverse("").join("") + a;
