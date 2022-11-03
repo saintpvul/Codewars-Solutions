4f7edcd0e9f030f628e9601e7d3edfb77fbcf5d6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-04 00:05:11 +0600

    feat: complete incrementer codewars problem

---
+++ 7kyu/esreveR/main.js
@@ -0,0 +1,17 @@
+/*
+
+Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
+
+*/
+
+// solution
+
+function reverse(array) {
+  let reversed = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversed.push(array[i]);
+  }
+  return reversed;
+}
