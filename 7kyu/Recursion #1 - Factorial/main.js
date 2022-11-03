870bd13b8d5934174d9f4c2bc7a628d8044c49e9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-03 23:41:50 +0600

    feat: complete recursion #1 - factorial codewars problem

--- 7kyu/Recursion #1 - Factorial/main.js
+++ 7kyu/Recursion #1 - Factorial/main.js
@@ -17,3 +17,9 @@ You have to create the function factorial that receives n and returns n!. You ha
 */
 
 // solution
+
+function factorial(n) {
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  else return n * factorial(n - 1);
+}
