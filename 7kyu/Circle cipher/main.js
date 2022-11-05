69c7ebe6dc07457372d40f1e1f1489641f235ee9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-05 23:59:13 +0600

    feat: complete circle cipher encode codewars problem

--- 7kyu/Circle cipher/main.js
+++ 7kyu/Circle cipher/main.js
@@ -1,8 +1,34 @@
 /*
 
+Imagine a circle. To encode the word codewars, we could split the circle into 8 parts (as codewars has 8 letters):
 
+Then add the letters in a clockwise direction:Then remove the circle:
+If we read the result from left to right, we get csordaew.
 
+Decoding is the same process in reverse. If we decode csordaew, we get codewars.
+
+Examples:
+encode "codewars" -> "csordaew"
+decode "csordaew" -> "codewars"
+encode "white" -> "wehti"
+decode "wehti" -> "white"
 
 */
 
 // solution
+
+function encode(s) {
+  let o = s.slice(0, s.length / 2).split(""),
+    e = s
+      .slice(s.length / 2, s.length)
+      .split("")
+      .reverse(),
+    encoded = [];
+
+  for (let i = 0; i <= o.length; i++) {
+    encoded.push(o[i]);
+    encoded.push(e[i]);
+  }
+
+  return encoded.join("");
+}
