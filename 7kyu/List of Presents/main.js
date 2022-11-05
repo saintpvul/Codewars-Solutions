4fa0f318f61dd86807b1f5b7a1c694af05dc0441
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-05 23:02:26 +0600

    feat: start list of presents codewars problem

---
+++ 7kyu/List of Presents/main.js
@@ -0,0 +1,17 @@
+/*
+
+Task:
+Leo's girlfriend asked him to buy a gift list during his next trip, now he wants to know how many of them will he be able to buy.
+
+Write a function to help Leo out. The first parameter of the function is Leo's budget; the second one is an array containing the price of each gift. You should return an integer representing the maximum amount of gifts Leo can buy.
+
+Example:
+Maximum budget: 20
+Gift List: [13, 2, 4, 6, 1]
+Should return 4.
+
+_ NOTE: All numbers will be integers >= 0, and the array will never be empty. _
+
+*/
+
+// solution
