b2d4a4c468000339a24aebc48a672948a7558fce
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-06 22:39:22 +0600

    feat: complete find the capitals codewars problem

--- 7kyu/Find the capitals/main.js
+++ 7kyu/Find the capitals/main.js
@@ -9,3 +9,14 @@ Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
 */
 
 // solution
+
+var capitals = function (word) {
+  let indexes = [];
+  word.split("");
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
+      indexes.push(i);
+    }
+  }
+  return indexes;
+};
