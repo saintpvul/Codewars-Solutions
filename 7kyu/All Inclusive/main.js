71043c7e9900da8434764cb3e9df7e76b15d5942
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-08 03:43:01 +0600

    feat: complete all inclusive codewars problem

--- 7kyu/All Inclusive/main.js
+++ 7kyu/All Inclusive/main.js
@@ -24,3 +24,12 @@ Ref: https://en.wikipedia.org/wiki/String_(computer_science)#Rotations
 */
 
 // solution
+
+function containAllRots(str, arr) {
+  for (let i = 0; i < str.length; i++) {
+    if (arr.indexOf(str.slice(i) + str.slice(0, i)) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
