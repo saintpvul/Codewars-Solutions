bae864dca0cc7ffdb2d6a8e5f451556d0c879d2d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-13 00:22:12 +0600

    feat: complete count with your fingers codewars problem

--- 7kyu/T.T.T.32 Count with your fingers/main.js
+++ 7kyu/T.T.T.32 Count with your fingers/main.js
@@ -38,3 +38,18 @@ Examples
 */
 
 // solution
+
+function whichFinger(n) {
+  let hand = [
+    "Thumb",
+    "Index finger",
+    "Middle finger",
+    "Ring finger",
+    "Little finger",
+  ];
+  while (n > 5) {
+    n -= 4;
+    hand.reverse();
+  }
+  return hand[n - 1];
+}
