c7cee3bc44ca10c9e6ee63ebf3cbcdc02d89caf2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-12 23:15:44 +0600

    feat: const find the max product codewars problem

--- 7kyu/Find the maximal product/main.js
+++ 7kyu/Find the maximal product/main.js
@@ -29,3 +29,16 @@ So, the function should return 8.
 */
 
 // solution
+
+function findMaxProduct(arr) {
+  const product = [];
+  let sum = 1;
+  for (let k = 0; k < arr.length; k++) {
+    for (let i = k; i < arr.length; i += k + 1) {
+      sum *= arr[i];
+    }
+    product.push(sum);
+    sum = 1;
+  }
+  return Math.max(...product);
+}
