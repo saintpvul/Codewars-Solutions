a379bf9286c96bceb04b9aff092f40c11fac9d1f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-14 22:17:37 +0600

    feat: complete reverse list problem

--- 7kyu/Reverse list/main.js
+++ 7kyu/Reverse list/main.js
@@ -5,3 +5,12 @@ Write reverseList function that simply reverses lists.
 */
 
 // solution
+
+function reverseList(arr) {
+  return arr.reverse();
+  // let reversed = []
+  // for(let i = arr.length; i > 0; i--){
+  //  reversedArr.push(arr[i])
+  //}
+  // return reversed
+}
