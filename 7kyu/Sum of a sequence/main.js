c55a471dbf414ad8205737cdfd7b43fb71bb1eb5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-14 22:13:51 +0600

    feat: complete sum of a sequence codewars problem

--- 7kyu/Sum of a sequence/main.js
+++ 7kyu/Sum of a sequence/main.js
@@ -16,3 +16,21 @@ Examples
 */
 
 // solution
+
+const sequenceSum = (begin, end, step) => {
+  if (begin > end) return 0;
+  let result = 0;
+  for (let i = begin; i <= end; i += step) {
+    result += i;
+  }
+  return result;
+};
+
+const sequenceSum = (begin, end, step) => {
+  if (begin > end) return 0;
+  let result = [];
+  for (let i = begin; i <= end; i += step) {
+    result.push(i);
+  }
+  return result.reduce((s, v) => (s += v), 0);
+};
