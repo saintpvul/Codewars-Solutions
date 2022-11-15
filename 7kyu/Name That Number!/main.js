ab9e562e99c83341124d7d63581b38832f9a2821
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-15 21:22:27 +0600

    feat: start name that number codewars problem

---
+++ 7kyu/Name That Number!/main.js
@@ -0,0 +1,12 @@
+/*
+
+In this kata, you'll be given an integer of range 0 <= x <= 99 and have to return that number spelt out in English. A few examples:
+
+nameThatNumber(4)   // returns "four"
+nameThatNumber(19)  // returns "nineteen"
+nameThatNumber(99)  // returns "ninety nine"
+Words should be separated by only spaces and not hyphens. No need to validate parameters, they will always be in the range [0, 99]. Make sure that the returned String has no leading of trailing spaces. Good luck!
+
+*/
+
+// solution
