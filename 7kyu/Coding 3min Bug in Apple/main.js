cef56094f74bda8a1619bf281f19c49a6eca4c7d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-16 22:03:17 +0600

    feat: complete coding 3min: bug in apple codewars problem

--- 7kyu/Coding 3min Bug in Apple/main.js
+++ 7kyu/Coding 3min Bug in Apple/main.js
@@ -16,3 +16,11 @@ output: Location of "B", [x,y]
 */
 
 // solution
+
+function sc(apple) {
+  for (let i = 0; i < apple.length; i++) {
+    if (apple[i].includes("B")) {
+      return [i, apple[i].indexOf("B")];
+    }
+  }
+}
