fd32d9c8580792d40996069c018a8111735f5148
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-16 22:56:07 +0600

    feat: complete likes vs dislikes codewars problem

--- 7kyu/Likes Vs Dislikes/main.js
+++ 7kyu/Likes Vs Dislikes/main.js
@@ -15,3 +15,29 @@ likeOrDislike([Like,Dislike,Dislike]) => Nothing
 */
 
 // solution
+
+const likeOrDislike = (buttons) =>
+  buttons.reduce((c, b) => (b === c ? Nothing : b), Nothing);
+
+function likeOrDislike(buttons) {
+  let dl = false,
+    l = false;
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons.length === 0) {
+      return "Nothing";
+    } else if (buttons.length === 1) {
+      return buttons[i];
+    } else if (buttons[i] === "Dislike" && !dl) {
+      dl = true;
+      l = false;
+    } else if (buttons[i] === "Like" && !l) {
+      l = true;
+      dl = false;
+    } else if (buttons[i] === "Dislike" && dl) {
+      dl = false;
+    } else if (buttons[i] === "Like" && l) {
+      l = false;
+    }
+  }
+  return dl ? "Dislike" : l ? "Like" : "Nothing";
+}
