b5c7afe8471202bb0f02d3a84e18b7bc83110457
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-17 19:57:43 +0600

    feat: start tidy number (special nums series #9) codewars problem

---
+++ 7kyu/Tidy Number (Special Numbers Series #9)/main.js
@@ -0,0 +1,18 @@
+/*
+
+Definition
+A Tidy number is a number whose digits are in non-decreasing order.
+
+Task
+Given a number, Find if it is Tidy or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+Number passed is always Positive .
+
+Return the result as a Boolean
+
+*/
+
+// solution
