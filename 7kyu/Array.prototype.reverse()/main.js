d605a5336a4f078667b4eea956d23c2726df0882
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-18 20:50:02 +0600

    feat: start array.prototype.reverse() codewars problem

---
+++ 7kyu/Array.prototype.reverse()/main.js
@@ -0,0 +1,16 @@
+/*
+
+The Array's reverse() method has gone missing! Re-write it, quick-sharp!
+
+When this method is called, it reverses the order of the items in the original array. Then then it returns that same, original array. No new arrays should need to be created to pass this kata.
+
+Here's an example:
+
+var input = [1, 2, 3, 4];
+input.reverse(); // == [4, 3, 2, 1]  // returned by .reverse()
+input;           // == [4, 3, 2, 1]  // items reordered in the original array
+
+
+*/
+
+// solution
