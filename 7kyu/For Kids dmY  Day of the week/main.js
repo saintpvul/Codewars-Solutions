c4355fef88030c0a87605fb4d56c33ee9f116ca3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-18 21:43:53 +0600

    feat: start for kids dmy day of the week codewars problem

---
+++ 7kyu/For Kids dmY  Day of the week/main.js
@@ -0,0 +1,10 @@
+/*
+
+#~For Kids Challenges~#
+Your task is easy, write a function that takes an date in format d/m/Y(String) and return what day of the week it was(String).
+Example: "21/01/2017" -> "Saturday", "31/03/2017" -> "Friday"
+Have fun!
+
+*/
+
+// solution
