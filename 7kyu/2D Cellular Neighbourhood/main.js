d71b484d23c6e66f9dc87b00a9f756f897371c7e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-19 21:07:49 +0600

    feat: complete 2d cellular neighbourhood codewars problem

--- 7kyu/2D Cellular Neighbourhood/main.js
+++ 7kyu/2D Cellular Neighbourhood/main.js
@@ -40,3 +40,29 @@ If you like puzzles take a look at Rubik's cube
 */
 
 // solution
+
+function get_neighbourhood(type, arr, [y, x]) {
+  if (!arr[y] || arr[y][x] === undefined) return [];
+  return (
+    type === "moore"
+      ? [
+          [y - 1, x - 1],
+          [y - 1, x],
+          [y - 1, x + 1],
+          [y, x - 1],
+          [y, x + 1],
+          [y + 1, x - 1],
+          [y + 1, x],
+          [y + 1, x + 1],
+        ]
+      : [
+          [y, x - 1],
+          [y, x + 1],
+          [y - 1, x],
+          [y + 1, x],
+        ]
+  )
+    .filter(([y, x]) => arr[y] && arr[y][x] !== undefined)
+    .map(([y, x]) => arr[y][x])
+    .sort((a, b) => a - b);
+}
