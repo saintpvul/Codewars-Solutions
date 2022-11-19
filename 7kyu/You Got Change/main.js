4c17e0767e07f7a775b69e307c63326839018a95
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-19 21:04:56 +0600

    feat: complete you got change? codewars problem

--- 7kyu/You Got Change/main.js
+++ 7kyu/You Got Change/main.js
@@ -24,3 +24,32 @@ giveChange(217) // => [2,1,1,0,0,2]
 */
 
 // solution
+
+function giveChange(amount) {
+  let change = [0, 0, 0, 0, 0, 0];
+  while (amount >= 100) {
+    change[5]++;
+    amount -= 100;
+  }
+  while (amount >= 50) {
+    change[4]++;
+    amount -= 50;
+  }
+  while (amount >= 20) {
+    change[3]++;
+    amount -= 20;
+  }
+  while (amount >= 10) {
+    change[2]++;
+    amount -= 10;
+  }
+  while (amount >= 5) {
+    change[1]++;
+    amount -= 5;
+  }
+  while (amount >= 1) {
+    change[0]++;
+    amount--;
+  }
+  return change;
+}
