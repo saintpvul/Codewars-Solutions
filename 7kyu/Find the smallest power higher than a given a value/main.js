e3bf8262ea83c9022fd9a3e2c98a968d084e5015
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-20 22:47:10 +0600

    feat: complete find the smallest pwer higher than a given a value codewars problem

--- 7kyu/Find the smallest power higher than a given a value/main.js
+++ 7kyu/Find the smallest power higher than a given a value/main.js
@@ -20,3 +20,11 @@ Happy coding!!
 */
 
 // solution
+
+function findNextPower(val, pow_) {
+  for (let i = 1; i <= val; i++) {
+    if (Math.pow(i, pow_) > val) {
+      return Math.pow(i, pow_);
+    }
+  }
+}
