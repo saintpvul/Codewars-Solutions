67fdc5ae23480630129c97808136165fc8641a45
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-21 21:04:17 +0600

    feat: start next featured number higher than a given value codewars problem

---
+++ 7kyu/Next Featured Number Higher than a Given Value/main.js
@@ -0,0 +1,25 @@
+/*
+
+Make a function that receives a value, val and outputs the smallest higher number than the given value, and this number belong to a set of positive integers that have the following properties:
+
+their digits occur only once
+
+they are odd
+
+they are multiple of three
+
+nextNumb(12) == 15
+
+nextNumb(13) == 15
+
+nextNumb(99) == 105
+
+nextNumb(999999) == 1023459
+
+nextNumber(9999999999) == "There is no possible number that
+fulfills those requirements"
+Enjoy the kata!!
+
+*/
+
+// solution
