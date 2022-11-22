b7cf6c59054884a44dc72b6f187733800c412442
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-22 23:59:16 +0600

    feat: complete factorial codewars problem

--- 7kyu/factorial.error/main.js
+++ 7kyu/factorial.error/main.js
@@ -9,3 +9,11 @@ More details about factorial can be found here.
 */
 
 // solution
+
+function factorial(n) {
+  if (n < 0 || n > 12) {
+    throw new RangeError("The argument must be between 0 and 12.");
+  } else {
+    return n > 1 ? factorial(n - 1) * n : 1;
+  }
+}
