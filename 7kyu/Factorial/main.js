9bfa935242c14af46a59dc14b7a2d6e3a26a66f8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-22 23:24:11 +0600

    feat: complete factorial codewars problem

--- 7kyu/Factorial/main.js
+++ 7kyu/Factorial/main.js
@@ -7,3 +7,12 @@ https://en.wikipedia.org/wiki/Factorial
 */
 
 // solution
+
+const factorial = (n) => (n ? factorial(n - 1) * n : 1);
+
+function factorial(n) {
+  let f = [];
+  if (n == 0 || n == 1) return 1;
+  if (f[n] > 0) return f[n];
+  return (f[n] = factorial(n - 1) * n);
+}
