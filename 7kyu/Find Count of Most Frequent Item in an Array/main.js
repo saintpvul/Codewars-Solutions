bf4e3d4b1b8a1842bb0f7270fbac9ada02c64d72
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-22 23:37:49 +0600

    feat: complete find count of most frequent item in array codewars problem

--- 7kyu/Find Count of Most Frequent Item in an Array/main.js
+++ 7kyu/Find Count of Most Frequent Item in an Array/main.js
@@ -10,3 +10,31 @@ The most frequent number in the array is -1 and it occurs 5 times.
 */
 
 // solution
+
+function mostFrequentItemCount(collection) {
+  if (collection.length < 1) return 0;
+  let count = 0,
+    temp = 0;
+  for (let i = 0; i < collection.length; i++) {
+    for (let j = 0; j < collection.length; j++) {
+      if (collection[j] === collection[i]) {
+        temp++;
+      }
+    }
+    if (temp > count) {
+      count = temp;
+    }
+    temp = 0;
+  }
+  return count;
+}
+
+function mostFrequentItemCount(collection) {
+  if (collection.length < 1) return 0;
+
+  let map = {};
+  for (let num of collection) {
+    map[num] = (map[num] || 0) + 1;
+  }
+  return Math.max(...Object.values(map));
+}
