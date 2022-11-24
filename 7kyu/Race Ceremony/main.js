9aca11b8d82e40a112ca1b3e56cfddfbbe007348
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-25 01:55:54 +0600

    feat: complete race ceremony codewars problem

--- 7kyu/Race Ceremony/main.js
+++ 7kyu/Race Ceremony/main.js
@@ -20,3 +20,20 @@ Examples (input -> output)
 */
 
 // solution
+
+function racePodium(blocks) {
+  let first = Math.ceil(blocks / 3) + 1;
+  let second = first - 1;
+  let third = blocks - first - second;
+  if (third == 0) {
+    third = 1;
+    second -= 1;
+  }
+  return [second, first, third];
+}
+
+const racePodium = (blocks) =>
+  ((second) =>
+    blocks === 7 ? [2, 4, 1] : [second, second + 1, blocks - second * 2 - 1])(
+    (blocks / 3 + 2 / 3) | 0
+  );
