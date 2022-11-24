066c16db0e92117868a326b7fa8d57823902efb3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-24 23:18:23 +0600

    feat: complete rock paper scissors lizard spock codewars problem

--- 7kyu/Rock Paper Scissors Lizard Spock/main.js
+++ 7kyu/Rock Paper Scissors Lizard Spock/main.js
@@ -21,3 +21,43 @@ Values will be given as one of "rock", "paper", "scissors", "lizard", "spock".
 */
 
 // solution
+
+function rpsls(pl1, pl2) {
+  let match = {
+    scissors: ["paper", "lizard"],
+    paper: ["rock", "spock"],
+    rock: ["lizard", "scissors"],
+    lizard: ["spock", "paper"],
+    spock: ["scissors", "rock"],
+  };
+
+  return match[pl1].indexOf(pl2) >= 0
+    ? "Player 1 Won!"
+    : match[pl2].indexOf(pl1) >= 0
+    ? "Player 2 Won!"
+    : "Draw!";
+}
+
+function rpsls(pl1, pl2) {
+  let match = [
+      "scissors cuts paper",
+      "paper covers rock",
+      "rock crushes lizard",
+      "lizard poisons spock",
+      "spock smashes scissors",
+      "scissors decapitates lizard",
+      "lizard eats paper",
+      "paper disproves spock",
+      "spock vaporizes rock",
+      "rock crushes scissors",
+    ].map((i) => i.split(" ")),
+    win = "Draw!";
+  match.map((v) =>
+    v[0] === pl1 && v[2] === pl2
+      ? (win = "Player 1")
+      : v[0] === pl2 && v[2] === pl1
+      ? (win = "Player 2")
+      : v
+  );
+  return win !== "Draw!" ? `${win} Won!` : win;
+}
