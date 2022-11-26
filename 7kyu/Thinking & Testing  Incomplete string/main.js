d2f2c24dabfc8ebc9d45c09f3d5f7fd36533715f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-26 19:46:58 +0600

    feat: complete thininfg & testing incomplete string codewars problem

--- 7kyu/Thinking & Testing  Incomplete string/main.js
+++ 7kyu/Thinking & Testing  Incomplete string/main.js
@@ -11,3 +11,12 @@ Look at result of testcase, guess the code!
 */
 
 // solution
+
+function testit(s) {
+  //return s?
+  //return s.substr(0,1) ?
+  //return s.substr(0,s.length/2) ?
+  return s.replace(/../g, (s) =>
+    String.fromCharCode((s.charCodeAt(0) + s.charCodeAt(1)) / 2)
+  );
+}
