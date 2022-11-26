8ad040d9019e19d5ddfbe4ad4d95962bf6266e78
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-26 20:43:12 +0600

    feat: complete thinking & testing math of middle school codewars problem

--- 7kyu/Thinking & Testing  Math of Middle school/main.js
+++ 7kyu/Thinking & Testing  Math of Middle school/main.js
@@ -11,3 +11,12 @@ Look at result of testcase, guess the code!
 */
 
 // solution
+
+function testit(a, b) {
+  return [
+    a[0] * b[0] + a[1] * b[2],
+    a[0] * b[1] + a[1] * b[3],
+    a[2] * b[0] + a[3] * b[2],
+    a[2] * b[1] + a[3] * b[3],
+  ];
+}
