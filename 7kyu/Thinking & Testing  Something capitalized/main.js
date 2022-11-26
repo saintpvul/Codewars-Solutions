8b09c6243054c7dce1b1221e901524d8245c14fa
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-26 20:10:43 +0600

    feat: complete thinking & testing something capitalized codewars problem

--- 7kyu/Thinking & Testing  Something capitalized/main.js
+++ 7kyu/Thinking & Testing  Something capitalized/main.js
@@ -11,3 +11,10 @@ Look at result of testcase, guess the code!
 */
 
 // solution
+
+function testit(s) {
+  return s
+    .split(" ")
+    .map((c) => c.slice(0, c.length - 1) + c.slice(c.length - 1).toUpperCase())
+    .join(" ");
+}
