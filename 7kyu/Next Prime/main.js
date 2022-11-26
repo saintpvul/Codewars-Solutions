025234377fc523046ee029b3f96578854c0e61be
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-26 19:19:42 +0600

    feat: complete next prime codewars problem

--- 7kyu/Next Prime/main.js
+++ 7kyu/Next Prime/main.js
@@ -13,3 +13,13 @@ Examples
 */
 
 // solution
+
+function nextPrime(n) {
+  if (++n < 2) return 2;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return nextPrime(n);
+    }
+  }
+  return n;
+}
