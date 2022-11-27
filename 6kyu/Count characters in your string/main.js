45ce22c5ada71e7ad0929b0fcff73bed6f43ee95
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-27 22:40:36 +0600

    feat: complete count characters in your string codewars problem

--- 6kyu/Count characters in your string/main.js
+++ 6kyu/Count characters in your string/main.js
@@ -7,3 +7,12 @@ What if the string is empty? Then the result should be empty object literal, {}.
 */
 
 // solution
+
+function count(string) {
+  let output = {};
+  string = string.toLowerCase().replace(/[^\w]/g);
+  for (let char of string) {
+    output[char] = output[char] + 1 || 1;
+  }
+  return output;
+}
