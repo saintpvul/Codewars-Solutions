998691feb9a6a242da6e90e2ff25c50e194bb67d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-27 23:05:24 +0600

    feat: start form the minimum codewars problem

---
+++ 7kyu/Form The Minimum/main.js
@@ -0,0 +1,11 @@
+/*
+
+Task
+Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+Notes:
+Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+
+*/
+
+// solution
