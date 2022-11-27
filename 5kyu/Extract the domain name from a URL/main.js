30d893c42fb2a27e566fa58bad5f8c48719f4eba
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-27 21:55:56 +0600

    feat: complete extract the domain name from a url codewars problem

--- 5kyu/Extract the domain name from a URL/main.js
+++ 5kyu/Extract the domain name from a URL/main.js
@@ -10,3 +10,11 @@ Write a function that when given a URL as a string, parses out just the domain n
 */
 
 // solution
+
+function domainName(url) {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .split(".")[0];
+}
