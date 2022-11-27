36b716d0da508f0c12bcce5fb2bc783f1c558362
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-27 22:58:02 +0600

    feat: start write number in expanded form - part 2 codewars problem

---
+++ 6kyu/Write Number in Expanded Form - Part 2/main.js
@@ -0,0 +1,18 @@
+/*
+
+This is version 2 of my 'Write Number in Exanded Form' Kata.
+
+You will be given a number and you will need to return it as a string in expanded form :
+
+expanded form illustration
+
+For example:
+
+expanded_from(807.304); // Should return "800 + 7 + 3/10 + 4/1000"
+expanded_from(1.24); // should return "1 + 2/10 + 4/100"
+expanded_from(7.304); // should return "7 + 3/10 + 4/1000"
+expanded_from(0.04); // should return "4/100"
+
+*/
+
+// solution
