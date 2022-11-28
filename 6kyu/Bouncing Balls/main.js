4904fb6629287b1d7c70b936a52c26b3a5a954cb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-28 23:21:50 +0600

    feat: complete bouncing balls codewars problem

--- 6kyu/Bouncing Balls/main.js
+++ 6kyu/Bouncing Balls/main.js
@@ -28,3 +28,12 @@ Examples:
 */
 
 // solution
+
+function bouncingBall(h, bounce, window) {
+  let count = -1;
+  if (bounce > 0 && bounce < 1)
+    while (h > window) {
+      (count += 2), (h *= bounce);
+    }
+  return count;
+}
