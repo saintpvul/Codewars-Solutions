6aec65dc73e5f1baa57eee6fe8d26d4be258661b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-29 22:18:56 +0600

    feat: complete number pairs codewars problem

--- 7kyu/Number Pairs/main.js
+++ 7kyu/Number Pairs/main.js
@@ -14,3 +14,17 @@ getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88]
 */
 
 // solution
+
+function getLargerNumbers(a, b) {
+  let out = [];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] >= b[i]) {
+      out.push(a[i]);
+    } else {
+      out.push(b[i]);
+    }
+  }
+  return out;
+}
+
+const getLargerNumbers = (a, b) => a.map((v, i) => Math.max(v, b[i]));
