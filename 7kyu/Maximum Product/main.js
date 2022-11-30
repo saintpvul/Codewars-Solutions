ca8877bd483801896697f5cb5195c4906c76d757
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-11-30 22:59:23 +0600

    feat: start maximum product codewars problem

---
+++ 7kyu/Maximum Product/main.js
@@ -0,0 +1,16 @@
+/*
+
+Task
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+Notes
+Array/list size is at least 2.
+
+Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+
+
+
+*/
+
+// solution
