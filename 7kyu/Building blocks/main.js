0b24f2f7864d7474db7a867be67dc28c925938bb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-01 23:54:05 +0600

    feat: complete building blocks codewars problem

--- 7kyu/Building blocks/main.js
+++ 7kyu/Building blocks/main.js
@@ -35,3 +35,26 @@ Any feedback would be much appreciated
 */
 
 // solution
+
+class Block {
+  constructor(data) {
+    [this.w, this.l, this.h] = data;
+  }
+  getWidth() {
+    return this.w;
+  }
+  getLength() {
+    return this.l;
+  }
+  getHeight() {
+    return this.h;
+  }
+  getVolume() {
+    let { l, w, h } = this;
+    return w * l * h;
+  }
+  getSurfaceArea() {
+    let { l, w, h } = this;
+    return 2 * (l * w + l * h + w * h);
+  }
+}
