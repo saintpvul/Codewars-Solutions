2d810bf5aaa358b2acd967d9ddcd9a7f30039851
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-03 21:49:35 +0600

    feat: complete adding arrays codewars problem

--- 7kyu/Adding Arrays/main.js
+++ 7kyu/Adding Arrays/main.js
@@ -39,3 +39,14 @@ arrAdder([
 */
 
 // solution
+
+function arrAdder(arr) {
+  let result = [];
+  for (let i = 0; i < arr[0].length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      result.push(arr[j][i]);
+    }
+    result.push(" ");
+  }
+  return result.join("").trim();
+}
