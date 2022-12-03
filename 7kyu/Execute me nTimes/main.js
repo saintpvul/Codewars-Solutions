072d550c3769f3ff92bc119285f57027de68394d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-04 01:51:15 +0600

    feat: start execute me ntimes problem

---
+++ 7kyu/Execute me nTimes/main.js
@@ -0,0 +1,11 @@
+/*
+
+You're given a procedure / method that executes a passed action n times, but it does so in a purely sequential manner. If you inspect the sample tests, you'll find that the action takes about 1 second to complete, and this action is repeated 20 times which results in a timeout.
+
+Find a way to execute the action n times in parallel, such that the sample tests complete under the 12 second time limit imposed by Codewars. The submission tests will also test for similar scenarios.
+
+Hint: If you're not sure where to start, read up on async/await.
+
+*/
+
+// solution
