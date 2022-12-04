eb4031a151e6a18d7367ce47c68a51732593efac
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-05 00:00:59 +0600

    feat: complete fix string case codewars problem

--- 7kyu/Fix string case/main.js
+++ 7kyu/Fix string case/main.js
@@ -14,3 +14,13 @@ More examples in test cases. Good luck!
 */
 
 // solution
+
+function solve(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toUpperCase()) {
+      count++;
+    }
+  }
+  return count > s.length / 2 ? s.toUpperCase() : s.toLowerCase();
+}
