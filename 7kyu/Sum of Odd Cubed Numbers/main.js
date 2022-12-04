dcebee34287a8a14c96941742095dac6b40dfd0d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-05 02:19:27 +0600

    feat: start sum of odd cubed numbers codewars problem

---
+++ 7kyu/Sum of Odd Cubed Numbers/main.js
@@ -0,0 +1,7 @@
+/*
+
+Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
+
+*/
+
+// solution
