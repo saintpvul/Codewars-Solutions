9965d4933461291b8daf36b8d083f6b692eaa137
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-05 02:54:28 +0600

    feat: complete most digits codewars problem

--- 7kyu/Most digits/main.js
+++ 7kyu/Most digits/main.js
@@ -7,3 +7,19 @@ If two numbers in the argument array have the same number of digits, return the 
 */
 
 // solution
+
+function findLongest(arr) {
+  let ndx = 0,
+    l = 0,
+    check = [...arr].map((e) => e.toString());
+  for (let i = 0; i < check.length; i++) {
+    if (check[i].length > l) {
+      l = check[i].length;
+      ndx = i;
+    }
+  }
+  return arr[ndx];
+}
+
+const findLongest = (arr) =>
+  arr.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
