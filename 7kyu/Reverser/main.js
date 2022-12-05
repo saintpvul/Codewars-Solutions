a5e519559961136db146fba892bde6886dfe51db
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-06 01:49:45 +0600

    feat: complete reverser codewars problem

--- 7kyu/Reverser/main.js
+++ 7kyu/Reverser/main.js
@@ -14,3 +14,12 @@ const forbidden = "
 */
 
 // solution
+
+function reverse(n) {
+  let rev = 0;
+  while (n > 0) {
+    rev = rev * 10 + (n % 10);
+    n = Math.floor(n / 10);
+  }
+  return rev;
+}
