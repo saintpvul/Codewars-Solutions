a19582e729f50fc6792952d421ca63bd3db068e3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-05 20:25:41 +0600

    feat: start divide and conquer codewars problem

---
+++ 7kyu/Divide and Conquer/main.js
@@ -0,0 +1,9 @@
+/*
+
+Given a mixed array of number and string representations of integers, add up the non-string integers and subtract this from the total of the string integers.
+
+Return as a number.
+
+*/
+
+// solution
