ec262beb8dc28b11501a885b683a1a9ab29587b1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-06 01:45:22 +0600

    feat: start reverser codewars problem

---
+++ 7kyu/Reverser/main.js
@@ -0,0 +1,16 @@
+/*
+
+Impliment the reverse function, which takes in input n and reverses it. For instance, reverse(123) should return 321. You should do this without converting the inputted number into a string.
+
+// Please do not use
+const forbidden = "
+                  '
+                  `
+                  string
+                  fixed
+                  precision
+                  .keys
+
+*/
+
+// solution
