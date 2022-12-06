790bafdc242b1cf5fc8cb9d3c306f40c97db3070
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-07 03:01:38 +0600

    feat: complete toleetspeak codewars problem

--- 7kyu/ToLeetSpeak/main.js
+++ 7kyu/ToLeetSpeak/main.js
@@ -43,3 +43,38 @@ In this kata we use a simple LeetSpeak dialect. Use this alphabet:
 */
 
 // solution
+
+function toLeetSpeak(str) {
+  let leet = {
+    A: "@",
+    B: "8",
+    C: "(",
+    D: "D",
+    E: "3",
+    F: "F",
+    G: "6",
+    H: "#",
+    I: "!",
+    J: "J",
+    K: "K",
+    L: "1",
+    M: "M",
+    N: "N",
+    O: "0",
+    P: "P",
+    Q: "Q",
+    R: "R",
+    S: "$",
+    T: "7",
+    U: "U",
+    V: "V",
+    W: "W",
+    X: "X",
+    Y: "Y",
+    Z: "2",
+  };
+  return str
+    .split("")
+    .map((c) => (leet[c] ? leet[c] : c))
+    .join("");
+}
