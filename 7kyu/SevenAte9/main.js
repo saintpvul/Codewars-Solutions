24e9628b68cd81cd9ebec23d95c4560979b11c85
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-07 02:47:44 +0600

    feat: complete sevenate9 codewars problem

--- 7kyu/SevenAte9/main.js
+++ 7kyu/SevenAte9/main.js
@@ -8,3 +8,15 @@ Write a function that removes every lone 9 that is inbetween 7s.
 */
 
 // solution
+
+function sevenAte9(str) {
+  let res = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == 9 && str[i - 1] == 7 && str[i + 1] == 7) {
+      continue;
+    } else {
+      res.push(str[i]);
+    }
+  }
+  return res.join("");
+}
