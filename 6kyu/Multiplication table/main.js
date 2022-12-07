f81f96e9e5c10f6996ff222eada6ba5eeb0c88cd
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-07 20:52:45 +0600

    feat: complete multiplication table codewars problem

--- 6kyu/Multiplication table/main.js
+++ 6kyu/Multiplication table/main.js
@@ -12,3 +12,14 @@ for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
 */
 
 // solution
+
+const multiplicationTable = (size) => {
+  let output = [];
+  for (let i = 1; i <= size; i++) {
+    let num = Array(size)
+      .fill(1)
+      .map((e, ndx) => (e + ndx) * i);
+    output.push(num);
+  }
+  return output;
+};
