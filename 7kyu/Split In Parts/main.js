f2c728adb3fc9ad412ac74b687cfea6835ae10ed
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-09 01:59:26 +0600

    feat: complete split in parts codewars problem

--- 7kyu/Split In Parts/main.js
+++ 7kyu/Split In Parts/main.js
@@ -19,3 +19,11 @@ Size is always positive
 */
 
 // solution
+
+var splitInParts = function (s, partLength) {
+  let result = [];
+  for (let i = 0; i < s.length; i += partLength) {
+    result.push(s.slice(i, i + partLength));
+  }
+  return result.join(" ");
+};
