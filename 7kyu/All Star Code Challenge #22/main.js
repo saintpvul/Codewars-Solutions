fbc83008f30649f09bee6a73f7fa799fc2dad05e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-08 23:59:31 +0600

    feat: complete the officer ii - boredom score codewars problem

--- 7kyu/All Star Code Challenge #22/main.js
+++ 7kyu/All Star Code Challenge #22/main.js
@@ -22,3 +22,13 @@ For example:
 */
 
 // solution
+
+function toTime(seconds) {
+  let h = 0;
+  while (seconds >= 3600) {
+    h += 1;
+    seconds -= 3600;
+  }
+  let sec = Math.trunc(seconds / 60);
+  return `${h} hour(s) and ${sec} minute(s)`;
+}
