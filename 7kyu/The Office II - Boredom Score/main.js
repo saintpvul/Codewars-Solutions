e9dbb643e9af2d78461dbc075efc10a504bbe367
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-08 23:56:28 +0600

    feat: complete the officer ii - boredom score codewars problem

--- 7kyu/The Office II - Boredom Score/main.js
+++ 7kyu/The Office II - Boredom Score/main.js
@@ -31,3 +31,25 @@ The Office V - Find a Chair
 */
 
 // solution
+
+function boredom(staff) {
+  const arr = Object.values(staff).reduce((a, b) => a + obj[b], 0);
+  return arr <= 80
+    ? "kill me now"
+    : arr >= 100
+    ? "party time!!"
+    : "i can handle this";
+}
+
+const obj = {
+  accounts: 1,
+  finance: 2,
+  canteen: 10,
+  regulation: 3,
+  trading: 6,
+  change: 6,
+  IS: 8,
+  retail: 5,
+  cleaning: 4,
+  "pissing about": 25,
+};
