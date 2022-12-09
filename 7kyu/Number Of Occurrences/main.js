b289211bd93ce8c0fc3789cfaa1da5ed92721702
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-10 03:29:15 +0600

    feat: start number of occurences codewars problem

---
+++ 7kyu/Number Of Occurrences/main.js
@@ -0,0 +1,14 @@
+/*
+
+Write a function that returns the number of occurrences of an element in an array.
+
+Examples
+var arr = [0, 1, 2, 2, 3];
+arr.numberOfOccurrences(0) === 1;
+arr.numberOfOccurrences(4) === 0;
+arr.numberOfOccurrences(2) === 2;
+arr.numberOfOccurrences("a") === 0;
+
+*/
+
+// solution
