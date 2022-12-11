6d229e7297d565dab287a8c768f959a1e9e8fcc5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-11 22:10:59 +0600

    feat: complete consecutive itemscodewars problem

--- 7kyu/Consecutive items/main.js
+++ 7kyu/Consecutive items/main.js
@@ -7,3 +7,13 @@ It is guaranteed that a and b are both present in arr.
 */
 
 // solution
+
+const consecutive = (arr, a, b) =>
+  a < b
+    ? arr.indexOf(a) === arr.indexOf(b) - 1 ||
+      arr.indexOf(a) === arr.indexOf(b) + 1
+    : arr.indexOf(b) === arr.indexOf(a) - 1 ||
+      arr.indexOf(b) === arr.indexOf(a) + 1;
+
+const consecutive = (arr, a, b) =>
+  Math.abs(arr.indexOf(a) - arr.indexOf(b)) === 1;
