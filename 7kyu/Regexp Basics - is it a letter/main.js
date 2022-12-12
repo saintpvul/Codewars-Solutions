c8f7526ccbd41b2e5730e2323bc1fe701743bc41
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-12 18:37:09 +0600

    feat: complete regexp basics - is it a letter codewars problem

--- 7kyu/Regexp Basics - is it a letter/main.js
+++ 7kyu/Regexp Basics - is it a letter/main.js
@@ -5,3 +5,10 @@ Complete the code which should return true if the given object is a single ASCII
 */
 
 // solution
+
+String.prototype.isLetter = function () {
+  return this.length != 1
+    ? false
+    : this.toLowerCase().charCodeAt() >= 97 &&
+        this.toLowerCase().charCodeAt() <= 122;
+};
