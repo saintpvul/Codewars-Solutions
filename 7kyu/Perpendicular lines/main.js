84d31a620aab63980562568113484e242417479a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-15 00:02:51 +0600

    feat: start perpendicular lines codewars problem

---
+++ 7kyu/Perpendicular lines/main.js
@@ -0,0 +1,12 @@
+/*
+
+You are given an input (n) which represents the amount of lines you are given, your job is to figure out what is the maximum amount of perpendicular bisectors you can make using these lines.
+
+Note: A perpendicular bisector is one that forms a 90 degree angle
+
+n will always be greater than or equal to 0
+Examples with 3 and 4 lines
+
+*/
+
+// solution
