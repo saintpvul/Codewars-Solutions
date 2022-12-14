1282973cc692427521d5044f62f319530891e166
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-15 00:03:22 +0600

    feat: complete perpendicular lines codewars problem

--- 7kyu/Perpendicular lines/main.js
+++ 7kyu/Perpendicular lines/main.js
@@ -10,3 +10,9 @@ Examples with 3 and 4 lines
 */
 
 // solution
+
+// You are given an input (n) which represents the amount of lines you are given.
+// Your job is to figure out what is the maximum amount of perpendicular bisectors you can make using these lines.
+function maxBisectors(n) {
+  return Math.round((n * n) / 4);
+}
