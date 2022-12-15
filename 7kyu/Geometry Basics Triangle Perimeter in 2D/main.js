ba47b1d66850a1912a1a9fb745e999e7bd755a47
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-15 23:47:44 +0600

    feat: complete how many consecutive numbers are needed codewars problem

--- 7kyu/Geometry Basics Triangle Perimeter in 2D/main.js
+++ 7kyu/Geometry Basics Triangle Perimeter in 2D/main.js
@@ -11,3 +11,11 @@ Tests round answers to 6 decimal places.
 */
 
 // solution
+
+function trianglePerimeter(triangle) {
+  return (
+    Math.hypot(triangle.a.x - triangle.b.x, triangle.a.y - triangle.b.y) +
+    Math.hypot(triangle.a.x - triangle.c.x, triangle.a.y - triangle.c.y) +
+    Math.hypot(triangle.b.x - triangle.c.x, triangle.b.y - triangle.c.y)
+  );
+}
