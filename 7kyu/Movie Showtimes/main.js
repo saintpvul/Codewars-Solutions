e1ac1ffb57329596d898f8e77e5a0aa99218ed48
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-16 01:43:46 +0600

    feat: complete movie showtimes codewars problem

--- 7kyu/Movie Showtimes/main.js
+++ 7kyu/Movie Showtimes/main.js
@@ -30,3 +30,12 @@ Good luck!
 */
 
 // solution
+
+function movie_times(open, close, length) {
+  let sessions = [];
+  if (open > close) close += 24;
+  for (let i = open * 60; i + length <= close * 60; i += length + 15) {
+    sessions.push([Math.floor(i / 60) % 24, i % 60]);
+  }
+  return sessions;
+}
