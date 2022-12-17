7ebcc2fe6ac9307bc42a377e4a1b12974cbac079
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-18 01:52:12 +0600

    feat: complete string merge! codewars problem

--- 7kyu/String Merge!/main.js
+++ 7kyu/String Merge!/main.js
@@ -11,3 +11,8 @@ Examples
 */
 
 // solution
+
+const stringMerge = (string1, string2, letter) =>
+  `${string1.slice(0, string1.indexOf(letter))}${string2.slice(
+    string2.indexOf(letter)
+  )}`;
