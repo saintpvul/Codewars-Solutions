b81600dd0fa97adb858cb922cf23fb1053195de5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-17 22:18:43 +0600

    feat: complete transform to prime codewars problem

--- 6kyu/Transform To Prime/main.js
+++ 6kyu/Transform To Prime/main.js
@@ -25,3 +25,19 @@ Since , the sum of the list's elements equal to (189) , the minimum number to be
 */
 
 // solution
+
+function minimumNumber(numbers) {
+  let sum = numbers.reduce((a, b) => a + b, 0);
+  for (let i = 0; ; i++) {
+    if (isPrime(sum + i)) {
+      return i;
+    }
+  }
+}
+function isPrime(n) {
+  let rt = Math.sqrt(n);
+  for (let i = 2; i <= rt; i++) {
+    if (n % i === 0) return false;
+  }
+  return n !== 1;
+}
