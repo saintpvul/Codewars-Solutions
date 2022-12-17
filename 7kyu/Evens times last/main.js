e881b67ef30f01464437872690c477447fc917c5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-17 22:50:53 +0600

    feat: complete evens times last codewars problem

--- 7kyu/Evens times last/main.js
+++ 7kyu/Evens times last/main.js
@@ -9,3 +9,12 @@ If the sequence is empty, you should return 0.
 */
 
 // solution
+function evenLast(numbers) {
+  let evens = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (i % 2 === 0) {
+      evens.push(numbers[i]);
+    }
+  }
+  return evens.reduce((s, v) => (s += v), 0) * numbers[numbers.length - 1] || 0;
+}
