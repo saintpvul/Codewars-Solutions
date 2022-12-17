8045e57967bf4fbe80a1482b5f83ab9e007fafe5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-18 01:47:16 +0600

    feat: complete sort the odd codewars problem

--- 6kyu/Sort the odd/main.js
+++ 6kyu/Sort the odd/main.js
@@ -11,3 +11,32 @@ Examples
 */
 
 // solution
+
+// slow solution
+function sortArray(array) {
+  let res = [],
+    odds = [],
+    evens = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2) {
+      odds.push(array[i]);
+    } else evens.push(array[i]);
+  }
+  odds = odds.sort((a, b) => a - b);
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2) {
+      res.push(odds[0]);
+      odds.shift();
+    } else {
+      res.push(evens[0]);
+      evens.shift();
+    }
+  }
+  return res;
+}
+
+// bit faster solution
+const sortArray = (a) => {
+  let o = a.filter((v) => v % 2).sort((a, b) => a - b);
+  return a.map((v) => (v % 2 ? o.shift() : v));
+};
