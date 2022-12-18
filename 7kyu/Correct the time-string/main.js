7ce37504c20b97c381dba6a6fa4bf21f10364359
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-18 16:55:38 +0600

    feat: complete correct the time- strin codewars problem

--- 7kyu/Correct the time-string/main.js
+++ 7kyu/Correct the time-string/main.js
@@ -19,3 +19,9 @@ I have created other katas. Have a look if you like coding and challenges.
 */
 
 // solution
+
+function timeCorrect(timestring) {
+  if (!timestring) return timestring;
+  if (!/^(\d\d:){2}\d\d$/.test(timestring)) return null;
+  return new Date(0, 0, 0, ...timestring.split(":")).toString().slice(16, 24);
+}
