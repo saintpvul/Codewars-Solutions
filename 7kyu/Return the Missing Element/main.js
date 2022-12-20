d6378d9fa2ba90c1510c95fe3d42b6be96fc38c3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-21 00:00:54 +0600

    feat: complete return the missing element codewars problem

--- 7kyu/Return the Missing Element/main.js
+++ 7kyu/Return the Missing Element/main.js
@@ -15,10 +15,10 @@ Examples:
 // solution
 
 function getMissingElement(superImportantArray) {
+  superImportantArray = superImportantArray.sort((a, b) => a - b);
   let count = 0;
-  let arr = superImportantArray.sort((a, b) => a - b);
-  for (let i = 0; i <= arr.length; i++) {
-    if (i == arr[i]) count++;
+  for (let i = 0; i <= superImportantArray.length; i++) {
+    if (i === superImportantArray[i]) count++;
   }
   return count;
 }
