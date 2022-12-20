549daa0ae988248a0072b3e086131ed7eab5b592
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-20 23:31:55 +0600

    feat: complete plus - minus - plus - plus -...- count codewars problem

--- 7kyu/Plus - minus - plus - plus - ... - Count/main.js
+++ 7kyu/Plus - minus - plus - plus - ... - Count/main.js
@@ -23,3 +23,24 @@ catchSignChange(arr) == 2;
 */
 
 // solution
+
+const catchSignChange = (arr) => {
+  if (!arr) return 0;
+  let count = 0,
+    plus = false;
+  if (arr[0] >= 0) {
+    plus = true;
+  } else if (arr[0] < 0) {
+    plus = false;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0 && plus === true) {
+      plus = false;
+      count++;
+    } else if (arr[i] >= 0 && plus === false) {
+      plus = true;
+      count++;
+    }
+  }
+  return count;
+};
