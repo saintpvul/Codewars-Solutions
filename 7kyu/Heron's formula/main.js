9832d0d25e0e13ca40372fb8aaf8511e2686b7ae
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-21 23:46:26 +0600

    feat: complete heron's formula codewars problem

--- 7kyu/Heron's formula/main.js
+++ 7kyu/Heron's formula/main.js
@@ -20,3 +20,11 @@ Output should have 2 digits precision.
 */
 
 // solution
+
+const heron = (a, b, c) =>
+  Math.sqrt(
+    ((a + b + c) / 2) *
+      ((a + b + c) / 2 - a) *
+      ((a + b + c) / 2 - b) *
+      ((a + b + c) / 2 - c)
+  );
