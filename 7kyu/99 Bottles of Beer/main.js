1e50b2e62dfe8aa1563d7ae16bd5f63cc7672ea0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-21 23:55:01 +0600

    feat: complete 99bottles of beer codewars problem

--- 7kyu/99 Bottles of Beer/main.js
+++ 7kyu/99 Bottles of Beer/main.js
@@ -46,3 +46,31 @@ Example
 */
 
 // solution
+
+var sing = () => {
+  let song = [];
+  for (let i = 99; i > 1; i--) {
+    song.push(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+    if (i - 1 === 1)
+      song.push(
+        "Take one down and pass it around, " +
+          (i - 1) +
+          " bottle of beer on the wall."
+      );
+    else
+      song.push(
+        "Take one down and pass it around, " +
+          (i - 1) +
+          " bottles of beer on the wall."
+      );
+  }
+  song.push("1 bottle of beer on the wall, 1 bottle of beer.");
+  song.push(
+    "Take one down and pass it around, no more bottles of beer on the wall."
+  );
+  song.push("No more bottles of beer on the wall, no more bottles of beer.");
+  song.push(
+    "Go to the store and buy some more, 99 bottles of beer on the wall."
+  );
+  return song;
+};
