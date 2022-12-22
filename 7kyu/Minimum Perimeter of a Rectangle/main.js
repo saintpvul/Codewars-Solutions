0d1c097afd560a54ddc0451475b080d44c07d9b6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-22 23:41:04 +0600

    feat: complete mininmum perimeter off a rectangle codewars problem

--- 7kyu/Minimum Perimeter of a Rectangle/main.js
+++ 7kyu/Minimum Perimeter of a Rectangle/main.js
@@ -24,3 +24,11 @@ Input range:
 */
 
 // solution
+
+function minimumPerimeter(area) {
+  for (let i = Math.floor(Math.sqrt(area)); i > 0; i--) {
+    if (area % i == 0) {
+      return 2 * (i + area / i);
+    }
+  }
+}
