11502b5bc1c675519947e55493be9fbadf47e563
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-22 23:58:54 +0600

    feat: complete Fun with ES6 Classes #2 - Animals and Inheritance codewars problem

--- 7kyu/Fun with ES6 Classes #2 - Animals and Inheritance/main.js
+++ 7kyu/Fun with ES6 Classes #2 - Animals and Inheritance/main.js
@@ -37,3 +37,30 @@ Dogs have an identical introduce/Introduce method as any other animal, but they 
 */
 
 // solution
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, `shark`, status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, `cat`, status);
+  }
+
+  introduce() {
+    return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, `dog`, status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
