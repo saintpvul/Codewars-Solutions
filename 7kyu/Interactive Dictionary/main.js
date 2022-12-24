7170e0eb88509ffec1c543f54879abaf959fbd14
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-24 23:02:43 +0600

    feat: start interacrive dictionary codewars problem

---
+++ 7kyu/Interactive Dictionary/main.js
@@ -0,0 +1,18 @@
+/*
+
+In this kata, your job is to create a class Dictionary which you can add words to and their entries. Example:
+
+>>> let d = new Dictionary();
+
+>>> d.newEntry("Apple", "A fruit that grows on trees");
+
+>>> console.log(d.look("Apple"));
+A fruit that grows on trees
+
+>>> console.log(d.look("Banana"));
+Can't find entry for Banana
+Good luck and happy coding!
+
+*/
+
+// solution
