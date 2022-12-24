7d712ef1b119f70d162ed588dce1f46259acc0b7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-24 23:06:29 +0600

    feat: complete interacrive dictionary codewars problem

--- 7kyu/Interactive Dictionary/main.js
+++ 7kyu/Interactive Dictionary/main.js
@@ -16,3 +16,17 @@ Good luck and happy coding!
 */
 
 // solution
+
+class Dictionary {
+  constructor() {
+    this.d = {};
+  }
+
+  newEntry(key, value) {
+    this.d[key] = value;
+  }
+
+  look(key) {
+    return this.d[key] || `Can't find entry for ${key}`;
+  }
+}
