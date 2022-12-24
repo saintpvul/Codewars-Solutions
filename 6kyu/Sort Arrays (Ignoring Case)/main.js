62eaf04a82fda2c8b56e5da14dc9302c970977b7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-24 23:26:16 +0600

    feat: start sort arrays(ignoring case) codewars problem

---
+++ 6kyu/Sort Arrays (Ignoring Case)/main.js
@@ -0,0 +1,11 @@
+/*
+
+Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+
+*/
+
+// solution
