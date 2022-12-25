e9fcc5a12557b61a830efbb70dccc9752c3ed7dd
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-25 23:06:52 +0600

    feat: complete the 12 days of christmas codewars problem

--- 7kyu/The 12 Days of Christmas/main.js
+++ 7kyu/The 12 Days of Christmas/main.js
@@ -26,3 +26,23 @@ a partridge in a pear tree.
 */
 
 // solution
+
+const comparator = (a, b) => {
+  let song = [
+    "On the 12th day of Christmas my true love gave to me",
+    "12 drummers drumming,",
+    "11 pipers piping,",
+    "10 lords a leaping,",
+    "9 ladies dancing,",
+    "8 maids a milking,",
+    "7 swans a swimming,",
+    "6 geese a laying,",
+    "5 golden rings,",
+    "4 calling birds,",
+    "3 French hens,",
+    "2 turtle doves and",
+    "a partridge in a pear tree.",
+  ];
+
+  return song.indexOf(a) - song.indexOf(b);
+};
