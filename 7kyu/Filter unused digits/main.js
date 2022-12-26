84c636cd48a0038bca63f131ec6d265308b91eae
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-26 23:44:11 +0600

    feat: complete filter unused digits codewars problem

--- 7kyu/Filter unused digits/main.js
+++ 7kyu/Filter unused digits/main.js
@@ -11,3 +11,10 @@ Note: the digits in the resulting string should be sorted.
 */
 
 // solution
+
+function unusedDigits() {
+  let d = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    args = [...arguments].join("");
+
+  return d.filter((v) => !args.includes(v)).join("");
+}
