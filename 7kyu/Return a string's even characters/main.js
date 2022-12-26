8414d5aef92de5e505a8885c87756a70f4a78149
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-27 01:21:15 +0600

    feat: complete return a string's even chars codewars problem

--- 7kyu/Return a string's even characters/main.js
+++ 7kyu/Return a string's even characters/main.js
@@ -10,3 +10,14 @@ For example:
 */
 
 // solution
+
+function evenChars(string) {
+  if (string.length < 2 || string.length > 100) return "invalid string";
+  let result = [];
+  for (let i = 1; i <= string.length; i++) {
+    if (i % 2 === 0) {
+      result.push(string[i - 1]);
+    }
+  }
+  return result;
+}
