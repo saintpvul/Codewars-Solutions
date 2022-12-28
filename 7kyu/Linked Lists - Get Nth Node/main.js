3335b503d1cb7f9672eb56169a291e43fa6e6107
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-28 23:51:10 +0600

    feat: start linked list - get nth node codewars problem

---
+++ 7kyu/Linked Lists - Get Nth Node/main.js
@@ -0,0 +1,14 @@
+/*
+
+Linked Lists - Get Nth
+Implement a GetNth() function that takes a linked list and an integer index and returns the node stored at the Nth index position. GetNth() uses the C numbering convention that the first node is index 0, the second is index 1, ... and so on.
+
+So for the list 42 -> 13 -> 666, GetNth(1) should return Node(13);
+
+getNth(1 -> 2 -> 3 -> null, 0).data === 1
+getNth(1 -> 2 -> 3 -> null, 1).data === 2
+The index should be in the range [0..length-1]. If it is not, or if the list is empty, GetNth() should throw/raise an exception (ArgumentException in C#, InvalidArgumentException in PHP, Exception in Java).
+
+*/
+
+// solution
