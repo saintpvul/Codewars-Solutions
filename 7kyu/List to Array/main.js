dc123b172d5df9e5f57e8ed5d28b7526bf85fd02
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-28 23:38:43 +0600

    feat: start list of array codewars problem

---
+++ 7kyu/List to Array/main.js
@@ -0,0 +1,15 @@
+/*
+
+Linked lists are data structures composed of nested or chained objects, each containing a single value and a reference to the next object.
+
+Here's an example of a list:
+
+{value: 1, next: {value: 2, next: {value: 3, next: null}}}
+Write a function listToArray (or list_to_array in Python) that converts a list to an array, like this:
+
+[1, 2, 3]
+Assume all inputs are valid lists with at least one value. For the purpose of simplicity, all values will be either numbers, strings, or Booleans.
+
+*/
+
+// solution
