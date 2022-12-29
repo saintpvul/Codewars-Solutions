f077ecbe72a5557025eba945af8173191e269213
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-29 21:53:25 +0600

    feat: start hurry up, the walls are closing in codewars problem

---
+++ 7kyu/Hurry up, the walls are closing in/main.js
@@ -0,0 +1,12 @@
+/*
+
+Given a list walls closing in at you, can you make it past those walls without being hit?
+
+Input
+walls: an array of 2-ples, each 2-ple storing non-negative numbers representing the distance of the walls closing in at you from each side respectively to the center of the room.
+Output
+return a boolean indicating whether it is possible to run past the walls without being hit.
+
+*/
+
+// solution
