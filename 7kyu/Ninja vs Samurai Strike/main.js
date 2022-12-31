45642253250125e178d554de224c1903164c2047
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-31 22:39:41 +0600

    feat: complete ninja vs samurai strike codewars problem

--- 7kyu/Ninja vs Samurai Strike/main.js
+++ 7kyu/Ninja vs Samurai Strike/main.js
@@ -12,3 +12,12 @@ Can you figure out what is wrong?
 */
 
 // solution
+
+var Warrior = function (name) {
+  this.name = name;
+  this.health = 100;
+};
+
+Warrior.prototype.strike = function (enemy, swings) {
+  enemy.health = Math.max(0, enemy.health - swings * 10);
+};
