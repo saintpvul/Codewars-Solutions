8786d61b218ab3a7d6c67ea671533be8f47b08bb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-02 03:53:21 +0600

    feat: start the span function codewars problem

---
+++ 7kyu/The Span Function/main.js
@@ -0,0 +1,22 @@
+/*
+
+The span function is a good one to know. It accepts a sequence and a predicate function and returns two sequences. The first sequence contains all the elements of the argument sequence up to the item that caused the first failure of the predicate. The second returned sequence contains the rest of the original sequence. The original sequence is not modified.
+
+For example,
+
+
+function isEven (x) {
+  return Math.abs(x) % 2 === 0;
+}
+
+var arr = [2,4,6,1,8,10];
+
+// This is true
+span(arr, isEven) === [[2,4,6],[1,8,10]]
+Your task is to...wait for it... write your own span function !!!
+
+Hint/Challenge: If you have completed the takeWhile function and the dropWhile function, then you can solve this problem in one line!
+
+*/
+
+// solution
