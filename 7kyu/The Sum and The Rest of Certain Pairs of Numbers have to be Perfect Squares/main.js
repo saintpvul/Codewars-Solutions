25785033f8f12c37bdd16c4ba3737043fe524568
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-02 23:09:02 +0600

    feat: complete the sum and the rest of certain pairs of numbers to be perfect squares codewars problem

--- 7kyu/The Sum and The Rest of Certain Pairs of Numbers have to be Perfect Squares/main.js
+++ 7kyu/The Sum and The Rest of Certain Pairs of Numbers have to be Perfect Squares/main.js
@@ -28,3 +28,13 @@ Happy coding and enjoy it!! (We will be having a second part of a similar exerci
 */
 
 // solution
+
+function closestPairTonum(n) {
+  for (let i = n - 1; i >= 1; i--) {
+    for (let j = i - 1; j >= 1; j--) {
+      if (Math.sqrt(i + j) % 1 === 0 && Math.sqrt(i - j) % 1 === 0) {
+        return [i, j];
+      }
+    }
+  }
+}
