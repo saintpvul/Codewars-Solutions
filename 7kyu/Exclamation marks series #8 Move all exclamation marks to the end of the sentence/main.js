6f3808f785ee09d2bd08826e8ab25e485508a2fc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-06 23:37:21 +0600

    feat: complete Exclamation marks series #8: Move all exclamation marks to the end of the sentence codewars problem

--- 7kyu/Exclamation marks series #8 Move all exclamation marks to the end of the sentence/main.js
+++ 7kyu/Exclamation marks series #8 Move all exclamation marks to the end of the sentence/main.js
@@ -13,3 +13,15 @@ remove("Hi! Hi!! Hi!") === "Hi Hi Hi!!!!"
 */
 
 // solution
+
+function remove(string) {
+  let output = string.replace(/!/g, ""),
+    count = 0;
+  string = string.split("");
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "!") {
+      count++;
+    }
+  }
+  return output + "!".repeat(count);
+}
