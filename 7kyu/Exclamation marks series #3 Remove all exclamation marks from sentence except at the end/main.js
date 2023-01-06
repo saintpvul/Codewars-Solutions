c16d80edd8834a76a32923ecd9fd9515ae1a9cf3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-06 23:21:14 +0600

    feat: start Exclamation marks series #3: Remove all exclamation marks from sentence except at the end codewars problem

---
+++ 7kyu/Exclamation marks series #3 Remove all exclamation marks from sentence except at the end/main.js
@@ -0,0 +1,16 @@
+/*
+
+Description:
+Remove all exclamation marks from sentence except at the end.
+
+Examples
+remove("Hi!") == "Hi!"
+remove("Hi!!!") == "Hi!!!"
+remove("!Hi") == "Hi"
+remove("!Hi!") == "Hi!"
+remove("Hi! Hi!") == "Hi Hi!"
+remove("Hi") == "Hi"
+
+*/
+
+// solution
