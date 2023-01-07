c6a1413cea8584b60f3492b35c83f1a82fb1e6df
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-07 21:02:27 +0600

    feat: complete regexp basics - is if ipbv4 address? codewars problem

--- 6kyu/Regexp Basics - is it IPv4 address/main.js
+++ 6kyu/Regexp Basics - is it IPv4 address/main.js
@@ -9,3 +9,9 @@ It should only accept addresses in canonical representation, so no leading 0s, s
 */
 
 // solution
+
+String.prototype.ipv4Address = function () {
+  return /^(?=\d+\.\d+\.\d+\.\d+$)(?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.?){4}$/.test(
+    this
+  );
+};
