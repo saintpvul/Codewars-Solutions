1c78dab70278c5786ff4fbb9dfe5310564a38e2a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-07 20:44:57 +0600

    feat: complete nodejs intro codewars problem

--- 7kyu/Node.js Intro/main.js
+++ 7kyu/Node.js Intro/main.js
@@ -33,3 +33,11 @@ The NodeJS documentation is helpful, so I suggest you look at that. If you plan 
 */
 
 // solution
+
+String.prototype.toBase64 = function () {
+  return new Buffer(this.toString()).toString("base64");
+};
+
+String.prototype.fromBase64 = function () {
+  return new Buffer(this.toString(), "base64").toString("utf8");
+};
