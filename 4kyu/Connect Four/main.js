9ddc6ed5a3e955506de8a97c1938426f05c2861b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-07 21:09:30 +0600

    feat: complete connect four codewars problem

--- 4kyu/Connect Four/main.js
+++ 4kyu/Connect Four/main.js
@@ -26,3 +26,19 @@ You should return "Yellow", "Red" or "Draw" accordingly.
 */
 
 // solution
+
+function whoIsWinner(piecesPositionList) {
+  const dict = { A: 35, B: 36, C: 37, D: 38, E: 39, F: 40, G: 41 },
+    res = new Array(42).fill("-"),
+    rRegExp =
+      /((R.{6}){3}R)|(^((.{0,3}|.{7,10}|.{14,17}|.{21,24}|.{28,31}|.{35,38})R{4}))|(^((.{3,6}|.{10,13}|.{17,20})(R.{5}){3}R))|(^(.{0,3}|.{7,10}|.{14,17})(R.{7}){3}R)/,
+    yRegExp =
+      /((Y.{6}){3}Y)|(^((.{0,3}|.{7,10}|.{14,17}|.{21,24}|.{28,31}|.{35,38})Y{4}))|(^((.{3,6}|.{10,13}|.{17,20})(Y.{5}){3}Y))|(^(.{0,3}|.{7,10}|.{14,17})(Y.{7}){3}Y)/;
+  for (let s of piecesPositionList) {
+    res[dict[s[0]]] = s[2];
+    dict[s[0]] -= 7;
+    if (rRegExp.test(res.join(""))) return "Red";
+    if (yRegExp.test(res.join(""))) return "Yellow";
+  }
+  return "Draw";
+}
