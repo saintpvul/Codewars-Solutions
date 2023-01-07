e37a7c97e10df2e06e205d3c627c8d843b974046
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-07 20:09:00 +0600

    feat: start password hashes codewars problem

---
+++ 7kyu/Password Hashes/main.js
@@ -0,0 +1,9 @@
+/*
+
+When you sign up for an account somewhere, some websites do not actually store your password in their databases. Instead, they will transform your password into something else using a cryptographic hashing algorithm.
+
+After the password is transformed, it is then called a password hash. Whenever you try to login, the website will transform the password you tried using the same hashing algorithm and simply see if the password hashes are the same.
+
+*/
+
+// solution
