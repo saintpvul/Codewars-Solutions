faaead3c2f3819acd8564aa6adf9f05483c32ab3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-10 22:49:17 +0600

    feat: complete ga-de-ry-po-lu-ki cypher codewars problem

--- 7kyu/GA-DE-RY-PO-LU-KI cypher/main.js
+++ 7kyu/GA-DE-RY-PO-LU-KI cypher/main.js
@@ -30,3 +30,34 @@ Example
 */
 
 // solution
+
+let dict = {
+  G: "A",
+  A: "G",
+  D: "E",
+  E: "D",
+  R: "Y",
+  Y: "R",
+  P: "O",
+  O: "P",
+  L: "U",
+  U: "L",
+  K: "I",
+  I: "K",
+  g: "a",
+  a: "g",
+  d: "e",
+  e: "d",
+  r: "y",
+  y: "r",
+  p: "o",
+  o: "p",
+  l: "u",
+  u: "l",
+  k: "i",
+  i: "k",
+};
+
+const encode = (str) => str.replace(/./g, (c) => dict[c] || c);
+
+const decode = (str) => str.replace(/./g, (c) => dict[c] || c);
