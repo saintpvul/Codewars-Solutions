b2830b35a7849aba506927f2eccde2479c668ce0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-12 20:28:06 +0600

    feat: complete the alphabeti product codewars problem

--- 7kyu/The alphabet product/main.js
+++ 7kyu/The alphabet product/main.js
@@ -6,3 +6,16 @@ I have four positive integers, A, B, C and D, where A < B < C < D. The input is 
 */
 
 // solution
+
+function alphabet(ns) {
+  ns = ns.sort((a, b) => a - b);
+
+  for (let i = 0; i < ns.length - 1; i++) {
+    let result = ns[i] * ns[i + 1];
+    let idx = ns.indexOf(result);
+    if (idx !== -1) {
+      ns.splice(idx, 1);
+    }
+  }
+  return ns[3];
+}
