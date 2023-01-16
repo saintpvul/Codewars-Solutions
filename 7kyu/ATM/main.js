a8ec92194b179a6be3f990b8a3bc7250f68374e7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-16 23:56:58 +0600

    feat: complete atm codewars problem

--- 7kyu/ATM/main.js
+++ 7kyu/ATM/main.js
@@ -11,3 +11,16 @@ Good Luck!!!
 */
 
 // solution
+
+function solve(n) {
+  let count = 0,
+    nums = [500, 200, 100, 50, 20, 10];
+
+  for (let i of nums) {
+    while (i <= n) {
+      n = n - i;
+      count += 1;
+    }
+  }
+  return n !== 0 ? -1 : count;
+}
