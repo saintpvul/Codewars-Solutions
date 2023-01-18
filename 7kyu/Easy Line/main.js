ed0ea7e194da2af0d3a4eca4e2b04981987f0942
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-18 23:52:37 +0600

    feat: complete easy line codewars problem

--- 7kyu/Easy Line/main.js
+++ 7kyu/Easy Line/main.js
@@ -35,3 +35,11 @@ easyLine(50) => 67
 */
 
 // solution
+
+function easyLine(n) {
+  let sum = 1;
+  for (let i = 1; i <= n; i++) {
+    sum *= (n + i) / i;
+  }
+  return Math.round(Math.log(sum));
+}
