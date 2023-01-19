bc7c0bc6c3fb8ecf964866a438efbca9eecf6122
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-20 03:26:36 +0600

    feat: complete Regular Expression - Check if divisible by 0b111 (7) codewars problem

--- 2kyu/Regular Expression - Check if divisible by 0b111 (7)/main.js
+++ 2kyu/Regular Expression - Check if divisible by 0b111 (7)/main.js
@@ -12,3 +12,8 @@ No leading 0's will be tested unless the string exactly denotes 0.
 */
 
 // solution
+
+// Write a regular expression to detect whether a binary number is divisible by 7
+// It won't be accepted if you code something else like Function
+const solution =
+  /^(0|(10((0|11)(1|00))*(10|(0|11)01)|11)(01*0(0|101|1(1|00)((0|11)(1|00))*(10|(0|11)01)))*1)+$/;
