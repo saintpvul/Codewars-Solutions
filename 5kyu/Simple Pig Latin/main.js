cb57ceb7d46f95f2da5fe6cafbed9a4f296a42e9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-21 20:39:10 +0600

    feat: start simple pig latin codewars problem

---
+++ 5kyu/Simple Pig Latin/main.js
@@ -0,0 +1,11 @@
+/*
+
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+
+*/
+
+// solution
