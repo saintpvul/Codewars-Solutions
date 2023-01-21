7db0e704cf7dcc18afa6395796cf8bf4d6b83557
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-21 16:32:57 +0600

    feat: complete who likes it? codewars problem

--- 6kyu/Who likes it/main.js
+++ 6kyu/Who likes it/main.js
@@ -11,8 +11,16 @@ Implement the function which takes an array containing the names of people that 
 ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 Note: For 4 or more names, the number in "and 2 others" simply increases.
 
-
-
 */
 
 // solution
+
+function likes(names) {
+  if (!names.length) return "no one likes this";
+  if (names.length === 1) return `${names.join("")} likes this`;
+  return names.length < 4
+    ? `${names.slice(0, -1).join(", ")} and ${names.slice(-1)} like this`
+    : `${names.slice(0, 2).join(", ")} and ${
+        names.slice(2).length
+      } others like this`;
+}
