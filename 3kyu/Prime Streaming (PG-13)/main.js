262288e5f828e3350c63857250555877372056f0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-23 21:46:11 +0600

    feat: start prime streaming (pg-13) codewars problem

---
+++ 3kyu/Prime Streaming (PG-13)/main.js
@@ -0,0 +1,9 @@
+/*
+
+Create an endless stream of prime numbers - a bit like IntStream.of(2, 3, 5, 7, 11, 13, 17), but infinite. The stream must be able to produce a million primes in a few seconds.
+
+If this is too easy, try Prime Streaming (NC-17).
+
+*/
+
+// solution
