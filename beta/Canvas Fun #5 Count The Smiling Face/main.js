421c32dfa2b752220cf2eab4301813b43222ed9f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-26 23:39:54 +0600

    feat: start cancas fun #5 count the smiling face codewars problem

---
+++ beta/Canvas Fun #5 Count The Smiling Face/main.js
@@ -0,0 +1,31 @@
+/*
+
+
+
+*/
+
+// solution
+/*
+
+Welcome to Canvas Fun ;-) In this series, we'll learning how to draw the image on a canvas.
+
+Task
+You are given a image.
+
+
+Your task is to count the number of smiling face in the image.
+
+No new canvas methods here. You just need some old methods from #4.
+
+Note
+The number of face is 1 to 10.
+
+The size of face is randomly.
+
+The faces will not cover each other
+
+All faces are whole face, no half face appears.
+
+*/
+
+//
