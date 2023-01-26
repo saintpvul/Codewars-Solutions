128efc3d082cdbf888d86991f30a0c80666729b5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-26 23:47:38 +0600

    feat: complete split strings codewars problem

--- 6kyu/Split Strings/main.js
+++ 6kyu/Split Strings/main.js
@@ -10,3 +10,14 @@ Examples:
 */
 
 // solution
+
+const solution = (str) => {
+  if (!str.length) return [];
+  if (str.length % 2) str = str + "_";
+  str = str.split("");
+  let res = [];
+  for (let i = 0; i < str.length; i + 2) {
+    res.push(str.splice(i, i + 2).join(""));
+  }
+  return res;
+};
