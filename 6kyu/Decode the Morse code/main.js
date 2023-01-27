692b58f5f662e7a8f7861350abbe60e4227f61db
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-27 23:50:55 +0600

    feat: complete decode the morse code codewars problem

--- 6kyu/Decode the Morse code/main.js
+++ 6kyu/Decode the Morse code/main.js
@@ -17,16 +17,16 @@ NOTE: For coding purposes you have to use ASCII characters . and -, not Unicode 
 
 The Morse code table is preloaded for you as a dictionary, feel free to use it:
 
-Coffeescript/C++/Go/JavaScript/Julia/PHP/Python/Ruby/TypeScript: MORSE_CODE['.--']
+Coffeescript/C++/Go/JavaScript/Julia/PHP/Python/Ruby/TypeScript: MORSE-CODE['.--']
 C#: MorseCode.Get(".--") (returns string)
 F#: MorseCode.get ".--" (returns string)
-Elixir: @morse_codes variable (from use MorseCode.Constants). Ignore the unused variable warning for morse_codes because it's no longer used and kept only for old solutions.
+Elixir: @morse-codes variable (from use MorseCode.Constants). Ignore the unused variable warning for morse-codes because it's no longer used and kept only for old solutions.
 Elm: MorseCodes.get : Dict String String
 Haskell: morseCodes ! ".--" (Codes are in a Map String String)
 Java: MorseCode.get(".--")
 Kotlin: MorseCode[".--"] ?: "" or MorseCode.getOrDefault(".--", "")
 Racket: morse-code (a hash table)
-Rust: MORSE_CODE
+Rust: MORSE-CODE
 Scala: morseCodes(".--")
 Swift: MorseCode[".--"] ?? "" or MorseCode[".--", default: ""]
 C: provides parallel arrays, i.e. morse[2] == "-.-" for ascii[2] == "C"
@@ -40,3 +40,60 @@ After you complete this kata, you may try yourself at Decode the Morse code, adv
 */
 
 // solution
+
+function decodeMorse(morseCode) {
+  morseCode = morseCode.trim();
+
+  let morse = {
+      ".-": "A",
+      "-...": "B",
+      "-.-.": "C",
+      "-..": "D",
+      ".": "E",
+      "..-.": "F",
+      "--.": "G",
+      "....": "H",
+      "..": "I",
+      ".---": "J",
+      "-.-": "K",
+      ".-..": "L",
+      "--": "M",
+      "-.": "N",
+      "---": "O",
+      ".--.": "P",
+      "--.-": "Q",
+      ".-.": "R",
+      "...": "S",
+      "-": "T",
+      "..-": "U",
+      "...-": "V",
+      ".--": "W",
+      "-..-": "X",
+      "-.--": "Y",
+      "--..": "Z",
+      ".----": "1",
+      "..---": "2",
+      "...--": "3",
+      "....-": "4",
+      ".....": "5",
+      "-....": "6",
+      "--...": "7",
+      "---..": "8",
+      "----.": "9",
+      "-----": "0",
+    },
+    decode = [];
+
+  let prepareChars = morseCode.split("   ");
+
+  for (let i = 0; i < prepareChars.length; i++) {
+    let chars = prepareChars[i].split(" ");
+    for (let char = 0; char < chars.length; char++) {
+      if (MORSE_CODE[chars[char]]) {
+        decode.push(MORSE_CODE[chars[char]]);
+      }
+    }
+    decode.push(" ");
+  }
+  return decode.join("").trim();
+}
