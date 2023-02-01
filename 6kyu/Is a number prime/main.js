f1691aa05fdc06f010934f983b87341a8f429e27
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-01 23:53:57 +0600

    feat: complete is a number prime? codewars problem

--- 6kyu/Is a number prime/main.js
+++ 6kyu/Is a number prime/main.js
@@ -9,10 +9,16 @@ You can assume you will be given an integer input.
 You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
 NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
 Example
-is_prime(1)  /* false */
-is_prime(2)  /* true  */
-is_prime(-1) /* false */
-
+is_prime(1)  /* false 
+is_prime(2)  /* true  
+is_prime(-1) /* false 
 */
 
 // solution
+
+function isPrime(num) {
+  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+    if (!(num % i)) return false;
+  }
+  return num > 1;
+}
