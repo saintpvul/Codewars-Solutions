c1c35c7eb557be1c07e65d175c74c7ad20f0545b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-01 23:24:43 +0600

    feat: complete moving zeros to the end codewars problem

--- 6kyu/Moving Zeros To The End/main.js
+++ 6kyu/Moving Zeros To The End/main.js
@@ -8,3 +8,16 @@ moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 */
 
 // solution
+
+function moveZeros(arr) {
+  let zeros = [],
+    e = [];
+  for (let i of arr) {
+    if (i === 0) {
+      zeros.push(i);
+    } else {
+      e.push(i);
+    }
+  }
+  return e.concat(zeros);
+}
