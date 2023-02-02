595c7f486cf662467eac52d547d9deeb77e325bd
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-02 22:01:41 +0600

    feat: complete two sum codewars problem

--- 6kyu/Two Sum/main.js
+++ 6kyu/Two Sum/main.js
@@ -13,3 +13,13 @@ twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
 */
 
 // solution
+
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
