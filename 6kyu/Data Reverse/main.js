d6bfcb20b31b86e735919fb6cf1c9a8b5c3f2cbc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-04 19:59:21 +0600

    feat: complete data reverse codewars problem

--- 6kyu/Data Reverse/main.js
+++ 6kyu/Data Reverse/main.js
@@ -20,3 +20,11 @@ Note: In the C and NASM languages you are given the third parameter which is the
 */
 
 // solution
+
+function dataReverse(data) {
+  let res = [];
+  for (let i = 0; i < data.length; i += 8) {
+    res.push(data.slice(i, i + 8));
+  }
+  return res.reverse().flat();
+}
