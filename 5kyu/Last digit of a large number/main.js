4a341be1f3a4a9a810561819d61423d6804e82f1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-04 23:24:00 +0600

    feat: start last digit of a large num codewars problem

---
+++ 5kyu/Last digit of a large number/main.js
@@ -0,0 +1,75 @@
+/*
+
+Define a function that takes in two non-negative integers 
+�
+a and 
+�
+b and returns the last decimal digit of 
+�
+�
+a 
+b
+ . Note that 
+�
+a and 
+�
+b may be very large!
+
+For example, the last decimal digit of 
+9
+7
+9 
+7
+  is 
+9
+9, since 
+9
+7
+=
+4782969
+9 
+7
+ =4782969. The last decimal digit of 
+(
+2
+200
+)
+2
+300
+(2 
+200
+ ) 
+2 
+300
+ 
+ , which has over 
+1
+0
+92
+10 
+92
+  decimal digits, is 
+6
+6. Also, please take 
+0
+0
+0 
+0
+  to be 
+1
+1.
+
+You may assume that the input will always be valid.
+
+Examples
+lastDigit("4", "1")            // returns 4
+lastDigit("4", "2")            // returns 6
+lastDigit("9", "7")            // returns 9    
+lastDigit("10","10000000000")  // returns 0
+Remarks
+JavaScript, C++, R, PureScript, COBOL
+Since these languages don't have native arbitrarily large integers, your arguments are going to be strings representing non-negative integers instead.
+
+*/
+
+// solution
