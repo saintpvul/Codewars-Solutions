fad006a8a9b0a1637e2c6bb3419ed7a3b78c291b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-07 01:14:57 +0600

    feat: start kebabize codewars problem

---
+++ 6kyu/Kebabize/main.js
@@ -0,0 +1,13 @@
+/*
+
+Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+kebabize('camelsHaveThreeHumps') // camels-have-three-humps
+kebabize('camelsHave3Humps') // camels-have-humps
+Notes:
+
+the returned string should only contain lowercase letters
+
+*/
+
+// solution
