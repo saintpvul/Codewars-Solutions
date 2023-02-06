2c0ab05ae81413b8dd8c957cc0b751cededdce63
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-07 01:41:11 +0600

    feat: start character with longest consecutive repetition codewars problem

---
+++ 6kyu/Character with longest consecutive repetition/main.js
@@ -0,0 +1,17 @@
+/*
+
+For a given string s find the character c (or C) with longest consecutive repetition and return:
+
+[c, l]
+where l (or L) is the length of the repetition. If there are two or more characters with the same l return the first in order of appearance.
+
+For empty string return:
+
+["", 0]
+In JavaScript: If you use Array.sort in your solution, you might experience issues with the random tests as Array.sort is not stable in the Node.js version used by CodeWars. This is not a kata issue.
+
+Happy coding! :)
+
+*/
+
+// solution
