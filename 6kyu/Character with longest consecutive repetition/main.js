76875a8839bd861709d873a20ba084e0305aeb9b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-07 02:03:28 +0600

    feat: complete character with longest consecutive repetition codewars problem

--- 6kyu/Character with longest consecutive repetition/main.js
+++ 6kyu/Character with longest consecutive repetition/main.js
@@ -15,3 +15,22 @@ Happy coding! :)
 */
 
 // solution
+
+function longestRepetition(s) {
+  let char = "",
+    count = 1,
+    temp = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      count++;
+    } else {
+      if (temp.every((s) => s < count)) {
+        char = s[i] + count;
+      }
+      temp.push(count);
+      count = 1;
+    }
+  }
+  return !char ? ["", 0] : [char.slice(0, 1), char.slice(1) * 1];
+}
