45d1596e33c03870c63ebe4b55f98865d8441bb2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-08 22:44:07 +0600

    feat: start linked lists - sorted inser codewars problem

---
+++ 6kyu/Linked Lists - Sorted Insert/main.js
@@ -0,0 +1,14 @@
+/*
+
+Linked Lists - Sorted Insert
+
+Write a SortedInsert() function which inserts a node into the correct location of a pre-sorted linked list which is sorted in ascending order. SortedInsert takes the head of a linked list and data used to create a node as arguments. SortedInsert() should also return the head of the list.
+
+sortedInsert(1 -> 2 -> 3 -> null, 4) === 1 -> 2 -> 3 -> 4 -> null)
+sortedInsert(1 -> 7 -> 8 -> null, 5) === 1 -> 5 -> 7 -> 8 -> null)
+sortedInsert(3 -> 5 -> 9 -> null, 7) === 3 -> 5 -> 7 -> 9 -> null)
+The push() and buildOneTwoThree() functions do not need to be redefined.
+
+*/
+
+// solution
