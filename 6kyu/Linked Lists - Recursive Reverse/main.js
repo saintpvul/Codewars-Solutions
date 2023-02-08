ebb7025a6f5c57bfc833ab7ea60ac9499f3ac942
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-08 22:38:29 +0600

    feat: complete linked lists - recursive reverse codewars problem

--- 6kyu/Linked Lists - Recursive Reverse/main.js
+++ 6kyu/Linked Lists - Recursive Reverse/main.js
@@ -10,3 +10,13 @@ reverse(list) === 5 -> 6 -> 3 -> 1 -> 2 -> null
 */
 
 // solution
+
+function Node(data = null, next = null) {
+  this.data = data;
+  this.next = next;
+}
+
+function reverse(head, prev = null) {
+  if (!head) return prev;
+  return reverse(head.next, new Node(head.data, prev));
+}
