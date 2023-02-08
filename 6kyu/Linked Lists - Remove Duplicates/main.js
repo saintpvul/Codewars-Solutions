8897f96846f5a7a01d4b8b7902da1909ac527e29
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-08 22:40:14 +0600

    feat: start linked lists - remove duplicates codewars problem

---
+++ 6kyu/Linked Lists - Remove Duplicates/main.js
@@ -0,0 +1,17 @@
+/*
+
+Linked Lists - Remove Duplicates
+
+Write a RemoveDuplicates() function which takes a list sorted in increasing order and deletes any duplicate nodes from the list. Ideally, the list should only be traversed once. The head of the resulting list should be returned.
+
+var list = 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5 -> null
+removeDuplicates(list) === 1 -> 2 -> 3 -> 4 -> 5 -> null
+If the passed in list is null/None/nil, simply return null.
+
+Note: Your solution is expected to work on long lists. Recursive solutions may fail due to stack size limitations.
+
+The push() and buildOneTwoThree() functions need not be redefined.
+
+*/
+
+// solution
