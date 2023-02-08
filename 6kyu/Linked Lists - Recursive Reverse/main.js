743a28346fd0358d5c577c20f2c99ee84eef7a26
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-08 22:32:52 +0600

    feat: start linked lists - recursive reverse codewars problem

---
+++ 6kyu/Linked Lists - Recursive Reverse/main.js
@@ -0,0 +1,12 @@
+/*
+
+Linked Lists - Recursive Reverse
+
+Write a Recursive Reverse() function that recursively reverses a linked list. You may want to use a nested function for the recursive calls.
+
+var list = 2 -> 1 -> 3 -> 6 -> 5 -> null
+reverse(list) === 5 -> 6 -> 3 -> 1 -> 2 -> null
+
+*/
+
+// solution
