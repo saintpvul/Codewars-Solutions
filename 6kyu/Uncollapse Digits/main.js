0a731e130705f6da9ef4471364b74857a5b6d14d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-10 22:51:14 +0600

    feat: start uncollapse digits  codewars problem

---
+++ 6kyu/Uncollapse Digits/main.js
@@ -0,0 +1,21 @@
+/*
+
+Task
+You will be given a string of English digits "stuck" together, like this:
+
+"zeronineoneoneeighttwoseventhreesixfourtwofive"
+
+Your task is to split the string into separate digits:
+
+"zero nine one one eight two seven three six four two five"
+
+Examples
+"three"              -->  "three"
+"eightsix"           -->  "eight six"
+"fivefourseven"      -->  "five four seven"
+"ninethreesixthree"  -->  "nine three six three"
+"fivethreefivesixthreenineonesevenoneeight"  -->  "five three five six three nine one seven one eight"
+
+*/
+
+// solution
