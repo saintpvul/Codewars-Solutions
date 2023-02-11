55498e71542436452fe2b0cd043d4d793b1c4b84
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-12 00:10:10 +0600

    feat: start pascal's triangle codewars problem

---
+++ 6kyu/Pascal's Triangle/main.js
@@ -0,0 +1,47 @@
+/*
+
+In mathematics, Pascal's triangle is a triangular array of the binomial coefficients expressed with formula
+
+(
+�
+�
+)
+=
+�
+!
+�
+!
+(
+�
+−
+�
+)
+!
+( 
+k
+n
+​
+ )= 
+k!(n−k)!
+n!
+​
+ 
+where n denotes a row of the triangle, and k is a position of a term in the row.
+
+Pascal's Triangle
+
+You can read Wikipedia article on Pascal's Triangle for more information.
+
+Task
+Write a function that, given a depth n, returns n top rows of Pascal's Triangle flattened into a one-dimensional list/array.
+
+Example:
+n = 1: [1]
+n = 2: [1,  1, 1]
+n = 4: [1,  1, 1,  1, 2, 1,  1, 3, 3, 1]
+Note
+Beware of overflow. Requested terms of a triangle are guaranteed to fit into the returned type, but depending on seleced method of calculations, intermediate values can be larger.
+
+*/
+
+// solution
