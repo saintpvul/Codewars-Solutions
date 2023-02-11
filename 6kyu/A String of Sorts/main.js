bbf132a8036261013b01e3c10167213a47af1f39
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-11 22:19:57 +0600

    feat: start a string of sorts codewars problem

---
+++ 6kyu/A String of Sorts/main.js
@@ -0,0 +1,16 @@
+/*
+
+Define a method that accepts 2 strings as parameters. The method returns the first string sorted by the second.
+
+sortString("foos", "of")       => "oofs"
+sortString("string", "gnirts") => "gnirts"
+sortString("banana", "abn")    => "aaabnn"
+To elaborate, the second string defines the ordering. It is possible that in the second string characters repeat, so you should remove repeating characters, leaving only the first occurrence.
+
+Any character in the first string that does not appear in the second string should be sorted to the end of the result in original order.
+
+
+
+*/
+
+// solution
