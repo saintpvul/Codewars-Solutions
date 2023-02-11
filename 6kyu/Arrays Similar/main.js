3fd6bd0429e0c3586891dc4b44d6610a4e5e4ff4
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-11 23:36:40 +0600

    feat: complete array similar codewars problem

--- 6kyu/Arrays Similar/main.js
+++ 6kyu/Arrays Similar/main.js
@@ -14,3 +14,15 @@ arraysSimilar(arr3, arr4); // Should equal false
 */
 
 // solution
+
+function arraysSimilar(arr1, arr2) {
+  arr1 = arr1.sort((a, b) => a - b);
+  arr2 = arr2.sort((a, b) => a - b);
+
+  if (arr1.length != arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
