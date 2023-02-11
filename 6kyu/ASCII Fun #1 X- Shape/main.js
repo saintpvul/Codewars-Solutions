26c68797de46060ffa5d1749a2e2ab28f671abf4
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-11 23:37:39 +0600

    feat: start ascii fun #1 x-shape codewars problem

---
+++ 6kyu/ASCII Fun #1 X- Shape/main.js
@@ -0,0 +1,18 @@
+/*
+
+You will get an odd integer n (>= 3) and your task is to draw an X. Each line is separated with \n.
+
+Use the following characters: ■ □ For Ruby, Crystal and PHP: whitespace and o
+
+Examples
+
+                                     ■□□□■
+            ■□■                      □■□■□
+  x(3) =>   □■□             x(5) =>  □□■□□
+            ■□■                      □■□■□
+                                     ■□□□■
+
+
+*/
+
+// solution
