6183d4eef6780c7f5155a673a96b9cedeb944cf0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-12 21:14:19 +0600

    feat: complete string average codewars problem

--- 6kyu/String average/main.js
+++ 6kyu/String average/main.js
@@ -9,3 +9,48 @@ If the string is empty or includes a number greater than 9, return "n/a"
 */
 
 // solution
+
+function averageString(str) {
+  if (!str.length) return "n/a";
+  let numbers = {
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9,
+      zero: 0,
+    },
+    res = 0;
+  str = str.split(" ");
+  for (let num of str) {
+    res += numbers[num];
+  }
+
+  res = Math.floor(res / str.length);
+
+  return res === 1
+    ? "one"
+    : res === 2
+    ? "two"
+    : res === 3
+    ? "three"
+    : res === 4
+    ? "four"
+    : res === 5
+    ? "five"
+    : res === 6
+    ? "six"
+    : res === 7
+    ? "seven"
+    : res === 8
+    ? "eight"
+    : res === 9
+    ? "nine"
+    : res === 0
+    ? "zero"
+    : "n/a";
+}
