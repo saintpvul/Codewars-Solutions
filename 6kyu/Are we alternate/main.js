2a8d4e88c992c55b8adb8453edecb9c6103edf68
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-13 23:51:42 +0600

    feat: start are we alternate? codewars problem

---
+++ 6kyu/Are we alternate/main.js
@@ -0,0 +1,15 @@
+/*
+
+Create a function isAlt() that accepts a string as an argument and validates whether the vowels (a, e, i, o, u) and consonants are in alternate order.
+
+isAlt("amazon")
+// true
+isAlt("apple")
+// false
+isAlt("banana")
+// true
+Arguments consist of only lowercase letters.
+
+*/
+
+// solution
