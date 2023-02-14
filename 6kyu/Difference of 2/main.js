0f43555a9100261c9ce06449e910e11c41176267
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-14 22:52:59 +0600

    feat: complete difference of 2 codewars problem

--- 6kyu/Difference of 2/main.js
+++ 6kyu/Difference of 2/main.js
@@ -18,3 +18,19 @@ Examples
 */
 
 // solution
+
+function twosDifference(input) {
+  input = input.sort((a, b) => a - b);
+
+  let res = [];
+
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] + 2 === input[j]) {
+        res.push([input[i], input[j]]);
+        break;
+      }
+    }
+  }
+  return res;
+}
