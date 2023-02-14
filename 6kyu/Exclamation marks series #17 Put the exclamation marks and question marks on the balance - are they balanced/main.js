b66fc16b064a66cc2010ab04c3bd660f85c69049
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-14 23:58:12 +0600

    feat: complete exclamation mark series #17 put the exclamation marks and question marks on the balance codewars problem

--- 6kyu/Exclamation marks series #17 Put the exclamation marks and question marks on the balance - are they balanced/main.js
+++ 6kyu/Exclamation marks series #17 Put the exclamation marks and question marks on the balance - are they balanced/main.js
@@ -13,3 +13,42 @@ Examples
 */
 
 // solution
+
+function balance(left, right) {
+  left = left.split("");
+  right = right.split("");
+
+  let count = [0, 0];
+  for (let i = 0; i < left.length; i++) {
+    if (left[i] === "!") {
+      count[0] += 2;
+    }
+    if (left[i] === "?") {
+      count[0] += 3;
+    }
+  }
+
+  for (let i = 0; i < right.length; i++) {
+    if (right[i] === "!") {
+      count[1] += 2;
+    }
+    if (right[i] === "?") {
+      count[1] += 3;
+    }
+  }
+  return count[0] > count[1]
+    ? "Left"
+    : count[0] < count[1]
+    ? "Right"
+    : "Balance";
+}
+
+/*
+
+function balance(left,right){
+  let l = left.split('').map(e=>e === '!' ? 2 : 3).reduce((s,v)=> s+= v, 0),
+      r = right.split('').map(e=>e === '!' ?  2 : 3).reduce((s,v)=> s+= v, 0)
+  return l > r ? 'Left' : l < r ? 'Right' : 'Balance'
+}
+
+*/
