ace1108430cfb39cd1703d3f6d0b1a21321afe67
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-16 23:13:16 +0600

    feat: complete single character palindromes codewars problem

--- 6kyu/Single character palindromes/main.js
+++ 6kyu/Single character palindromes/main.js
@@ -16,3 +16,27 @@ If you like this Kata, please try Single Character Palindromes II
 */
 
 // solution
+
+function solve(s) {
+  let pal = [...s],
+    isPal = (str) => str.split("").reverse().join("");
+
+  if (s === isPal(s)) {
+    return "OK";
+  }
+
+  if (
+    s
+      .split("")
+      .map((v) => s)
+      .some((e, i) => {
+        e = e.split("");
+        e.splice(i, 1).join("");
+        return e.join("") === isPal(e.join(""));
+      })
+  ) {
+    return "remove one";
+  }
+
+  return "not possible";
+}
