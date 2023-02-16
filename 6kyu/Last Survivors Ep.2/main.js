8c31019fc2bee9d060a33472c15a734822d622aa
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-16 23:55:11 +0600

    feat: complete last survivor ep.2 codewars problem

--- 6kyu/Last Survivors Ep.2/main.js
+++ 6kyu/Last Survivors Ep.2/main.js
@@ -23,3 +23,34 @@ If you like this kata, check out another one: Last Survivor Ep.3
 */
 
 // solution
+
+let alph = "abcdefghijklmnopqrstuvwxyza";
+
+function duplicate(a) {
+  let dup = [];
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length; j++)
+      if (i != j)
+        if (a[i] === a[j]) {
+          dup.push(a[i]);
+          break;
+        }
+    if (dup.length) break;
+  }
+  return dup;
+}
+
+function lastSurvivors(str) {
+  let dup = duplicate(str.split(""));
+  if (dup.length) {
+    for (let i = 0; i < dup.length; i++) {
+      for (let j = 0; j < 2; j++) {
+        let remove = str.indexOf(dup[i]);
+        str = str.slice(0, remove) + str.slice(remove + 1, str.length);
+      }
+      str += alph[alph.indexOf(dup[i]) + 1];
+    }
+    return lastSurvivors(str);
+  }
+  return str;
+}
