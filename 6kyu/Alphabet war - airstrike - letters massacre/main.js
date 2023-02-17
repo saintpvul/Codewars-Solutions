bbe18e2cd2cbe81faf056cc9680d5373fa5c1e96
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-18 00:00:09 +0600

    feat: complete alphabet war - airstike - letters massacre codewars problem

--- 6kyu/Alphabet war - airstrike - letters massacre/main.js
+++ 6kyu/Alphabet war - airstrike - letters massacre/main.js
@@ -31,3 +31,28 @@ alphabetWar("www*www****z");  //=> Left side wins!
 */
 
 // solution
+
+function alphabetWar(fight) {
+  let power = { w: 4, p: 3, b: 2, s: 1, m: -4, q: -3, d: -2, z: -1 };
+
+  let field = [];
+
+  fight = fight.split("");
+
+  for (let i = 0; i < fight.length; i++) {
+    if (fight[i - 1] != "*" && fight[i] != "*" && fight[i + 1] != "*") {
+      field.push(fight[i]);
+    }
+  }
+
+  let count = field.reduce(
+    (count, warrior) => (count += power[warrior] ? power[warrior] : 0),
+    0
+  );
+
+  return count < 0
+    ? "Right side wins!"
+    : count > 0
+    ? "Left side wins!"
+    : "Let's fight again!";
+}
