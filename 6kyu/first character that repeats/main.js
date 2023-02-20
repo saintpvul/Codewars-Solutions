6d20452fa48d22423a2aabfce15b4224aff141a3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-20 20:12:52 +0600

    feat: start first character that repeats codewars challenge

---
+++ 6kyu/first character that repeats/main.js
@@ -0,0 +1,20 @@
+/*
+
+Find the first character that repeats in a String and return that character.
+
+firstDup('tweet') => 't'
+firstDup('like') => undefined
+This is not the same as finding the character that repeats first. In that case, an input of 'tweet' would yield 'e'.
+
+Another example:
+
+In 'translator' you should return 't', not 'a'.
+
+v      v  
+translator
+  ^   ^
+While second 'a' appears before second 't', the first 't' is before the first 'a'.
+
+*/
+
+// solution
