968ec7a2484155e61deb808d30e3d3167278ebff
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-20 20:19:05 +0600

    feat: complete find the mine! codewars challenge

--- 6kyu/Find the Mine!/main.js
+++ 6kyu/Find the Mine!/main.js
@@ -14,3 +14,13 @@ mineLocation( [ [0, 0, 0], [0, 0, 0], [0, 1, 0] ] ) => returns [2, 1]
 */
 
 // solution
+
+function mineLocation(field) {
+  for (let i = 0; i < field.length; i++) {
+    for (let j = 0; j < field[i].length; j++) {
+      if (field[i][j] === 1) {
+        return [i, j];
+      }
+    }
+  }
+}
