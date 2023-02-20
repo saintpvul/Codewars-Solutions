75d716c22ab67b53b2c11770e3a625e386b62bba
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-21 02:15:16 +0600

    feat: start lowest product of 4 consecutive numbers codewars challenge

---
+++ 6kyu/Lowest product of 4 consecutive numbers/main.js
@@ -0,0 +1,13 @@
+/*
+
+Create a function that returns the lowest product of 4 consecutive digits in a number given as a string.
+
+This should only work if the number has 4 digits or more. If not, return "Number is too small".
+
+Example
+lowestProduct("123456789") --> 24 (1x2x3x4)
+lowestProduct("35") --> "Number is too small"
+
+*/
+
+// solution
