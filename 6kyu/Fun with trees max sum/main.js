4d7e77e8f142852913b557154fe42f2673168615
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-22 23:36:23 +0600

    feat: start fun with trees max sum codewars challenge

---
+++ 6kyu/Fun with trees max sum/main.js
@@ -0,0 +1,24 @@
+/*
+
+You are given a binary tree. Implement the method maxSum which returns the maximal sum of a route from root to leaf. For example, given the following tree:
+
+    17
+   /  \
+  3   -10
+ /    /  \
+2    16   1
+         /
+        13
+The method should return 23, since [17,-10,16] is the route from root to leaf with the maximal sum.
+
+The class TreeNode is available for you:
+
+var TreeNode = function(value, left, right) {
+  this.value = value;
+  this.left = left;
+  this.right = right;
+};
+
+*/
+
+// solution
