18a68e01b2885fd6359f99c9b0c88155dd483979
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-22 23:38:04 +0600

    feat: start fun with trees is perfect codewars challenge

---
+++ 5kyu/Fun with trees is perfect/main.js
@@ -0,0 +1,13 @@
+/*
+
+A perfect binary tree is a binary tree in which all interior nodes have two children and all leaves have the same depth or same level.
+
+You are given a class called TreeNode. Implement the method isPerfect which determines if a given tree denoted by its root node is perfect.
+
+Note: TreeNode class contains helper methods for tree creation, which might be handy for your solution. Feel free to update those methods, but make sure you keep their signatures intact (since they are used from within test cases).
+
+You can (and should) add more tests to validate your solution, since not all cases are covered in the example test cases.
+
+*/
+
+// solution
