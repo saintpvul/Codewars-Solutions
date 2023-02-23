b66a752f61d0fc908b521c5c2a72bb73be9ad6dc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-23 23:59:51 +0600

    feat: complete the observed pin codewars problem

--- 4kyu/The observed PIN/main.js
+++ 4kyu/The observed PIN/main.js
@@ -26,3 +26,39 @@ Detective, we are counting on you!
 */
 
 // solution
+
+function getPINs(observed) {
+  const adjacentDigits = {
+    0: ["0", "8"],
+    1: ["1", "2", "4"],
+    2: ["1", "2", "3", "5"],
+    3: ["2", "3", "6"],
+    4: ["1", "4", "5", "7"],
+    5: ["2", "4", "5", "6", "8"],
+    6: ["3", "5", "6", "9"],
+    7: ["4", "7", "8"],
+    8: ["0", "5", "7", "8", "9"],
+    9: ["6", "8", "9"],
+  };
+
+  function getPossibleDigits(digit) {
+    return adjacentDigits[digit];
+  }
+
+  function getPossiblePINs(pin) {
+    if (pin.length === observed.length) {
+      return [pin];
+    }
+
+    const nextDigit = observed[pin.length];
+    const possibleDigits = getPossibleDigits(nextDigit);
+
+    const possiblePINs = possibleDigits.flatMap((digit) =>
+      getPossiblePINs(pin + digit)
+    );
+
+    return possiblePINs;
+  }
+
+  return getPossiblePINs("").sort();
+}
