c65a4d134b86fcddf4ba3b32a50ede60111eb1f1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-24 00:24:34 +0600

    feat: start brainfuck gen codewars problem

---
+++ 6kyu/Brainfuck generator/main.js
@@ -0,0 +1,10 @@
+/*
+
+Convert text to brainfuck
+You are tasked to writting a function to_brainfuck/toBrainfuck that converts a given string to brainfuck that would print the given string. For example if we were to call to_brainfuck("Hello World!") it might give us a result that is something like: "-[------->+<]>-.-[->+++++<]>++.+++++++..+++.[--->+<]>-----.---[->+++<]>.-[--->+<]>---.+++.------.--------.-[--->+<]>.", if we execute that code we would get "Hello World!" at the output.
+
+
+
+*/
+
+// solution
