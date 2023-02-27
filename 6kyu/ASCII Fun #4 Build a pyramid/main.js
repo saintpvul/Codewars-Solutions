90f970c285c82a6a39311b4bb1e15384fbac9568
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-27 23:53:14 +0600

    feat: start ascii fun #4 build a pyramid codewars problem

---
+++ 6kyu/ASCII Fun #4 Build a pyramid/main.js
@@ -0,0 +1,26 @@
+/*
+
+Build a pyramid
+You will get a string s with an even length, and an integer n which represents the height of the pyramid and your task is to draw the following pattern. Each line is seperated with "\n".
+
+n will always be greater than 3. No need to check for invalid parameters
+There are no whitespaces at the end of the lines
+Example
+build_pyramid("00-00..00-00", 7) should return:
+
+                                    00-00..00-00
+                              0000--0000....0000--0000
+                        000000---000000......000000---000000
+                   00000000----00000000........00000000----00000000
+            0000000000-----0000000000..........0000000000-----0000000000
+      000000000000------000000000000............000000000000------000000000000
+00000000000000-------00000000000000..............00000000000000-------00000000000000
+Serie: ASCII Fun
+ASCII Fun #1: X-Shape
+ASCII Fun #2: Funny Dots
+ASCII Fun #3: Puzzle Tiles
+ASCII Fun #4: Build a pyramid
+
+*/
+
+// solution
