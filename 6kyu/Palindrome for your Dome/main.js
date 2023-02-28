e29b2bd8bd163a95af5e932732562d156c458ef7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-28 23:39:33 +0600

    feat: complete palindrome for your dome codewars problem

--- 6kyu/Palindrome for your Dome/main.js
+++ 6kyu/Palindrome for your Dome/main.js
@@ -1,7 +1,35 @@
 /*
 
+A palindrome is a word, phrase, number, or other sequence of symbols or elements, whose meaning may be interpreted the same way in either forward or reverse direction. Famous examples include "Amore, Roma", "A man, a plan, a canal: Panama" and "No 'x' in 'Nixon'". - wikipedia
 
+Our goal is to determine whether or not a given string is a valid palindrome or not.
+
+Like the above examples, here are a few test cases which are also populated:
+
+"Amore, Roma" => valid
+"A man, a plan, a canal: Panama" => valid
+"No 'x' in 'Nixon'" => valid
+"Abba Zabba, you're my only friend" => invalid
+You can see that they are case insensitive and disregards non alphanumeric characters. In addition to a few predefined tests, your function will also be tested against a random string generator 50 times which are guaranteed to produce valid palindromes.
+
+Notes:
+
+The empty string "" can be read forward or backward the same, it's a palindrome in our case.
 
 */
 
 // solution
+
+function palindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  if (str.length === 0 || str.length === 1) {
+    return true;
+  }
+
+  if (str[0] === str[str.length - 1]) {
+    return palindrome(str.substring(1, str.length - 1));
+  } else {
+    return false;
+  }
+}
