27adec5c5db19fcf692deb2af07754673773703e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-02 22:01:25 +0600

    feat: complete recover a secret string from random triplets codewars challenge

--- 4kyu/Recover a secret string from random triplets/main.js
+++ 4kyu/Recover a secret string from random triplets/main.js
@@ -11,3 +11,50 @@ You can assume nothing about the triplets given to you other than that they are 
 */
 
 // solution
+
+function recoverSecret(triplets) {
+  const graph = {};
+  const nodes = new Set();
+
+  triplets.forEach((triplet) => {
+    nodes.add(triplet[0]);
+    nodes.add(triplet[1]);
+    nodes.add(triplet[2]);
+  });
+
+  nodes.forEach((node) => {
+    graph[node] = { incoming: new Set(), outgoing: new Set() };
+  });
+
+  triplets.forEach((triplet) => {
+    const [char1, char2, char3] = triplet;
+    graph[char1].outgoing.add(char2);
+    graph[char1].outgoing.add(char3);
+    graph[char2].incoming.add(char1);
+    graph[char2].outgoing.add(char3);
+    graph[char3].incoming.add(char1);
+    graph[char3].incoming.add(char2);
+  });
+
+  const startNodes = [];
+  nodes.forEach((node) => {
+    if (graph[node].incoming.size === 0) {
+      startNodes.push(node);
+    }
+  });
+
+  const sortedNodes = [];
+  const queue = startNodes.slice();
+  while (queue.length > 0) {
+    const node = queue.shift();
+    sortedNodes.push(node);
+    graph[node].outgoing.forEach((neighbor) => {
+      graph[neighbor].incoming.delete(node);
+      if (graph[neighbor].incoming.size === 0) {
+        queue.push(neighbor);
+      }
+    });
+  }
+
+  return sortedNodes.join("");
+}
