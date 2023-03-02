4f85e192089ba368f92c8123231416fe42c8475c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-02 21:54:07 +0600

    feat: complete adding big numbers codewars challenge

--- 4kyu/Adding Big Numbers/main.js
+++ 4kyu/Adding Big Numbers/main.js
@@ -15,3 +15,20 @@ The numbers are positives
 */
 
 // solution
+
+function add(a, b) {
+  let res = "",
+    curr = 0;
+  let i = a.length - 1,
+    j = b.length - 1;
+
+  while (i >= 0 || j >= 0 || curr > 0) {
+    const sum = (+a[i] || 0) + (+b[j] || 0) + curr;
+    curr = Math.floor(sum / 10);
+    res = (sum % 10) + res;
+    i--;
+    j--;
+  }
+
+  return res;
+}
