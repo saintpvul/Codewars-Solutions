030dae4ac87502a86b4bc8d40c212ece83f97266
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-02 21:52:01 +0600

    feat: complete decode the morse code, advanced codewars challenge

--- 4kyu/Decode the Morse code, advanced/main.js
+++ 4kyu/Decode the Morse code, advanced/main.js
@@ -41,3 +41,32 @@ After you master this kata, you may try to Decode the Morse code, for real.
 */
 
 // solution
+
+function decodeBits(bits) {
+  bits = bits.replace(/(^0+|0+$)/g, "");
+
+  let rate = Math.min.apply(
+    null,
+    bits.match(/0+|1+/g).map(function (b) {
+      return b.length;
+    })
+  );
+
+  bits = bits
+    .replace(new RegExp("(?:111){" + rate + "}(?:0{" + rate + "}|$)", "g"), "-")
+    .replace(new RegExp("1{" + rate + "}(?:0{" + rate + "}|$)", "g"), ".")
+    .replace(new RegExp("(?:000000){" + rate + "}", "g"), "   ")
+    .replace(new RegExp("(?:00){" + rate + "}", "g"), " ");
+
+  return bits;
+}
+
+function decodeMorse(message) {
+  return message
+    .replace(/   /g, " _ ")
+    .split(" ")
+    .map(function (letter) {
+      return letter === "_" ? " " : MORSE_CODE[letter];
+    })
+    .join("");
+}
