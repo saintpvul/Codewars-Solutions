a5705cbc973d3d331020a20527a20aab966ec04d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-02 22:04:24 +0600

    feat: complete pyramid slide down codewars challenge

--- 4kyu/Pyramid Slide Down/main.js
+++ 4kyu/Pyramid Slide Down/main.js
@@ -22,3 +22,13 @@ My tests include some extraordinarily high pyramids so as you can guess, brute-f
 */
 
 // solution
+
+function longestSlideDown(pyramid) {
+  for (let i = pyramid.length - 2; i >= 0; i--) {
+    for (let j = 0; j < pyramid[i].length; j++) {
+      pyramid[i][j] += Math.max(pyramid[i + 1][j], pyramid[i + 1][j + 1]);
+    }
+  }
+
+  return pyramid[0][0];
+}
