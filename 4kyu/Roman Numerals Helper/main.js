9c18ac925f3b19fe253627c872b310c9dfe99a04
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-02 22:07:03 +0600

    feat: complete roman numerals helper codewars challenge

--- 4kyu/Roman Numerals Helper/main.js
+++ 4kyu/Roman Numerals Helper/main.js
@@ -25,3 +25,70 @@ M	1000
 */
 
 // solution
+
+class RomanNumerals {
+  static romanToIntMap = new Map([
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+  ]);
+
+  static intToRomanMap = new Map([
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ]);
+
+  static fromRoman(roman) {
+    let result = 0;
+
+    for (let i = 0; i < roman.length; i++) {
+      let currentChar = roman[i];
+      let currentValue = RomanNumerals.romanToIntMap.get(currentChar);
+      let nextChar = roman[i + 1];
+      let nextValue = RomanNumerals.romanToIntMap.get(nextChar);
+
+      if (nextValue && nextValue > currentValue) {
+        result += nextValue - currentValue;
+        i++;
+      } else {
+        result += currentValue;
+      }
+    }
+
+    return result;
+  }
+
+  static toRoman(number) {
+    let result = "";
+
+    for (let [int, roman] of RomanNumerals.intToRomanMap) {
+      while (number >= int) {
+        result += roman;
+        number -= int;
+      }
+    }
+
+    return result;
+  }
+}
