0088a5f0666202825b409c56316e241bfcd052b2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-03 21:39:25 +0600

    feat: complete is my friend cheating? codewars challenge

--- 5kyu/Is my friend cheating/main.js
+++ 5kyu/Is my friend cheating/main.js
@@ -34,3 +34,15 @@ See examples of returns for each language in "RUN SAMPLE TESTS"
 */
 
 // solution
+
+function removeNb(n) {
+  const sum = (n * (n + 1)) / 2;
+  const result = [];
+  for (let a = 1; a <= n; a++) {
+    const b = (sum - a) / (a + 1);
+    if (b % 1 === 0 && b <= n) {
+      result.push([a, b]);
+    }
+  }
+  return result;
+}
