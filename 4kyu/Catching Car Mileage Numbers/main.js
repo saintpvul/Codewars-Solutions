df1ad8f59e4633c03aeb9c0b9d49383fc018aff8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-04 17:56:06 +0600

    feat: complete catching car mileage numbers codewars challenge

--- 4kyu/Catching Car Mileage Numbers/main.js
+++ 4kyu/Catching Car Mileage Numbers/main.js
@@ -46,3 +46,51 @@ You should only ever output 0, 1, or 2.
 */
 
 // solution
+
+function isPalindrome(number) {
+  return (
+    number.toString().length > 2 &&
+    /^(.?)(.?)(.?)(.?)(.?).?\5\4\3\2\1$/.test(number)
+  );
+}
+
+function isSequentialAsc(number) {
+  return number.toString().length > 2 && "1234567890".indexOf(number) >= 0;
+}
+
+function isSequentialDesc(number) {
+  return number.toString().length > 2 && "9876543210".indexOf(number) >= 0;
+}
+
+function isAwesomePhrase(number, awesomePhrases) {
+  return awesomePhrases.some(function (n) {
+    return n == number;
+  });
+}
+
+function isFollowedByZero(number) {
+  return number % 100 == 0 || number % 1000 == 0 || number % 10000 == 0;
+}
+
+function isSameDigits(number) {
+  return /^(\d)\1{2,4}$/.test(number);
+}
+
+function isInteresting(number, awesomePhrases) {
+  let isInteresting = 0;
+
+  for (let i = 0; i < 3 && !isInteresting && number >= 98; ++i, number++) {
+    isInteresting =
+      isPalindrome(number) ||
+      isSequentialAsc(number) ||
+      isSequentialDesc(number) ||
+      isAwesomePhrase(number, awesomePhrases) ||
+      isFollowedByZero(number) ||
+      isSameDigits(number)
+        ? i == 0
+          ? 2
+          : 1
+        : 0;
+  }
+  return isInteresting;
+}
