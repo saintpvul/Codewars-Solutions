03015b1e77896af8a3de0febaebe165ae2590ff4
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-04 19:38:41 +0600

    feat: complete sum of intervals codewars challenge

--- 4kyu/Sum of Intervals/main.js
+++ 4kyu/Sum of Intervals/main.js
@@ -47,3 +47,25 @@ Your algorithm should be able to handle large intervals. All tested intervals ar
 */
 
 // solution
+
+function sumIntervals(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let totalLength = 0;
+  let currentInterval = intervals[0];
+
+  for (let i = 1; i < intervals.length; i++) {
+    const nextInterval = intervals[i];
+
+    if (nextInterval[0] <= currentInterval[1]) {
+      currentInterval[1] = Math.max(currentInterval[1], nextInterval[1]);
+    } else {
+      totalLength += currentInterval[1] - currentInterval[0];
+      currentInterval = nextInterval;
+    }
+  }
+
+  totalLength += currentInterval[1] - currentInterval[0];
+
+  return totalLength;
+}
