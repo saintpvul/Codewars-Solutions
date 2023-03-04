f0f401857a7381313f1f9e9c81f94bb970482d6c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-04 18:00:45 +0600

    feat: complete tic-tac-toe checker codewars challenge

--- 5kyu/Tic-Tac-Toe Checker/main.js
+++ 5kyu/Tic-Tac-Toe Checker/main.js
@@ -18,3 +18,53 @@ You may assume that the board passed in is valid in the context of a game of Tic
 */
 
 // solution
+
+function isSolved(board) {
+  //check rows
+  for (let i = 0; i < 3; i++) {
+    if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+      if (board[i][0] === 1) {
+        return 1; // X won
+      } else if (board[i][0] === 2) {
+        return 2; // O won
+      }
+    }
+  }
+  //check cols
+  for (let i = 0; i < 3; i++) {
+    if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+      if (board[0][i] === 1) {
+        return 1; // X won
+      } else if (board[0][i] === 2) {
+        return 2; // O won
+      }
+    }
+  }
+  //check diag
+  if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+    if (board[0][0] === 1) {
+      return 1; // X won
+    } else if (board[0][0] === 2) {
+      return 2; // O won
+    }
+  }
+
+  if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+    if (board[0][2] === 1) {
+      return 1; // X won
+    } else if (board[0][2] === 2) {
+      return 2; // O won
+    }
+  }
+
+  // Check for empty spots
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] === 0) {
+        return -1; // Game is not finished
+      }
+    }
+  }
+
+  return 0;
+}
