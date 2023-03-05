9f50c57bbb9ae06024542372eca42f32818a01eb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-05 19:37:00 +0600

    feat: start next smaller number with tha same digits codewars challenge

---
+++ 4kyu/Next smaller number with the same digits/main.js
@@ -0,0 +1,22 @@
+/*
+
+Write a function that takes a positive integer and returns the next smaller positive integer containing the same digits.
+
+For example:
+
+nextSmaller(21) == 12
+nextSmaller(531) == 513
+nextSmaller(2071) == 2017
+Return -1 (for Haskell: return Nothing, for Rust: return None), when there is no smaller number that contains the same digits. Also return -1 when the next smaller number with the same digits would require the leading digit to be zero.
+
+nextSmaller(9) == -1
+nextSmaller(111) == -1
+nextSmaller(135) == -1
+nextSmaller(1027) == -1 // 0721 is out since we don't write numbers with leading zeros
+some tests will include very large numbers.
+test data only employs positive integers.
+The function you write for this challenge is the inverse of this kata: "Next bigger number with the same digits."
+
+*/
+
+// solution
