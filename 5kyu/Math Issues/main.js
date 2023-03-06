47ab3c02decede8262f1ea4ce03a1a067b389883
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-06 21:23:00 +0600

    feat: start math issues codewars challenge

---
+++ 5kyu/Math Issues/main.js
@@ -0,0 +1,13 @@
+/*
+
+Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? We can assure, that only non-negative numbers are passed as arguments. So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
+
+Here is a list of functions, we need:
+
+Math.round()
+Math.ceil()
+Math.floor()
+
+*/
+
+// solution
