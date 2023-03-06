746219c32c42691d6a49021456c6888c0051603d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-06 22:24:37 +0600

    feat: complete merged string checker codewars challenge

--- 5kyu/Math Issues/main.js
+++ 5kyu/Math Issues/main.js
@@ -11,3 +11,25 @@ Math.floor()
 */
 
 // solution
+
+function isMerge(s, part1, part2, memo = new Map()) {
+  const key = `${s},${part1},${part2}`;
+  if (memo.has(key)) {
+    return memo.get(key);
+  }
+  if (!s) {
+    const result = !(part1 || part2);
+    memo.set(key, result);
+    return result;
+  }
+  const char = s[0];
+  let result = false;
+  if (part1[0] === char) {
+    result = isMerge(s.slice(1), part1.slice(1), part2, memo);
+  }
+  if (!result && part2[0] === char) {
+    result = isMerge(s.slice(1), part1, part2.slice(1), memo);
+  }
+  memo.set(key, result);
+  return result;
+}
