a02aa01437b4cff42a07e746ddae9f6aab13d967
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-07 23:11:18 +0600

    feat: complete large factorials codewars challenge

--- 4kyu/Large factorials/main.js
+++ 4kyu/Large factorials/main.js
@@ -17,3 +17,27 @@ I have removed the use of require in the javascript language.
 */
 
 // solution
+
+function factorial(n) {
+  if (n < 0) {
+    return null;
+  } else if (n === 0) {
+    return "1";
+  } else {
+    let result = [];
+    result[0] = 1;
+    for (let i = 2; i <= n; i++) {
+      let carry = 0;
+      for (let j = 0; j < result.length; j++) {
+        let product = result[j] * i + carry;
+        result[j] = product % 10;
+        carry = Math.floor(product / 10);
+      }
+      while (carry > 0) {
+        result.push(carry % 10);
+        carry = Math.floor(carry / 10);
+      }
+    }
+    return result.reverse().join("");
+  }
+}
