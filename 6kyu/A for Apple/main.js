bcbfb3cdac6fa87c0689d95732d3d4aa132856f3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-11 22:51:34 +0600

    feat: complete a for apple codewars challenge

--- 6kyu/A for Apple/main.js
+++ 6kyu/A for Apple/main.js
@@ -44,3 +44,25 @@ If n is odd, a(n) = a(n - 1), eg a(5) == a(4); a(9) == a(8)
 */
 
 // solution
+
+function a(n) {
+    if (n < 4) return "";
+    if (n % 2 != 0) n--;
+    let a = "",
+        len = 2 * n - 1,
+        mid = ~~(len / 2);
+    for (let i = 1; i <= n; i++) {
+        let start = mid - i + 2,
+            end = mid + i;
+        if (i == 1) (start = mid + 1), (end = mid + 1);
+        for (let j = 1; j <= len; j++) {
+            if (i == ~~(n / 2 + 1)) {
+                a += j >= start && j <= end && start % 2 == j % 2 ? "A" : " ";
+            } else {
+                a += j == start || j == end ? "A" : " ";
+            }
+        }
+        if (i != n) a += "\n";
+    }
+    return a;
+}
