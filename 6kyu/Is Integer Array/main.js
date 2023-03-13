1bad94fe084a154acb6d5f9ab30863ff1d091042
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-13 23:59:09 +0600

    feat: start is integer array codewars challenge

---
+++ 6kyu/Is Integer Array/main.js
@@ -0,0 +1,14 @@
+/*
+
+Write a function with the signature shown below:
+
+function isIntArray(arr) {
+  return true
+}
+returns true  / True if every element in an array is an integer or a float with no decimals.
+returns true  / True if array is empty.
+returns false / False for every other input.
+
+*/
+
+// solution
