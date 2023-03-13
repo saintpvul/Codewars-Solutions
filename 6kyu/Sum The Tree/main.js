ee92f0ff25d1d7cef3d0bd1a967ec0bd1c8afd5a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-13 23:58:46 +0600

    feat: start sum the tree codewars challenge

---
+++ 6kyu/Sum The Tree/main.js
@@ -0,0 +1,27 @@
+/*
+
+Given a node object representing a binary tree:
+
+Node:
+  value: <int>,
+  left: <Node> or null,
+  right: <Node> or null
+write a function that returns the sum of all values, including the root. Absence of a node will be indicated with a null value.
+
+Examples:
+
+10
+| \
+1  2
+=> 13
+
+1
+| \
+0  0
+    \
+     2
+=> 3
+
+*/
+
+// solution
