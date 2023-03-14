93b112a48dd31f91e2183a991dccf0b3c04a589d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-14 21:31:32 +0600

    feat: complete fruit machine codewars challenge

--- 6kyu/Fruit Machine/main.js
+++ 6kyu/Fruit Machine/main.js
@@ -90,3 +90,53 @@ Good luck and enjoy!
 */
 
 // solution
+
+function fruit(reels, spins) {
+    const reel = [
+        "Wild",
+        "Star",
+        "Bell",
+        "Shell",
+        "Seven",
+        "Cherry",
+        "Bar",
+        "King",
+        "Queen",
+        "Jack",
+    ];
+    const items = [reels[0][spins[0]], reels[1][spins[1]], reels[2][spins[2]]];
+
+    if (items[0] === items[1] && items[0] === items[2]) {
+        return (10 - reel.indexOf(items[0])) * 10;
+    }
+
+    let item = "";
+    let extra = "";
+
+    if (items[0] === items[1]) {
+        item = items[0];
+        extra = items[2];
+    }
+
+    if (items[0] === items[2]) {
+        item = items[0];
+        extra = items[1];
+    }
+
+    if (items[1] === items[2]) {
+        item = items[1];
+        extra = items[0];
+    }
+
+    if (item !== "") {
+        let score = 10 - reel.indexOf(item);
+
+        if (extra === "Wild") {
+            score = score * 2;
+        }
+
+        return score;
+    }
+
+    return 0;
+}
