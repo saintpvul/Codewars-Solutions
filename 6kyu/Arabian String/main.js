57e135a554e434341d8d53bdae703d34b0613b20
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-16 23:49:24 +0600

    feat: start arabian string codewars challenge

---
+++ 6kyu/Arabian String/main.js
@@ -0,0 +1,15 @@
+/*
+
+You must create a method that can convert a string from any format into PascalCase. This must support symbols too.
+
+Don't presume the separators too much or you could be surprised.
+
+For example: (Input --> Output)
+
+"example name" --> "ExampleName"
+"your-NaMe-here" --> "YourNameHere"
+"testing ABC" --> "TestingAbc"
+
+*/
+
+// solution
