3b9638427236fb048a1d958778b959745c423150
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-19 23:22:55 +0600

    feat: start strip url params codewars challenge

---
+++ 6kyu/Strip Url Params/main.js
@@ -0,0 +1,13 @@
+/*
+
+Complete the method so that it does the following:
+Removes any duplicate query string parameters from the url (the first occurence should be kept)
+Removes any query string parameters specified within the 2nd argument (optional array)
+Examples:
+stripUrlParams('www.codewars.com?a=1&b=2&a=2') === 'www.codewars.com?a=1&b=2'
+stripUrlParams('www.codewars.com?a=1&b=2&a=2', ['b']) === 'www.codewars.com?a=1'
+stripUrlParams('www.codewars.com', ['b']) === 'www.codewars.com'
+
+*/
+
+// solution
