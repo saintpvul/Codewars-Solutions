7a2908164a4ddf7f5c78fc649ab94bda2a4cdabf
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-20 21:48:22 +0600

    feat: complete hamming distance codewars challenge

--- 6kyu/Hamming Distance/main.js
+++ 6kyu/Hamming Distance/main.js
@@ -19,3 +19,11 @@ You can assume that the two inputs are ASCII strings of equal length.
 */
 
 // solution
+
+function hamming(a, b) {
+    let counter = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) counter++;
+    }
+    return counter;
+}
