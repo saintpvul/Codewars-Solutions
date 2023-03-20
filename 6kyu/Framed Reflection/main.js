57672749e40bcf5ab24dea337e643f595f7439bc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-20 23:28:30 +0600

    feat: complete framed reflection codewars challenge

--- 6kyu/Framed Reflection/main.js
+++ 6kyu/Framed Reflection/main.js
@@ -13,3 +13,18 @@ Words in your solution should be left-aligned.
 */
 
 // solution
+
+function mirror(text) {
+    text = text.split(" ");
+    let maxLen = Math.max(...text.map((e) => e.length));
+    let sides = "*".repeat(maxLen + 4);
+    let res = [];
+    for (let i = 0; i < text.length; i++) {
+        res.push(
+            `${"*"} ${text[i].split("").reverse().join("")}${" ".repeat(
+                maxLen - text[i].length + 1
+            )}${"*"}`
+        );
+    }
+    return `${sides}\n${res.join("\n")}\n${sides}`;
+}
