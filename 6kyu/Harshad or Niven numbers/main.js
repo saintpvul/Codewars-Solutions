123278bdf50f6f8001b01cd57cedf4adbe9f9bee
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-22 21:44:14 +0600

    feat: complete harshad or niven numbers codewars challenge

--- 6kyu/Harshad or Niven numbers/main.js
+++ 6kyu/Harshad or Niven numbers/main.js
@@ -33,3 +33,66 @@ Harshad.getSerie(3, 1000)  ==>  [ 1002, 1008, 1010 ]
 */
 
 // solution
+
+/**
+ * Utility class for Harshad numbers (also called Niven numbers).
+ *
+ * @namespace Harshad
+ */
+var Harshad = (function () {
+    "use strict";
+
+    return {
+        /**
+         * Returns true when the given number is a valid Harshad number.
+         *
+         * @param {Number} number The given number
+         * @returns {Boolean}
+         * @function Harshad.isValid
+         */
+        isValid: function (number) {
+            let sum = String(number)
+                .split("")
+                .reduce((s, v) => (s += parseInt(v)), 0);
+            return number % sum === 0;
+        },
+        /**
+         * Gets the next Harshad number after the given number.
+         *
+         * @param {Number} number The given number
+         * @returns {Number}
+         * @function Harshad.getNext
+         */
+        getNext: function (number) {
+            let nextNum = number + 1;
+
+            while (!Harshad.isValid(nextNum)) {
+                nextNum++;
+            }
+
+            return nextNum;
+        },
+        /**
+         * Returns the suite of Harshad numbers, starting after a given number.
+         *
+         * @param {Number} count The number of elements to return
+         * @param {Number} start The number after which the serie should start;
+         *  defaults to 0
+         * @returns {Array}
+         * @function Harshad.getSerie
+         */
+        getSerie: function (count, start = 0) {
+            let niven = [];
+            let nextNum = start + 1;
+
+            while (niven.length < count) {
+                if (Harshad.isValid(nextNum)) {
+                    niven.push(nextNum);
+                }
+                nextNum = Harshad.getNext(nextNum);
+            }
+
+            return niven;
+        },
+    };
+})();
