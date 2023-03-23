e53520b23d55ec020410c1ff1945188c71702dd7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-23 23:14:54 +0600

    feat: start fun with lists countlf codewars challenge

---
+++ 6kyu/Fun with lists countIf/main.js
@@ -0,0 +1,19 @@
+/*
+
+Implement the method countIf (count_if in PHP and Python), which accepts a linked list (head) and a predicate function, and returns the number of elements which apply to the given predicate.
+
+For example: Given the list: 1 -> 2 -> 3, and the predicate x => x >= 2, countIf / count_if should return 2, since x >= 2 applies to both 2 and 3.
+
+The linked list is defined as follows:
+
+function Node(data, next = null) {
+  this.data = data;
+  this.next = next;
+}
+Note: the list may be null and can hold any type of value.
+
+Good luck!
+
+*/
+
+// solution
