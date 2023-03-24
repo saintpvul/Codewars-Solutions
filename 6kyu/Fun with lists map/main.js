26c1b9d92b3ae2b72abc4a37ba2ba6055b0ab192
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-25 01:08:44 +0600

    feat: start fun with lists map codewars challenge

---
+++ 6kyu/Fun with lists map/main.js
@@ -0,0 +1,19 @@
+/*
+
+Implement the method map, which accepts a linked list (head) and a mapping function, and returns a new linked list (head) where every element is the result of applying the given mapping method to each element of the original list.
+
+For example: Given the list: 1 -> 2 -> 3, and the mapping function x => x * 2, map should return 2 -> 4 -> 6
+
+The linked list is defined as follows:
+
+function Node(data, next = null) {
+  this.data = data;
+  this.next = next;
+}
+Note: the list may be null and can hold any type of value.
+
+Good luck!
+
+*/
+
+// solution
