1e87585c1d7c31a82557d331384ce3487afee96e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-28 20:46:10 +0600

    feat: complete ascii hex converter codewars challenge

--- 6kyu/ASCII hex converter/main.js
+++ 6kyu/ASCII hex converter/main.js
@@ -12,3 +12,20 @@ Converter.toAscii("4c6f6f6b206d6f6d2c206e6f2068616e6473")
 */
 
 // solution
+
+var Converter = {
+    toAscii: function (hex) {
+        let res = "";
+        for (let i = 0; i < hex.length; i += 2) {
+            res += String.fromCharCode(parseInt(hex.slice(i, i + 2), 16));
+        }
+        return res;
+    },
+    toHex: function (ascii) {
+        let res = "";
+        for (let i = 0; i < ascii.length; i++) {
+            res += ("0" + ascii[i].charCodeAt().toString(16)).slice(-2);
+        }
+        return res;
+    },
+};
