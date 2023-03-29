f068d9fcd878525b7ae3a5e4af58ff47f666badb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-29 23:43:50 +0600

    feat: complete my safety's tresures codewars challenge

--- 6kyu/Mr. Safety's treasures/main.js
+++ 6kyu/Mr. Safety's treasures/main.js
@@ -18,3 +18,44 @@ Example
 */
 
 // solution
+
+function unlock(str) {
+    const letterToDigit = {
+        a: "2",
+        b: "2",
+        c: "2",
+        d: "3",
+        e: "3",
+        f: "3",
+        g: "4",
+        h: "4",
+        i: "4",
+        j: "5",
+        k: "5",
+        l: "5",
+        m: "6",
+        n: "6",
+        o: "6",
+        p: "7",
+        q: "7",
+        r: "7",
+        s: "7",
+        t: "8",
+        u: "8",
+        v: "8",
+        w: "9",
+        x: "9",
+        y: "9",
+        z: "9",
+    };
+
+    let digits = "";
+    for (let i = 0; i < str.length; i++) {
+        const letter = str[i].toLowerCase();
+        if (letterToDigit[letter]) {
+            digits += letterToDigit[letter];
+        }
+    }
+
+    return digits;
+}
