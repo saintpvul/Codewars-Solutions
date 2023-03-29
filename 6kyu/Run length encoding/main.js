dd3aa01eb46f17c06aa0ebbaa8e502d664038ac7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-03-29 23:57:16 +0600

    feat: complete run length encoding codewars challenge

--- 6kyu/Run length encoding/main.js
+++ 6kyu/Run length encoding/main.js
@@ -18,3 +18,22 @@ runLengthEncoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb")
 */
 
 // solution
+
+var runLengthEncoding = function (str) {
+    str = str.split("");
+
+    let res = [];
+
+    if (!str.length) return res;
+
+    let count = 1;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i + 1] != str[i]) {
+            res.push([count, str[i]]);
+            count = 1;
+        } else {
+            count++;
+        }
+    }
+    return res;
+};
