1d56c3a6dd5ecef5015f8776dc4caaf546260846
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-03 21:50:47 +0600

    feat: start count the islands codewars challenge

---
+++ 6kyu/Count the Islands/main.js
@@ -0,0 +1,26 @@
+/*
+
+Summary
+Implement an algorithm which analyzes a two-color image and determines how many isolated areas of a single color the image contains.
+
+Islands
+An "island" is a set of adjacent pixels of one color (1) which is surrounded by pixels of a different color (0). Pixels are considered adjacent if their coordinates differ by no more than 1 on the X or Y axis.
+
+Below you can see an example with 2 islands:
+
+on the left in the form of a matrix of 1's and 0's
+on the right in an equivalent stringified form using "X" and "~" characters for better readability
+[
+  [0,0,0,0,0,0,0,0,0,0],          "~~~~~~~~~~"
+  [0,0,1,1,0,0,0,0,0,0],          "~~XX~~~~~~"
+  [0,0,1,1,0,0,0,0,0,0],          "~~XX~~~~~~"
+  [0,0,0,0,0,0,0,0,1,0],          "~~~~~~~~X~"
+  [0,0,0,0,0,1,1,1,0,0],          "~~~~~XXX~~"
+  [0,0,0,0,0,0,0,0,0,0],          "~~~~~~~~~~"
+]
+Specification
+Your task is to implement a function which accepts a matrix containing the numbers 0 and 1. It should return the number of islands as an integer.
+
+*/
+
+// solution
