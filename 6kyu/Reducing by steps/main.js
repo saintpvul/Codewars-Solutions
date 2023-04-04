37edbc04a68ca20cee36cea5a1abbdb1c9caf829
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-05 00:30:47 +0600

    feat: complete reducing by steps codewars challenge

--- 6kyu/Reducing by steps/main.js
+++ 6kyu/Reducing by steps/main.js
@@ -49,3 +49,38 @@ In OCaml arrays are replaced by lists.
 */
 
 // solution
+
+unction gcdi(x, y) {
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while (y) {
+    let t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}
+
+function lcmu(a, b) {
+  return Math.abs(a * b) / gcdi(a, b);
+}
+
+function som(a, b) {
+  return a + b;
+}
+
+function maxi(a, b) {
+  return Math.max(a, b);
+}
+
+function mini(a, b) {
+  return Math.min(a, b);
+}
+
+function operArray(fct, arr, init) {
+  return arr.reduce((acc, cur) => {
+    const result = fct(acc[acc.length - 1], cur);
+    acc.push(result);
+    return acc;
+  }, [init]).slice(1);
+}
