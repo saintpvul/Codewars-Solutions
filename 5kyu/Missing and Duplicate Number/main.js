78686f02cee8e8b928e9559d49bd243c17621890
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-04 21:39:09 +0600

    feat: start missing and suplicate number codewars challenge

---
+++ 5kyu/Missing and Duplicate Number/main.js
@@ -0,0 +1,21 @@
+/*
+
+You are given an array containing all integers from 1 to N, except one that is missing.
+
+In order to fill the array, one of the elements of the sequence has been duplicated.
+
+Your mission is to find which number is missing and which one is duplicated.
+
+Example:
+
+solution([2,4,1,6,3,4]) should return [5,4] because 5 is missing and 4 appears twice.
+
+Remarks:
+
+You are not allowed to sort the array.
+Your solution should not time out for large values of N.
+Ideally, your solution should not use extra space except the one provided by the input array (which can be modified).
+
+*/
+
+// solution
