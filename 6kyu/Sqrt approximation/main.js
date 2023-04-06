49e935448035baf04ca54e579728107e85a23d00
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-07 00:37:01 +0600

    feat: start sqrt approximation codewars challenge

---
+++ 6kyu/Sqrt approximation/main.js
@@ -0,0 +1,19 @@
+/*
+
+We want to approximate the sqrt function. Usually this function takes a floating point number and returns another floating point number, but in this kata we're going to work with integral numbers instead.
+
+Your task is to write a function that takes an integer n and returns either
+
+an integer k if n is a square number, such that k * k == n or
+a range (k, k+1), such that k * k < n and n < (k+1) * (k+1).
+Examples
+assert.deepEqual( sqrtApproximation(4), 2 );
+assert.deepEqual( sqrtApproximation(5), [2,3] );
+Note : pow and sqrt functions are disabled.
+
+Remarks
+In dynamic languages, return either a single value or an array/list. In Haskell, use Either.
+
+*/
+
+// solution
