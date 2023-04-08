aea0528ff586e2d7b824d0d698e4e53f7b4deeb6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-08 23:08:28 +0600

    feat: start pell numbers codewars challenge

---
+++ 6kyu/Pell Numbers/main.js
@@ -0,0 +1,17 @@
+/*
+
+The Pell sequence is the sequence of integers defined by the initial values
+
+P(0) = 0, P(1) = 1
+and the recurrence relation
+
+P(n) = 2 * P(n-1) + P(n-2)
+The first few values of P(n) are
+
+0, 1, 2, 5, 12, 29, 70, 169, 408, 985, 2378, 5741, 13860, 33461, 80782, 195025, 470832, ...
+Task
+Your task is to write a method that returns nth Pell number
+
+*/
+
+// solution
