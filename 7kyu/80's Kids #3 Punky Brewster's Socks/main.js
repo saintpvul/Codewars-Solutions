797d2b4dbc1544e7e15ffe34a68c423e02973e21
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-11 23:19:30 +0600

    feat: start 80's kids #3: punky brewster's socks codewars challenge

---
+++ 7kyu/80's Kids #3 Punky Brewster's Socks/main.js
@@ -0,0 +1,36 @@
+/*
+
+Punky loves wearing different colored socks, but Henry can't stand it.
+
+Given an array of different colored socks, return a pair depending on who was picking them out.
+
+Example:
+
+getSocks('Punky', ['red','blue','blue','green']) -> ['red', 'blue']
+Note that Punky can have any two colored socks, in any order, as long as they are different and both exist. Henry always picks a matching pair.
+
+If there is no possible combination of socks, return an empty array.
+
+Check out my other 80's Kids Katas:
+80's Kids #1: How Many Licks Does It Take
+80's Kids #2: Help Alf Find His Spaceship
+
+80's Kids #3: Punky Brewster's Socks
+
+80's Kids #4: Legends of the Hidden Temple
+
+80's Kids #5: You Can't Do That on Television
+
+80's Kids #6: Rock 'Em, Sock 'Em Robots
+
+80's Kids #7: She's a Small Wonder
+
+80's Kids #8: The Secret World of Alex Mack
+
+80's Kids #9: Down in Fraggle Rock
+
+80's Kids #10: Captain Planet
+
+*/
+
+// solution
